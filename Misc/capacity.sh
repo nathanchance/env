d899193df82759a5d7eb61ba3587fd80a41d73c0
Nathan Chancellor <natechancellor@gmail.com>
2016-05-03 23:59:18 -0700

    Update capacity

--- Misc/capacity.sh
+++ Misc/capacity.sh
@@ -1,1 +1,5 @@
-du -csh ~/DU ~/PN-CMTE ~/PN-Layers ~/AOSiP ~/GApps ~/shared ~/scripts
+# du (disk usage)
+# -c: report a grand total at the end
+# -h: list as human readable
+# -s: reports a sum for the usage of each directory
+du -c -h -s .[!.]* *|
