3330e48c31ecabaaf49f2f77ed16b196c4f43b72
Nathan Chancellor <natechancellor@gmail.com>
2016-05-14 22:16:32 -0700

    Add Elite script
    Add Elite build script here (since it is a personal one) and turn
    public facing script into a template

---
+++ Misc/build_elite.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+# Path to build your kernel
+SOURCEDIR=~/Kernels/elite_angler
+
+# Directory for the AnyKernel updater
+AKDIR=${SOURCEDIR}/packagesm
+
+# Make it show nathan@chancellor in the kernel version
+export KBUILD_BUILD_USER=nathan
+export KBUILD_BUILD_HOST=chancellor
+
+# Upload directory
+UPLOADDIR=~/shared/Elite
+
+# Date to add to zip
+today=$(date +"%m_%d_%Y")
+
+# Start tracking time
+START=$(date +%s)
+
+# Change to source directory to start
+cd ${SOURCEDIR}
+
+# Clean out everything
+git reset --hard
+git clean -f -d
+make clean
+make mrproper
+
+# Setup the build
+cd ${SOURCEDIR}/arch/arm64/configs/BBKconfigsM
+for KERNELNAME in *
+ do
+  cd ${SOURCEDIR}
+
+# Setup output directory
+mkdir -p "out/${KERNELNAME}"
+cp -R "${AKDIR}/system" out/${KERNELNAME}
+cp -R "${AKDIR}/META-INF" out/${KERNELNAME}
+cp -R "${AKDIR}/patch" out/${KERNELNAME}
+cp -R "${AKDIR}/ramdisk" out/${KERNELNAME}
+cp -R "${AKDIR}/tools" out/${KERNELNAME}
+cp -R "${AKDIR}/anykernel.sh" out/${KERNELNAME}
+
+# Flashable zip name
+ZIPNAME=${KERNELNAME}-${today}
+
+# Toolchain location and info
+TOOLCHAIN=~/Kernels/Linaro-4.9_aarch64/bin/aarch64-linux-android-
+export ARCH=arm64
+export SUBARCH=arm64
+
+# remove backup files
+find ./ -name '*~' | xargs rm
+
+# make kernel
+make 'angler_defconfig'
+make -j`grep 'processor' /proc/cpuinfo | wc -l` CROSS_COMPILE=${TOOLCHAIN}
+
+# Grab zImage-dtb
+echo ""
+echo "<<>><<>>  Collecting Image.gz-dtb <<>><<>>"
+echo ""
+cp ${SOURCEDIR}/arch/arm64/boot/Image.gz-dtb out/${KERNELNAME}/Image.gz-dtb
+done
+
+# Build Zip
+echo "Creating ${ZIPNAME}.zip"
+cd ${SOURCEDIR}/out/${KERNELNAME}/
+7z a -tzip -mx5 "${ZIPNAME}.zip"
+
+# Remove previous kernel zip in the upload directory
+rm ${UPLOADDIR}/*.zip
+
+# Move the new one into the upload directory
+mv ${ZIPNAME}.zip ${UPLOADDIR}
+
+# Upload it
+. ~/upload.sh
+
+# Remove the out directory
+rm -rf ${SOURCEDIR}/out
+
+# Go to the home directory
+cd ~/
+
+# Success! Stop tracking time
+END=$(date +%s)
+echo "====================================="
+echo "Compilation and upload successful!"
+echo "Total time elapsed: $(echo $(($END-$START)) | awk '{print int($1/60)"mins "int($1%60)"secs"}')"
+echo "====================================="
