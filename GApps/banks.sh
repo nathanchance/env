1fbbabdac4434ed159dbaac333aa2aa083ec6b81
Nathan Chancellor <natechancellor@gmail.com>
2016-05-14 22:10:07 -0700

    Add comments to GApps scripts
    Comments save lives

--- GApps/banks.sh
+++ GApps/banks.sh
@@ -1,11 +1,18 @@
+# Change to GApps directory
 cd ~/GApps/banks_dynamic_gapps
+# Clean unsaved changed
 git reset --hard
 git clean -f -d
+# Get new changes
 git pull
+# Make GApps
 . mkgapps.sh
+# Remove current GApps and move the new ones in their place
 rm ~/shared/GApps/BaNkS_Dynamic_*.zip
 mv ~/GApps/banks_dynamic_gapps/out/BaNkS_Dynamic_*.zip ~/shared/GApps
+# Upload them
 . ~/upload.sh
+# Go home and we're done!
 cd ~/
 echo "==================================="
 echo "Compilation and upload successful!"
--- GApps/pngapps.sh
+++ GApps/pngapps.sh
@@ -1,11 +1,18 @@
+# Change to GApps directory
 cd ~/GApps/purenexus_dynamic_gapps
+# Clean unsaved changed
 git reset --hard
 git clean -f -d
+# Get new changes
 git pull
+# Make GApps
 . mkgapps.sh
+# Remove current GApps and move the new ones in their place
 rm ~/shared/GApps/PureNexus_*.zip
 mv ~/GApps/purenexus_dynamic_gapps/out/PureNexus_*.zip ~/shared/GApps
+# Upload them
 . ~/upload.sh
+# Go home and we're done!
 cd ~/
 echo "==================================="
 echo "Compilation and upload successful!"
