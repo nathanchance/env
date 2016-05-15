9ee1c27ed74b16ce4db51c785660a64c70ca7231
Nathan Chancellor <natechancellor@gmail.com>
2016-05-15 16:55:32 -0700

    Beef up source setup script

--- Misc/source_setup.sh
+++ Misc/source_setup.sh
@@ -7,13 +7,18 @@ ROMDIR=${ANDROIDDIR}/ROMs
 GAPPSDIR=${ANDROIDDIR}/GApps
 KERNELSDIR=${ANDROIDDIR}/Kernels
 
-
 if [ "${STARTOVER}" == "restart" ]
 then
-   # remove any previous directories
-   rm -rf ${ROMDIR}
-   rm -rf ${GAPPSDIR}
-   rm -rf ${KERNELSDIR}
+   rm -rf ${ANDROIDDIR}
+else
+   sudo apt-get install curl
+   curl https://raw.githubusercontent.com/akhilnarang/scripts/master/build-environment-setup.sh | bash
+   mkdir ~/bin
+   PATH=~/bin:$PATH
+   curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+   chmod a+x ~/bin/repo
+   git config --global user.name "Nathan Chancellor"
+   git config --global user.email "natechancellor@gmail.com"
 fi
 
 # Make head directories
@@ -21,6 +26,10 @@ mkdir ${ANDROIDDIR}
 mkdir ${ROMDIR}
 mkdir ${GAPPSDIR}
 mkdir ${KERNELSDIR}
+
+# Sync in scripts
+cd ${ANDROIDDIR}
+git clone https://github.com/nathanchance/scripts.git
 
 # Sync DU
 mkdir ${ROMDIR}/DU
@@ -55,6 +64,14 @@ breakfast bullhead
 breakfast hammerhead
 breakfast shamu
 
+# Sync PN-Mod
+mkdir ${ROMDIR}/PN-Mod
+cd ${ROMDIR}/PN-Mod
+repo init -u https://github.com/ezio84/pnmod-manifest.git -b mm
+repo sync --force-sync
+. build/envsetup.sh
+breakfast angler
+
 # Sync GApps
 cd ${GAPPSDIR}
 git clone https://github.com/DirtyUnicorns/banks_dynamic_gapps.git Banks
@@ -64,5 +81,3 @@ git clone https://github.com/PureNexusProject/purenexus_dynamic_gapps.git PN
 cd ${KERNELSDIR}
 git clone https://github.com/nathanchance/elite_angler.git
 git clone https://github.com/Elite-Kernels/Linaro-4.9_aarch64.git
-
-# rm -rf ~/GApps && mkdir ~/GApps && cd ~/GApps && git clone https://github.com/DirtyUnicorns/banks_dynamic_gapps.git Banks && git clone https://github.com/PureNexusProject/purenexus_dynamic_gapps.git PN
