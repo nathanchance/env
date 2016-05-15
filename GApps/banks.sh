b2dacdcb42dddeb090de417cecc54df894164181
Nathan Chancellor <natechancellor@gmail.com>
2016-05-14 23:43:18 -0400

    Add git reset and clean commands to GApps scripts

--- GApps/banks.sh
+++ GApps/banks.sh
@@ -1,10 +1,12 @@
 cd ~/GApps/banks_dynamic_gapps
+git reset --hard
+git clean -f -d
 git pull
 . mkgapps.sh
 rm ~/shared/GApps/BaNkS_Dynamic_*.zip
 mv ~/GApps/banks_dynamic_gapps/out/BaNkS_Dynamic_*.zip ~/shared/GApps
 . ~/upload.sh
 cd ~/
-echo "==============================="
+echo "==================================="
 echo "Compilation and upload successful!"
-echo "==============================="
+echo "==================================="
--- GApps/pngapps.sh
+++ GApps/pngapps.sh
@@ -1,10 +1,12 @@
 cd ~/GApps/purenexus_dynamic_gapps
+git reset --hard
+git clean -f -d
 git pull
 . mkgapps.sh
 rm ~/shared/GApps/PureNexus_*.zip
 mv ~/GApps/purenexus_dynamic_gapps/out/PureNexus_*.zip ~/shared/GApps
 . ~/upload.sh
 cd ~/
-echo "==============================="
+echo "==================================="
 echo "Compilation and upload successful!"
-echo "==============================="
+echo "==================================="
