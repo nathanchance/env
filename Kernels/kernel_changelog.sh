919f8a0db3004857b0f69e6dcd4129a0f4622d73
Nathan Chancellor <natechancellor@gmail.com>
2016-05-20 21:00:34 -0700

    Add changelog script
    Makes a changelog text file and uploads it if requested

---
+++ Kernels/kernel_changelog.sh
@@ -0,0 +1,85 @@
+#!/bin/sh
+
+# Usage:
+# $ . kernel_changelog.sh <ak|elite> <date> <upload|noupload>
+# date is in mm/dd/yyyy format
+
+# Variables:
+# KERNEL: Kernel name
+# START_DATE: The start date of the changelog
+# UPLOAD: Whether or not to upload
+# CURRENT_DATE: The end date of the changelog
+# FILE_DATE: Same as the CURRENT_DATE but formatted with _ instead of /
+# UPLOAD_DIR: The upload directory for the changelog
+# KERNEL_DIR: The directory of the source
+# KERNEL_NAME: The name of the kernel
+
+KERNEL=${1}
+START_DATE=${2}
+UPLOAD=${3}
+CURRENT_DATE=`date +"%m/%d/%Y"`
+FILE_DATE=`date +"%m_%d_%Y"`
+UPLOAD_DIR=~/shared/Kernels
+if [ ${KERNEL} == "ak" ]
+then
+   KERNEL_DIR=~/Kernels/AK-Angler
+   KERNEL_NAME=AK
+elif [ ${KERNEL} == "elite" ]
+then
+   KERNEL_DIR=~/Kernels/elite_angler
+   KERNEL_NAME=Elite
+fi
+
+# Remove previous changelog
+rm -rf ${UPLOAD_DIR}/${KERNEL_NAME}_changelog_*
+
+# Check the date start range is set
+if [ -z "$START_DATE" ]; then
+   echo ""
+   echo "ATTENTION: Start date not defined ------------------------------------------------"
+   echo ""
+   echo " >>> Please define a start date in mm/dd/yyyy format ..."
+   echo ""
+   echo "----------------------------------------------------------------------------------"
+   read START_DATE
+fi
+
+# Find the directories to log
+echo ""
+echo ""
+echo ""
+echo "${KERNEL_NAME} CHANGELOG ------------------------------------------------------------" | tr [a-z] [A-Z]
+echo ""
+find ${KERNEL_DIR} -name .git | sed 's/\/.git//g' | sed 'N;$!P;$!D;$d' | while read LINE
+do
+   cd ${LINE}
+   # Test to see if the repo needs to have a changelog written.
+   LOG=$(git log --pretty="%an - %s" --no-merges --since=${START_DATE} --date-order)
+   if [ -z "${LOG}" ]; then
+      echo " >>> Nothing updated on ${KERNEL_NAME} Kernel changelog, skipping ..."
+   else
+      # Write the changelog
+      echo " >>> Changelog is updated and written for ${KERNEL_NAME} Kernel ..."
+      echo "Project name: ${KERNEL_NAME} Kernel" >> "${UPLOAD_DIR}"/${KERNEL_NAME}_changelog_${FILE_DATE}.txt
+      echo "Dates: ${START_DATE} to ${CURRENT_DATE}" >> "${UPLOAD_DIR}"/${KERNEL_NAME}_changelog_${FILE_DATE}.txt
+      echo "${LOG}" | while read LINE
+      do
+         echo "   ${LINE}" >> "${UPLOAD_DIR}"/${KERNEL_NAME}_changelog_${FILE_DATE}.txt
+      done
+      echo "" >> "${UPLOAD_DIR}"/${KERNEL_NAME}_changelog_${FILE_DATE}.txt
+   fi
+done
+
+echo ""
+echo "-------------------------------------------------------------------------------------"
+echo ""
+echo ""
+echo ""
+
+# Upload the new changelog if requested
+if [ "${UPLOAD}" == "upload" ]
+then
+   . ~/upload.sh
+   # Go home
+   cd ~/
+fi
