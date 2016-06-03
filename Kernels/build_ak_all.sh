af940cc8e3bd391d531d5a626394555369d33771
Nathan Chancellor <natechancellor@gmail.com>
2016-06-02 23:53:41 -0700

    Add back UBER6.0 and UBER7.0 options
    Also add clearing ccache flag

--- Kernels/build_ak_all.sh
+++ Kernels/build_ak_all.sh
@@ -3,12 +3,38 @@
 # -----
 # Usage
 # -----
-# $ . build_ak_all.sh <update|noupdate>
+# $ . build_ak_all.sh <update|noupdate> <clearccache|noclearccache>
 
-
-
+if [ ${2} == "clearccache" ]
+then
+   ccache -C
+fi
 . build_ak.sh ${1} aosp4.9
+
+
+if [ ${2} == "clearccache" ]
+then
+   ccache -C
+fi
 . build_ak.sh ${1} uber4.9
+
+
+if [ ${2} == "clearccache" ]
+then
+   ccache -C
+fi
 . build_ak.sh ${1} uber5.3
-# . build_ak.sh ${1} uber6.0
-# . build_ak.sh ${1} uber7.0
+
+
+if [ ${2} == "clearccache" ]
+then
+   ccache -C
+fi
+. build_ak.sh ${1} uber6.0
+
+
+if [ ${2} == "clearccache" ]
+then
+   ccache -C
+fi
+. build_ak.sh ${1} uber7.0
