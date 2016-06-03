31b291fb5242f68b4e3571c838a109474f8081ea
Nathan Chancellor <natechancellor@gmail.com>
2016-06-03 01:11:50 -0700

    Remove option for ccache flag in AK all script
    No point to doing this, as ccache will not reuse code that was made
    with a different gcc version

--- Kernels/build_ak_all.sh
+++ Kernels/build_ak_all.sh
@@ -3,38 +3,11 @@
 # -----
 # Usage
 # -----
-# $ . build_ak_all.sh <update|noupdate> <clearccache|noclearccache>
+# $ . build_ak_all.sh <update|noupdate>
 
-if [ ${2} == "clearccache" ]
-then
-   ccache -C
-fi
+
 . build_ak.sh ${1} aosp4.9
-
-
-if [ ${2} == "clearccache" ]
-then
-   ccache -C
-fi
 . build_ak.sh ${1} uber4.9
-
-
-if [ ${2} == "clearccache" ]
-then
-   ccache -C
-fi
 . build_ak.sh ${1} uber5.3
-
-
-if [ ${2} == "clearccache" ]
-then
-   ccache -C
-fi
 . build_ak.sh ${1} uber6.0
-
-
-if [ ${2} == "clearccache" ]
-then
-   ccache -C
-fi
 . build_ak.sh ${1} uber7.0
