cf14b9589f04db6d5aa62a56cca4467e948cfd58
Nathan Chancellor <natechancellor@gmail.com>
2016-06-10 18:58:24 -0700

    Make Screw'd release script sync repo and remove at the end
    Conserves space

--- Releases/screwd_all.sh
+++ Releases/screwd_all.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 
-
+. rom_folder.sh screwd sync
 . screwd.sh angler sync
 . screwd.sh shamu sync
 . screwd.sh bullhead sync
 . screwd.sh hammerhead sync
-
+. rom_folder.sh screwd nosync
 
 
 cd ${LOGDIR}
