df22c123d35aad7e38cbdb94e691319a48c61bff
Nathan Chancellor <natechancellor@gmail.com>
2016-06-19 10:49:29 -0700

    Kylo: R31

--- Kernels/kylo.sh
+++ Kernels/kylo.sh
@@ -44,7 +44,7 @@ THREAD="-j$(grep -c ^processor /proc/cpuinfo)"
 KERNEL="Image.gz-dtb"
 DEFCONFIG="kylo_defconfig"
 BASE_KYLO_VER="Kylo"
-VER=".R30.M.angler."
+VER=".R31.M.angler."
 if [ "${TOOLCHAIN}" == "aosp" ]
 then
    TOOLCHAIN_VER="AOSP4.9"
