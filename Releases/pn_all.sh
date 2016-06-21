097eaa6679124337daad06a9f4560b3476c80ae8
Nathan Chancellor <natechancellor@gmail.com>
2016-06-20 20:27:03 -0700

    Add Pure Nexus Mod section to the PN release script

--- Releases/pn_all.sh
+++ Releases/pn_all.sh
@@ -4,14 +4,20 @@
 # -----
 # Usage
 # -----
-# $ . pn_all.sh
+# $ . pn_all.sh <mod>
 
-
-. pn.sh angler sync
-. pn.sh shamu sync
-. pn.sh bullhead sync
-. pn.sh hammerhead sync
-
+if [[ -n ${3} ]]
+then
+   . pn.sh angler sync ${3}
+   . pn.sh shamu sync ${3}
+   . pn.sh bullhead sync ${3}
+   . pn.sh hammerhead sync ${3}
+else
+   . pn.sh angler sync
+   . pn.sh shamu sync
+   . pn.sh bullhead sync
+   . pn.sh hammerhead sync
+fi
 
 cat ${COMPILE_LOG}
 cd ${HOME}
