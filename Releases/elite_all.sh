69ecb00c384e410c55c16ebdd9175a0cfd0060c9
Nathan Chancellor <natechancellor@gmail.com>
2016-07-04 14:17:00 -0700

    Fix Elite release script
    We no longer use update as the second parameter

--- Releases/elite_all.sh
+++ Releases/elite_all.sh
@@ -4,7 +4,7 @@
 # -----
 # Usage
 # -----
-# $ . elite_all.sh <tcupdate|notcupdate> <update|noupdate> <exp>
+# $ . elite_all.sh <tcupdate|notcupdate> <exp>
 # Parameter 1: Update the toolchains used to compile by running the update_toolchains script
 # Parameter 2: Experimental build (leave off if you want a release build)
 
@@ -14,20 +14,20 @@ then
    . sync_toolchains.sh
 fi
 # If the third parameter exists
-if [[ -n ${3} ]]
+if [[ -n ${2} ]]
 then
-   . elite.sh update aosp ${3}
-   . elite.sh update uber4 ${3}
-   . elite.sh update uber5 ${3}
-   . elite.sh update uber6 ${3}
-   . elite.sh update uber7 ${3}
-   . elite.sh update linaro4.9 ${3}
-   . elite.sh update linaro5.4 ${3}
-   . elite.sh update linaro6.1 ${3}
+   . elite.sh aosp ${3}
+   . elite.sh uber4 ${3}
+   . elite.sh uber5 ${3}
+   . elite.sh uber6 ${3}
+   . elite.sh uber7 ${3}
+   . elite.sh linaro4.9 ${3}
+   . elite.sh linaro5.4 ${3}
+   . elite.sh linaro6.1 ${3}
 else
-   . elite.sh update linaro4.9
-   . elite.sh update aosp
-   . elite.sh update uber4
+   . elite.sh linaro4.9
+   . elite.sh aosp
+   . elite.sh uber4
 fi
 
 
