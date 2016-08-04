0847800dbf0cfc65bd2afa7eab306fe95c0c4fb8
Nathan Chancellor <natechancellor@gmail.com>
2016-08-04 00:13:47 -0700

    Add an ALL script
    We running nightlies now so let’s just type . all.sh and be done with
    it :P

---
+++ all.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+#########
+# GAPPS #
+#########
+. gapps.sh banks
+. gapps.sh pn
+
+
+########
+# ROMS #
+########
+# PureNexus (Angler, Shamu, Bullhead, Hammerhead, Flo, Deb, and Flounder)
+. rom.sh release pn
+# PureNexus Mod (Angler, Shamu, Bullhead, and Hammerhead)
+. rom.sh normal pn mod
+# Dirty Unicorns (Angler, Shamu, Bullhead, Hammerhead, and Mako)
+. rom.sh normal du
+. rom.sh du mako
+# ResurrectionRemix (Shamu)
+. rom.sh rr
+# AOSiP (Angler, Shamu, and Bullhead)
+. rom.sh aosip angler
+. rom.sh aosip shamu
+. rom.sh aosip bullhead
+
+# Exit the session
+exit
