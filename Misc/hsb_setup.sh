5de3d992cfe751472d78cda92f3a676ff07b5584
Nathan Chancellor <natechancellor@gmail.com>
2016-08-22 13:29:48 -0700

    Update HSB setup script

--- Misc/hsb_setup.sh
+++ Misc/hsb_setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+
+#################
+##  FUNCTIONS  ##
+#################
+
 # Remove, make, and move into function
+# Parameter 1: the location of the folder you want to remake
 function rm_mk_cd() {
    rm -rf ${1}
    mkdir -p ${1}
@@ -8,27 +14,41 @@ function rm_mk_cd() {
 }
 
 # Repo init and repo sync function
+# Parameter 1: URL of the manifest
+# Parameter 2: Branch of the manifest
 function init_sync() {
    repo init -u ${1} -b ${2}
    time repo sync --force-sync -j$(grep -c ^processor /proc/cpuinfo)
 }
 
 # Repo init with reference and repo sync function
+# Parameter 1: Abbreviation of thing being synced (possible options are: pn, pn-mod, rr, aosip, du, beltz, or TWRP)
+# Parameter 2: URL of the manifest
+# Parameter 3: Branch of the manifest
 function init_sync_ref() {
    repo init -u ${2} -b ${3} --reference=${HOME}/ROMs/PN
-   if [[ "${1}" == "rr" ]]; then
+
+   if [[ "${1}" == "rr" || "${1}" == "pn-mod" ]]; then
+      case "${1}" in
+         "rr")
+            LM_URL=https://raw.githubusercontent.com/nathanchance/local_manifests/master/rr_shamu.xml ;;
+         "pn-mod")
+            LM_URL=https://raw.githubusercontent.com/nathanchance/local_manifests/master/pn-mod.xml ;;
+      esac
+
       rm_mk_cd .repo/local_manifests
-      wget https://raw.githubusercontent.com/nathanchance/local_manifests/master/rr_shamu.xml
-   elif [[ "${1}" == "pn-mod" ]]; then
-      rm_mk_cd .repo/local_manifests
-      wget https://raw.githubusercontent.com/nathanchance/local_manifests/master/pn-mod.xml
+      wget ${LM_URL}
+      cd ../..
    fi
+
    time repo sync --force-sync -j$(grep -c ^processor /proc/cpuinfo)
 }
 
 # Dependencies function
+# Parameter 1: Abbreviation of thing we are syncing dependencies for (possible options are: pn, pn-mod, rr, aosip, du, beltz, or TWRP)
+# Parameter 2: Devices to sync dependencies for
 function dependencies() {
-   case ${1} in
+   case "${2}" in
       "mako")
          DEVICES="angler shamu bullhead hammerhead mako" ;;
       "no-mako")
@@ -40,15 +60,9 @@ function dependencies() {
    . build/envsetup.sh
 
    for DEVICE in ${DEVICES}; do
-      case ${2} in
+      case "${1}" in
          "twrp")
-            lunch omni_${DEVICE}-eng
-            if [[ ${DEVICE} == "shamu" ]]; then
-               cd device/moto/shamu
-               git fetch ssh://nathanchance@gerrit.omnirom.org:29418/android_device_moto_shamu refs/changes/74/18874/1 && git cherry-pick FETCH_HEAD
-               cd ../../..
-               lunch omni_${DEVICE}-eng
-            fi ;;
+            lunch omni_${DEVICE}-eng ;;
          "beltz")
             lunch androidx_${DEVICE}-userdebug ;;
          *)
@@ -58,50 +72,65 @@ function dependencies() {
 }
 
 
-# GApps
+#############
+##  GApps  ##
+#############
+# Remove all previous GApps
 rm_mk_cd ${HOME}/GApps
+# Banks
 git clone https://github.com/MrBaNkS/banks_dynamic_gapps.git Banks
-git clone https://github.com/beanstown106/purenexus_dynamic_gapps.git PN
 
 
-# Kernels
+###############
+##  Kernels  ##
+###############
+# Remove all previous kernels
 rm_mk_cd ${HOME}/Kernels
+# Ninja
 git clone https://github.com/nathanchance/Ninja-Kernel.git Ninja
+# AK
 rm_mk_cd ${HOME}/Kernels/AK
 git clone https://github.com/nathanchance/AK-Angler.git Kernel
 git clone https://github.com/anarkia1976/AK-Angler-AnyKernel2.git AK2
+# Toolchains
 source sync_toolchains.sh
 
 
-# ROMs
+############
+##  ROMs  ##
+############
+# Remove all previous ROMs
 rm_mk_cd ${HOME}/ROMs
-
+# PureNexus - full sync - angler, shamu, bullhead, and hammmerhead
 rm_mk_cd ${HOME}/ROMs/PN
 init_sync https://github.com/PureNexusProject-Legacy/manifest.git mm2
-dependencies no-mako pn
-
+dependencies pn no-mako
+# PureNexus Mod - reference sync - angler, shamu, bullhead, and hammmerhead
 rm_mk_cd ${HOME}/ROMs/PN-Mod
 init_sync_ref pn-mod https://github.com/ezio84/pnmod-manifest.git mm2oms
-dependencies no-mako pn-mod
-
+dependencies pn-mod no-mako
+# Dirty Unicorns - reference sync - angler, shamu, bullhead, hammmerhead, and mako
 rm_mk_cd ${HOME}/ROMs/DU
 init_sync_ref du https://github.com/DirtyUnicorns/android_manifest.git m
-dependencies mako du
-
+dependencies du mako
+# AOSiP - reference sync - angler, shamu, bullhead, hammmerhead, and mako
 rm_mk_cd ${HOME}/ROMs/AOSiP
 init_sync_ref aosip git://github.com/AOSIP/platform_manifest.git oms
-dependencies mako aosip
-
+dependencies aosip mako
+# Beltz - reference sync - angler, shamu, bullhead, and hammmerhead
 rm_mk_cd ${HOME}/ROMs/Beltz
 init_sync_ref beltz git://github.com/beltz/platform_manifest.git beltz
-dependencies no-mako beltz
-
+dependencies beltz no-mako
+# ResurrectionRemix - reference sync - shamu
 rm_mk_cd ${HOME}/ROMs/RR
 init_sync_ref rr https://github.com/ResurrectionRemix/platform_manifest.git marshmallow
-dependencies shamu rr
+dependencies rr shamu
 
 
-# TWRP
+############
+##  TWRP  ##
+############
+# reference sync - angler, shamu, bullhead, hammmerhead, and mako
 rm_mk_cd ${HOME}/TWRP
 init_sync_ref twrp git://github.com/lj50036/platform_manifest_twrp_omni.git twrp-6.0
-dependencies mako twrp
+dependencies twrp mako
