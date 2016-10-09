bd89cde20ff24691aead17878033933424fcb8a7
Nathan Chancellor <natechancellor@gmail.com>
2016-10-08 18:10:26 -0700

    TWRP: overhaul script
    1. Let's do parameters smart (allow them to be put in any order and check to make sure they aren't blank)
    2. Add moar comments!
    3. Clean up some logic here and there
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- twrp.sh
+++ twrp.sh
@@ -19,189 +19,266 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 
-# Prints a formatted header; used for outlining what the script is doing to the user
-function echoText() {
-   RED="\033[01;31m"
-   RST="\033[0m"
+###########
+#         #
+#  USAGE  #
+#         #
+###########
 
+# $ twrp.sh <device>
+
+
+############
+#          #
+#  COLORS  #
+#          #
+############
+
+RED="\033[01;31m"
+BLINK_RED="\033[05;31m"
+RESTORE="\033[0m"
+
+
+###############
+#             #
+#  FUNCTIONS  #
+#             #
+###############
+
+# PRINTS A FORMATTED HEADER TO POINT OUT WHAT IS BEING DONE TO THE USER
+function echoText() {
    echo -e ${RED}
    echo -e "====$( for i in $( seq ${#1} ); do echo -e "=\c"; done )===="
    echo -e "==  ${1}  =="
    echo -e "====$( for i in $( seq ${#1} ); do echo -e "=\c"; done )===="
-   echo -e ${RST}
+   echo -e ${RESTORE}
 }
 
 
-# Creates a new line
+# CREATES A NEW LINE IN TERMINAL
 function newLine() {
    echo -e ""
 }
 
 
-# Compiliation function
-function compile() {
-   # Parameters
-   DEVICE=${1}
+################
+#              #
+#  PARAMETERS  #
+#              #
+################
 
-   # Flags
-   SUCCESS=false
+# UNASSIGN FLAGS AND RESET ROM_BUILD_TYPE
+SUCCESS=false
 
-   # Directories
-   SOURCE_DIR=${HOME}/TWRP
-   OUT_DIR=${SOURCE_DIR}/out/target/product/${DEVICE}
-   ZIP_MOVE=${HOME}/Web/.hidden/TWRP
+while [[ $# -ge 1 ]]; do
+   case "${1}" in
+      "shamu"|"angler"|"bullhead"|"hammerhead")
+         DEVICE=${1} ;;
+      *)
+         echo "Invalid parameter detected!" && exit ;;
+   esac
 
+   shift
+done
 
-   # File names
-   COMP_FILE=recovery.img
-   UPLD_FILE=twrp-${DEVICE}-$( TZ=MST date +%m%d%Y ).img
-   FILE_FORMAT=twrp-${DEVICE}*
-
-
-   # Export Java 8
-   export EXPERIMENTAL_USE_JAVA8=true
-
-
-   # Clear the terminal
-   clear
-
-
-   # Start tracking time
-   echoText "SCRIPT STARTING AT $( TZ=MST date +%D\ %r )"
-
-   START=$( TZ=MST date +%s )
-
-
-   # Change to the source directory
-   echoText "MOVING TO SOURCE DIRECTORY"
-
-   cd ${SOURCE_DIR}
-
-
-   # Start syncing the latest sources
-   echoText "SYNCING LATEST SOURCES"; newLine
-
-   repo sync --force-sync -j$(grep -c ^processor /proc/cpuinfo)
-
-
-   # Setup the build environment
-   echoText "SETTING UP BUILD ENVIRONMENT"; newLine
-
-   . build/envsetup.sh
-
-
-   # Prepare device
-   newLine; echoText "PREPARING $( echo ${DEVICE} | awk '{print toupper($0)}' )"
-
-   lunch omni_${DEVICE}-eng
-
-
-   # Clean up
-   echoText "CLEANING UP OUT DIRECTORY"; newLine
-
-   mka clobber
-
-
-   # Start building
-   newLine; echoText "MAKING TWRP"; newLine
-   NOW=$( TZ=MST date +"%Y-%m-%d-%S" )
-   time mka recoveryimage 2>&1 | tee ${LOGDIR}/Compilation/twrp_${DEVICE}-${NOW}.log
-
-
-   # If the compilation was successful, there will be a file in the format above in the out directory
-   if [[ $( ls ${OUT_DIR}/${COMP_FILE} 2>/dev/null | wc -l ) != "0" ]]; then
-      # Make the build result string show success
-      BUILD_RESULT_STRING="BUILD SUCCESSFUL"
-      SUCCESS=true
-
-
-
-      # If the upload directory doesn't exist, make it; otherwise, remove existing files in ZIP_MOVE
-      if [[ ! -d "${ZIP_MOVE}" ]]; then
-         newLine; echoText "MAKING UPLOAD DIRECTORY"
-
-         mkdir -p "${ZIP_MOVE}"
-      else
-         newLine; echoText "CLEANING UPLOAD DIRECTORY"; newLine
-
-         rm -vrf "${ZIP_MOVE}"/*${FILE_FORMAT}*
-      fi
-
-
-      # Copy new files to ZIP_MOVE
-      newLine; echoText "MOVING FILE TO UPLOAD DIRECTORY"; newLine
-
-      mv -v ${OUT_DIR}/${COMP_FILE} "${ZIP_MOVE}"/${UPLD_FILE}
-
-
-      # Upload the files
-      # newLine; echoText "UPLOADING FILE"; newLine
-
-
-
-      # Clean up out directory to free up space
-      newLine; echoText "CLEANING UP OUT DIRECTORY"; newLine
-
-      mka clobber
-
-
-      # Go back home
-      newLine; echoText "GOING HOME"
-
-      cd ${HOME}
-
-   # If the build failed, add a variable
-   else
-      BUILD_RESULT_STRING="BUILD FAILED"
-      SUCCESS=false
-   fi
-
-
-
-   # Stop tracking time
-   END=$( TZ=MST date +%s )
-   newLine; echoText "${BUILD_RESULT_STRING}!"
-
-   # Print the image location and its size if the script was successful
-   if [[ ${SUCCESS} = true ]]; then
-      echo -e ${RED}"FILE LOCATION: $( ls "${ZIP_MOVE}"/${UPLD_FILE} )"
-      echo -e "SIZE: $( du -h "${ZIP_MOVE}"/${UPLD_FILE} | awk '{print $1}' )"${RST}
-   fi
-
-   # Print the time the script finished and how long the script ran for regardless of success
-   echo -e ${RED}"TIME FINISHED: $( TZ=MST date +%D\ %r | awk '{print toupper($0)}' )"
-   echo -e ${RED}"DURATION: $( echo $((${END}-${START})) | awk '{print int($1/60)" MINUTES AND "int($1%60)" SECONDS"}' )"${RST}; newLine
-
-
-
-   # Add line to compile log in the following format:
-   # DATE: BASH_SOURCE (PARAMETERS)
-   echo -e "\n$( TZ=MST date +%H:%M:%S ): ${BASH_SOURCE} ${DEVICE}" >> ${LOG}
-
-   # BUILD <SUCCESSFUL|FAILED> IN # MINUTES AND # SECONDS
-   echo -e "${BUILD_RESULT_STRING} IN $( echo $((${END}-${START})) | awk '{print int($1/60)" MINUTES AND "int($1%60)" SECONDS"}' )" >> ${LOG}
-
-   # Only add a line about file location if script completed succesfully
-   if [[ ${SUCCESS} = true ]]; then
-      # FILE LOCATION: <PATH>
-      echo -e "FILE LOCATION: $( ls "${ZIP_MOVE}"/${UPLD_FILE} )" >> ${LOG}
-   fi
-
-   # Notify the user that their script is done
-   echo -e "\a"
-}
-
-
-# If the first parameter to the twrp.sh script is "all", we are running five builds for the devices we support; otherwise, it is just a device specific build
-if [[ "${1}" == "all" ]]; then
-   DEVICES="angler shamu bullhead hammerhead mako"
-
-   for DEVICE in ${DEVICES}; do
-      compile ${DEVICE}
-   done
-
-   cd ${HOME}
-   cat ${LOG}
-else
-   compile ${1}
+# PARAMETER VERIFICATION
+if [[ -z ${DEVICE} ]]; then
+   echo "You did not specify a necessary parameter (the device to compile for). Please re-run the script with the necessary parameters!" && exit
 fi
+
+
+###############
+#             #
+#  VARIABLES  #
+#             #
+###############
+
+# DIRECTORIES
+SOURCE_DIR=${HOME}/ROMs/Omni
+OUT_DIR=${SOURCE_DIR}/out/target/product/${DEVICE}
+IMG_MOVE=${HOME}/Web/.hidden/TWRP
+
+# FILE NAMES
+COMP_FILE=recovery.img
+UPLD_FILE=twrp-${DEVICE}-$( TZ=MST date +%m%d%Y ).img
+FILE_FORMAT=twrp-${DEVICE}*
+
+
+################
+# START SCRIPT #
+################
+
+clear
+
+# EXPORT JAVA8
+export EXPERIMENTAL_USE_JAVA8=true
+
+
+#######################
+# START TRACKING TIME #
+#######################
+
+echoText "SCRIPT STARTING AT $( TZ=MST date +%D\ %r )"
+
+START=$( TZ=MST date +%s )
+
+
+###########################
+# MOVE INTO SOURCE FOLDER #
+###########################
+
+echoText "MOVING TO SOURCE DIRECTORY"
+
+cd ${SOURCE_DIR}
+
+
+#############
+# REPO SYNC #
+#############
+
+echoText "SYNCING LATEST SOURCES"; newLine
+
+repo sync --force-sync -j$(grep -c ^processor /proc/cpuinfo)
+
+
+###########################
+# SETUP BUILD ENVIRONMENT #
+###########################
+
+echoText "SETTING UP BUILD ENVIRONMENT"; newLine
+
+# CHECK AND SEE IF WE ARE ON ARCH; IF SO, ACTIVARE A VIRTUAL ENVIRONMENT FOR PROPER PYTHON SUPPORT
+if [[ -f /etc/arch-release ]]; then
+   virtualenv2 venv
+   source venv/bin/activate
+fi
+
+source build/envsetup.sh
+
+
+##################
+# PREPARE DEVICE #
+##################
+
+newLine; echoText "PREPARING $( echo ${DEVICE} | awk '{print toupper($0)}' )"
+
+lunch omni_${DEVICE}-eng
+
+
+############
+# CLEAN UP #
+############
+
+echoText "CLEANING UP OUT DIRECTORY"; newLine
+
+mka clobber
+
+
+##################
+# START BUILDING #
+##################
+
+newLine; echoText "MAKING TWRP"; newLine
+NOW=$( TZ=MST date +"%Y-%m-%d-%S" )
+time mka recoveryimage 2>&1 | tee ${LOGDIR}/Compilation/twrp_${DEVICE}-${NOW}.log
+
+
+####################
+# IF TWRP COMPILED #
+####################
+
+# THERE WILL BE A FILE IN THE OUT FOLDER IN THE ABOVE FORMAT
+if [[ $( ls ${OUT_DIR}/${COMP_FILE} 2>/dev/null | wc -l ) != "0" ]]; then
+  # MAKE BUILD RESULT STRING REFLECT SUCCESSFUL COMPILATION
+  BUILD_RESULT_STRING="BUILD SUCCESSFUL"
+  SUCCESS=true
+
+
+  ##################
+  # IMG_MOVE LOGIC #
+  ##################
+
+  # MAKE IMG_MOVE IF IT DOESN'T EXIST OR CLEAN IT IF IT DOES
+  if [[ ! -d "${IMG_MOVE}" ]]; then
+     newLine; echoText "MAKING UPLOAD DIRECTORY"
+
+     mkdir -p "${IMG_MOVE}"
+  else
+     newLine; echoText "CLEANING UPLOAD DIRECTORY"; newLine
+
+     rm -vrf "${IMG_MOVE}"/*${FILE_FORMAT}*
+  fi
+
+
+  ####################
+  # MOVING TWRP FILE #
+  ####################
+
+  newLine; echoText "MOVING FILE TO UPLOAD DIRECTORY"; newLine
+
+  mv -v ${OUT_DIR}/${COMP_FILE} "${IMG_MOVE}"/${UPLD_FILE}
+
+
+###################
+# IF BUILD FAILED #
+###################
+
+else
+  BUILD_RESULT_STRING="BUILD FAILED"
+  SUCCESS=false
+fi
+
+
+
+##############
+# SCRIPT END #
+##############
+
+# DEACTIVATE VIRTUALENV IF WE ARE ON ARCH
+if [[ -f /etc/arch-release ]]; then
+   echoText "EXITING VIRTUAL ENV"
+   deactivate
+fi
+
+END=$( TZ=MST date +%s )
+newLine; echoText "${BUILD_RESULT_STRING}!"
+
+
+######################
+# ENDING INFORMATION #
+######################
+
+# IF THE BUILD WAS SUCCESSFUL, PRINT FILE LOCATION AND SIZE
+if [[ ${SUCCESS} = true ]]; then
+  echo -e ${RED}"FILE LOCATION: $( ls "${IMG_MOVE}"/${UPLD_FILE} )"
+  echo -e "SIZE: $( du -h "${IMG_MOVE}"/${UPLD_FILE} | awk '{print $1}' )"${RESTORE}
+fi
+
+# PRINT THE TIME THE SCRIPT FINISHED
+# AND HOW LONG IT TOOK REGARDLESS OF SUCCESS
+echo -e ${RED}"TIME FINISHED: $( TZ=MST date +%D\ %r | awk '{print toupper($0)}' )"
+echo -e ${RED}"DURATION: $( echo $((${END}-${START})) | awk '{print int($1/60)" MINUTES AND "int($1%60)" SECONDS"}' )"${RESTORE}; newLine
+
+
+##################
+# LOG GENERATION #
+##################
+
+# DATE: BASH_SOURCE (PARAMETERS)
+echo -e "\n$( TZ=MST date +%H:%M:%S ): ${BASH_SOURCE} ${DEVICE}" >> ${LOG}
+
+# BUILD <SUCCESSFUL|FAILED> IN # MINUTES AND # SECONDS
+echo -e "${BUILD_RESULT_STRING} IN $( echo $((${END}-${START})) | awk '{print int($1/60)" MINUTES AND "int($1%60)" SECONDS"}' )" >> ${LOG}
+
+# ONLY ADD A LINE ABOUT FILE LOCATION IF SCRIPT COMPLETED SUCCESSFULLY
+if [[ ${SUCCESS} = true ]]; then
+  # FILE LOCATION: <PATH>
+  echo -e "FILE LOCATION: $( ls "${IMG_MOVE}"/${UPLD_FILE} )" >> ${LOG}
+fi
+
+
+########################
+# ALERT FOR SCRIPT END #
+########################
+
+echo -e "\a" && cd ${HOME}
