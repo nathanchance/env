40ed8649df0cc76bd3124a703d48d84c7458fc34
Nathan Chancellor <natechancellor@gmail.com>
2016-12-11 01:21:04 -0700

    Add Substratum compilation script
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

---
+++ substratum.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+#
+# Substratum compilation and upload script
+#
+# Copyright (C) 2016 Nathan Chancellor
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+# COLORS
+RED="\033[01;31m"
+RESTORE="\033[0m"
+
+# PRINTS A FORMATTED HEADER TO POINT OUT WHAT IS BEING DONE TO THE USER
+function echoText() {
+   echo -e ${RED}
+   echo -e "====$( for i in $( seq ${#1} ); do echo -e "=\c"; done )===="
+   echo -e "==  ${1}  =="
+   echo -e "====$( for i in $( seq ${#1} ); do echo -e "=\c"; done )===="
+   echo -e ${RESTORE}
+}
+
+
+# CREATES A NEW LINE IN TERMINAL
+function newLine() {
+   echo -e ""
+}
+
+# START TIME TRACKING
+START=$( TZ=MST date +%s )
+
+# SET VARIABLES
+SOURCE_DIR=${HOME}/Repos/substratum
+OUT_DIR=${SOURCE_DIR}/app/build/outputs/apk
+APK_FORMAT=substratum*.apk
+APK_MOVE=${HOME}/Web/.superhidden/APKs
+
+# UNSET JAVA_HOME SO GRADLE CAN PROPERLY SET IT
+unset JAVA_HOME
+
+# GET CURRENT DIR FOR later
+CURRENT_DIR=$( pwd )
+
+# UPDATE REPO
+cd ${SOURCE_DIR}
+git pull upstream dev --rebase && git push
+
+# CLEAN PREVIOUS BUILD
+./gradlew clean
+
+# MAKE NEW APK
+./gradlew assembleDebug
+
+# IF THE APK WAS FOUND, MOVE IT
+if [[ $( ls ${OUT_DIR}/${APK_FORMAT} 2>/dev/null | wc -l ) != "0" ]]; then
+   RESULT_STRING="BUILD SUCCESSFUL"
+   if [[ -d ${APK_MOVE} ]]; then
+      rm -rf ${APK_MOVE}/${APK_FORMAT}
+   else
+      mkdir -p ${APK_MOVE}
+   fi
+   cp -v ${OUT_DIR}/${APK_FORMAT} ${APK_MOVE}
+
+# OTHERWISE, REPORT IT!
+else
+   RESULT_STRING="BUILD FAILED"
+fi
+
+# END TIME TRACKING
+END=$( TZ=MST date +%s )
+
+# PRINT RESULT
+# IF THE BUILD WAS SUCCESSFUL, PRINT FILE LOCATION AND SIZE
+newLine; echoText "${RESULT_STRING}!"
+if [[ ${RESULT_STRING} == "BUILD SUCCESSFUL" ]]; then
+  echo -e ${RED}"FILE LOCATION: $( ls "${APK_MOVE}"/${APK_FORMAT} )"
+  echo -e "SIZE: $( du -h "${APK_MOVE}"/${APK_FORMAT} | awk '{print $1}' )"${RESTORE}
+fi
+
+# PRINT HOW LONG IT TOOK REGARDLESS OF SUCCESS
+echo -e ${RED}"DURATION: $( echo $((${END}-${START})) | awk '{print int($1/60)" MINUTES AND "int($1%60)" SECONDS"}' )"${RESTORE}; newLine
+
+# MOVE BACK TO ORIGINAL DIRECTORY
+cd ${CURRENT_DIR}
