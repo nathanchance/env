13e50f9dd113ce9887eef51c47b08801f7245805
Nathan Chancellor <natechancellor@gmail.com>
2019-06-05 10:40:28 -0700

    server/zshrc.sh: Remove blank line
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- server/zshrc.sh
+++ server/zshrc.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env zsh
 
 {
-    echo
     echo "[[ -f \${HOME}/scripts/env/common ]] && source \"\${HOME}/scripts/env/common\""
     echo "type shell_setup &>/dev/null && shell_setup"
 } >> ~/.zshrc
