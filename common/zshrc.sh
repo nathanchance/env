f259f38307e1fb45622c939a5eceae139d0d3837
Nathan Chancellor <natechancellor@gmail.com>
2019-06-10 17:37:20 -0700

    common: Remove small setup scripts
    They have been moved into the scripts repo
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- common/zshrc.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-
-{
-    echo "[[ -f \${HOME}/scripts/env/common ]] && source \"\${HOME}/scripts/env/common\""
-    echo "type shell_setup &>/dev/null && shell_setup"
-} >> ~/.zshrc
