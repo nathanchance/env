2ba65eda5d7822151844c5e862cb432da8f7a0be
Nathan Chancellor <natechancellor@gmail.com>
2020-01-15 18:15:43 -0700

    windows: Update scripts
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- windows/reboot-wsl.ps1
+++ windows/reboot-wsl.ps1
@@ -12,18 +12,6 @@ else {
 # Shutdown the VM
 wsl --shutdown
 
-# Local paths/info
-$ssh_key = 'C:\Users\natec\Linux\id_ed25519'
-$server_ip = Get-Content 'C:\Users\natec\Linux\.server_ip'
-$wslkernel_destination = 'C:\Users\natec\Linux\kernel-4.19-clang'
-
-# Remote path
-$wslkernel_download = '/home/nathan/src/WSL2-Linux-Kernel/out.x86_64/arch/x86/boot/bzImage'
-
-# Download the new kernel
-Remove-Item -ErrorAction Ignore $wslkernel_destination
-scp -i $ssh_key nathan@$server_ip`:$wslkernel_download $wslkernel_destination
-
 # Start up the VM and print the version
 Start-Sleep -Seconds $sleeptime
 wsl -d Debian -- /usr/bin/batcat /proc/version
