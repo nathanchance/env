533a6a36e41242cb0a8071d060078b68c3975b46
Nathan Chancellor <natechancellor@gmail.com>
2020-04-22 16:21:22 -0700

    configs/local: Add snazzy theme for Blink
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

---
+++ configs/local/snazzy.js
@@ -0,0 +1,4 @@
+t.prefs_.set('background-color', "#282a36");
+t.prefs_.set('foreground-color', "#eff0eb");
+t.prefs_.set('cursor-color', "#eff0eb");
+t.prefs_.set('color-palette-overrides', ["#282a36","#ff5c57","#5af78e","#f3f99d","#57c7ff","#ff6ac1","#9aedfe","#f1f1f0","#686868","#ff5c57","#5af78e","#f3f99d","#57c7ff","#ff6ac1","#9aedfe","#eff0eb"]);
