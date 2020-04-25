b1a34e518e9bd5debdee2a83830741ae93d64273
Nathan Chancellor <natechancellor@gmail.com>
2020-04-24 19:40:55 -0700

    scripts: Add GitHub Actions workflow
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

---
+++ ci/check.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+ACTIONS=()
+while ((${#})); do
+    case ${1} in
+        shellcheck | shfmt) ACTIONS=("${1}") ;;
+    esac
+    shift
+done
+[[ -z ${ACTIONS[*]} ]] && ACTIONS=(shellcheck shfmt)
+
+for ACTION in "${ACTIONS[@]}"; do
+    case ${ACTION} in
+        shellcheck) fd -t x -E windows -x shellcheck ;;
+        shfmt) fd -t x -E windows -x shfmt -ci -d -i 4 ;;
+    esac
+done
