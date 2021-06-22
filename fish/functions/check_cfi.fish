06f083e69ef248ee5e2ec2932d901819ddb0903c
Nathan Chancellor <nathan@kernel.org>
2021-06-22 15:15:07 -0700

    fish/functions: Add check_cfi
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ fish/functions/check_cfi.fish
@@ -0,0 +1,17 @@
+#!/usr/bin/env fish
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2021 Nathan Chancellor
+
+function check_cfi -d "Run LTP's read_all testcase and check for CFI failures"
+    cbl_clone boot-utils
+
+    set read_all $CBL_GIT/boot-utils/debian/ltp/testcases/kernel/fs/read_all/read_all
+
+    if not test -x $read_all
+        $CBL_GIT/boot-utils/debian/ltp.sh; or return
+    end
+
+    sudo true; or return
+
+    sudo sh -c "dmesg -C && $read_all -d /proc && $read_all -d /sys && dmesg -l warn"
+end
