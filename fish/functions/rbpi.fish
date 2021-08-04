91656b63a3e2acf0ca490060c68b58a44387459f
Nathan Chancellor <nathan@kernel.org>
2021-08-04 13:40:01 -0700

    env: Update for next-20210804
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/rbpi.fish
+++ fish/functions/rbpi.fish
@@ -14,7 +14,7 @@ function rbpi -d "Rebase Raspberry Pi kernel on latest linux-next"
     git rh origin/master
 
     set -a patches 20210803200836.500658-1-nathan@kernel.org # [PATCH] PCI: Always initialize dev in pciconfig_read
-    set -a patches 20210803150904.80119-1-colin.king@canonical.com # [PATCH][next] brcmfmac: firmware: Fix uninitialized variable ret
+    set -a patches 20210804153434.3429348-1-linus.walleij@linaro.org # [PATCH v2] brcmfmac: firmware: Fix firmware loading
     for patch in $patches
         git b4 ams -P _ $patch; or return
     end
