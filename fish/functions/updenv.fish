2e0d7424aff593e22f99e536bcb95228157cb253
Nathan Chancellor <nathan@kernel.org>
2021-08-15 11:20:01 -0700

    fish/functions: Add updenv
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ fish/functions/updenv.fish
@@ -0,0 +1,8 @@
+#!/usr/bin/env fish
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2021 Nathan Chancellor
+
+function updenv -d "Pull updates from env repo and load them into environment"
+    git -C $ENV_FOLDER pull -qr; or return
+    rld
+end
