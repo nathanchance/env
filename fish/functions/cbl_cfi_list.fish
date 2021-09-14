a6909e76a98ea48f9bb8d7703b2d4b9e517e85d2
Nathan Chancellor <nathan@kernel.org>
2021-09-14 12:34:06 -0700

    pkgbuilds: Update for next-20210914
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_cfi_list.fish
+++ fish/functions/cbl_cfi_list.fish
@@ -4,8 +4,6 @@
 
 function cbl_cfi_list -d "Print 'git rebase --interactive' patch list for linux-next CFI tree"
     echo "
-exec git am $ENV_FOLDER/pkgbuilds/linux-next-llvm/0001-IB-qib-Fix-type-check-in-QIB_DIAGC_ATTR.patch
-
 exec git pll --no-edit sami clang-cfi
 "
 end
