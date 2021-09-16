f0564f08cc12f76a3e558b8adb002d2f19696193
Nathan Chancellor <nathan@kernel.org>
2021-09-16 13:28:16 -0700

    env: Update for next-20210916
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_cfi_list.fish
+++ fish/functions/cbl_cfi_list.fish
@@ -4,6 +4,9 @@
 
 function cbl_cfi_list -d "Print 'git rebase --interactive' patch list for linux-next CFI tree"
     echo "
+# [PATCH] ptp: ocp: Avoid operator precedence warning in ptp_ocp_summary_show()
+exec git b4 am https://lore.kernel.org/netdev/20210916194351.3860836-1-nathan@kernel.org/
+
 exec git pll --no-edit sami clang-cfi
 "
 end
