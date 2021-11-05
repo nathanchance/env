cc8e3a490653d884063b2c335257efdd0063b93b
Nathan Chancellor <nathan@kernel.org>
2021-11-05 09:58:49 -0700

    env: Update for next-20211105
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_cfi_list.fish
+++ fish/functions/cbl_cfi_list.fish
@@ -7,6 +7,12 @@ function cbl_cfi_list -d "Print 'git rebase --interactive' patch list for linux-
 # drm/i915: Avoid bitwise vs logical OR warning in snb_wm_latency_quirk()
 x fish -c \"curl -LSs 'https://cgit.freedesktop.org/drm/drm-intel/patch/?id=2e70570656adfe1c5d9a29940faa348d5f132199' | git am\"
 
-x fish -c 'git ru; and git ml --no-edit sami/clang-cfi^'
+# [PATCH v2] usb: dwc2: hcd_queue: Fix use of floating point literal
+x b4 -q shazam -l -s https://lore.kernel.org/r/20211105145802.2520658-1-nathan@kernel.org/
+
+# [PATCH] NFS: Don't trace an uninitialised value
+x b4 -q shazam -l -s https://lore.kernel.org/r/20211105163852.214665-1-trondmy@kernel.org/
+
+x git ml --no-edit sami/clang-cfi^
 "
 end
