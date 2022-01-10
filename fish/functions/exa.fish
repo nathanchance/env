cd10cab608e4d120af5f8f738dd78750e0e74c84
Nathan Chancellor <nathan@kernel.org>
2022-01-10 13:31:02 -0700

    fish: exa: Remove warning
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/exa.fish
+++ fish/functions/exa.fish
@@ -8,9 +8,6 @@ function exa -d "Runs exa through the system or podman depending on how it is av
     else if test -x $BIN_FOLDER/exa
         $BIN_FOLDER/exa $argv
     else
-        if test -z "$container"
-            print_warning "exa could not be found. Run 'upd exa' to install it. Falling back to 'ls'..."
-        end
-        command ls $argv
+        command ls --color=auto $argv
     end
 end
