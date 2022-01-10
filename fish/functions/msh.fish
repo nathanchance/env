abe01d235f0fb63744d64507eba41cbbb1382ecb
Nathan Chancellor <nathan@kernel.org>
2022-01-10 13:42:05 -0700

    fish: msh: Handle Tailscale IPs
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/msh.fish
+++ fish/functions/msh.fish
@@ -3,19 +3,54 @@
 # Copyright (C) 2021-2022 Nathan Chancellor
 
 function msh -d "Shorthand for mosh -o" -w mosh
-    switch $argv[1]
+    for arg in $argv
+        switch $arg
+            case -t --tailscale
+                set tailscale true
+            case '*'
+                set msh_args $arg
+        end
+    end
+
+    switch $msh_args
         case dsktp
-            set user_host nathan@192.168.4.177
+            if test "$tailscale" = true
+                set ip 100.70.49.74
+            else
+                set ip 192.168.4.177
+            end
+            set user_host nathan@$ip
+
         case lptp
-            set user_host nathan@192.168.4.137
+            if test "$tailscale" = true
+                set ip 100.125.217.41
+            else
+                set ip 192.168.4.137
+            end
+            set user_host nathan@$ip
+
         case pi3
-            set user_host pi@192.168.4.89
+            if test "$tailscale" = true
+                set ip 100.113.197.39
+            else
+                set ip 192.168.4.89
+            end
+            set user_host pi@$ip
+
         case pi4
-            set user_host pi@192.168.4.104
+            if test "$tailscale" = true
+                set ip 100.74.102.104
+            else
+                set ip 192.168.4.104
+            end
+            set user_host pi@$ip
+
         case svr
             set user_host nathan@$SERVER_IP
+
         case '*@*'
             set user_host $argv[1]
+
         case '*'
             print_error "Please specify a user@host combination"
             return 1
