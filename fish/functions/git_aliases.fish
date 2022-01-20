734f5e62b7bc989a31485951b2e95152fd439d4e
Nathan Chancellor <nathan@kernel.org>
2022-01-19 20:54:17 -0700

    fish: git_aliases: Fix adding '--gpg-sign'
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/git_aliases.fish
+++ fish/functions/git_aliases.fish
@@ -8,24 +8,24 @@ function git_aliases -d "Configure git aliases"
     end
     git config --global alias.ai '!f() { for file in $(git status --porcelain | awk \'{print $2}\' | fzf -m); do git add $file; done }; f' # add interactive
     git config --global alias.aa 'add --all'
-    git config --global alias.ac "commit$gpgsign --all --signoff --verbose" # add and commit
+    git config --global alias.ac "commit$gpg_sign --all --signoff --verbose" # add and commit
     git config --global alias.ama 'am --abort'
     git config --global alias.amc 'am --continue'
-    git config --global alias.ams "am$gpgsign --signoff" # am signoff
+    git config --global alias.ams "am$gpg_sign --signoff" # am signoff
     git config --global alias.ap 'apply -3 -v'
     git config --global alias.b 'branch --verbose'
     git config --global alias.bd 'branch --delete --force'
     git config --global alias.bn 'rev-parse --abbrev-ref HEAD' # branch name
     git config --global alias.bm 'branch --move'
     git config --global alias.bu 'branch --unset-upstream'
-    git config --global alias.c "commit$gpgsign --signoff --verbose"
-    git config --global alias.ca "commit$gpgsign --amend --signoff --verbose" # commit ammend
-    git config --global alias.cad "!git commit$gpgsign --amend --signoff --date=\"\$(date)\" --verbose" # commit amend date
+    git config --global alias.c "commit$gpg_sign --signoff --verbose"
+    git config --global alias.ca "commit$gpg_sign --amend --signoff --verbose" # commit ammend
+    git config --global alias.cad "!git commit$gpg_sign --amend --signoff --date=\"\$(date)\" --verbose" # commit amend date
     git config --global alias.cb 'rev-parse --abbrev-ref HEAD' # current branch
     git config --global alias.cf 'diff --name-only --diff-filter=U' # conflicts
     git config --global alias.ch checkout
     git config --global alias.cl 'clean -fxd'
-    git config --global alias.cp "cherry-pick$gpgsign --signoff"
+    git config --global alias.cp "cherry-pick$gpg_sign --signoff"
     git config --global alias.cpa 'cherry-pick --abort'
     git config --global alias.cpc 'cherry-pick --continue'
     git config --global alias.cpe 'cherry-pick --edit --signoff'
@@ -39,7 +39,7 @@ function git_aliases -d "Configure git aliases"
     git config --global alias.f fetch
     git config --global alias.fa 'fetch --all'
     git config --global alias.fixes 'show -s --format="Fixes: %h (\"%s\")"'
-    git config --global alias.fm "commit$gpgsign --file /tmp/mrg-msg" # finish merge
+    git config --global alias.fm "commit$gpg_sign --file /tmp/mrg-msg" # finish merge
     git config --global alias.fp format-patch
     git config --global alias.fpk 'format-patch --add-header="X-Patchwork-Bot: notify"'
     git config --global alias.kf 'show -s --format="%h (\"%s\")"' # kernel format
@@ -53,7 +53,7 @@ function git_aliases -d "Configure git aliases"
     git config --global alias.psu 'push --set-upstream'
     # shellcheck disable=SC2016
     git config --global alias.ra '!f() { for i in $(git cf); do git rf $i; done }; f' # reset all conflicts
-    git config --global alias.rb "rebase$gpgsign"
+    git config --global alias.rb "rebase$gpg_sign"
     git config --global alias.rba 'rebase --abort'
     git config --global alias.rbc 'rebase --continue'
     git config --global alias.rbs 'rebase --skip'
@@ -73,7 +73,7 @@ function git_aliases -d "Configure git aliases"
     git config --global alias.root 'rev-parse --show-toplevel'
     git config --global alias.rs 'reset --soft'
     git config --global alias.ru 'remote update'
-    git config --global alias.rv "revert$gpgsign --signoff"
+    git config --global alias.rv "revert$gpg_sign --signoff"
     git config --global alias.s 'status --short --branch'
     git config --global alias.sf status
     git config --global alias.sh 'show --first-parent'
@@ -88,8 +88,8 @@ function git_aliases -d "Configure git aliases"
     if test (git --version | head -n 1 | cut -d . -f 2) -ge 15
         set signoff " --signoff"
     end
-    git config --global alias.m "merge$gpgsign$signoff"
-    git config --global alias.ml "merge$gpgsign$signoff --log=500"
-    git config --global alias.pl "pull$gpgsign$signoff"
-    git config --global alias.pll "pull$gpgsign$signoff --log=500"
+    git config --global alias.m "merge$gpg_sign$signoff"
+    git config --global alias.ml "merge$gpg_sign$signoff --log=500"
+    git config --global alias.pl "pull$gpg_sign$signoff"
+    git config --global alias.pll "pull$gpg_sign$signoff --log=500"
 end
