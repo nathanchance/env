e173afe116c00213ff56630e0342dc9e2b4d29b0
Nathan Chancellor <nathan@kernel.org>
2022-01-26 16:49:37 -0700

    fish: in_container: Add quiet mode
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/in_container.fish
+++ fish/functions/in_container.fish
@@ -3,8 +3,17 @@
 # Copyright (C) 2022 Nathan Chancellor
 
 function in_container -d "Checks if command is being run in a container"
+    for arg in $argv
+        switch $arg
+            case -q --quiet
+                set quiet true
+        end
+    end
+
     if test -z "$container"; or not test -f /run/.containerenv
-        print_error "This command needs to be run in a container!"
+        if test "$quiet" != "true"
+            print_error "This command needs to be run in a container!"
+        end
         return 1
     end
 
