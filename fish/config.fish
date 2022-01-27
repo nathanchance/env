8e0f4d7577cdcd847cb2ef7c14c7c939051205c5
Nathan Chancellor <nathan@kernel.org>
2022-01-26 17:32:59 -0700

    fish: config.fish: Use 'in_container -q'
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/config.fish
+++ fish/config.fish
@@ -2,17 +2,17 @@
 # SPDX-License-Identifier: MIT
 # Copyright (C) 2021-2022 Nathan Chancellor
 
-if test -z "$container"
+if in_container -q
+    set -l folder
+    for folder in /qemu /tc /binutils /llvm
+        fish_add_path -gm $folder/bin
+    end
+else
     gpg_key_cache
     start_ssh_agent
     tmux_ssh_fixup
 
     set -e fish_user_paths; or true
-else
-    set -l folder
-    for folder in /qemu /tc /binutils /llvm
-        fish_add_path -gm $folder/bin
-    end
 end
 
 if command -q zoxide
