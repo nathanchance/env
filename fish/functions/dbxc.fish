47ebec5aa365613850adfd7404e68993d89fa08c
Nathan Chancellor <nathan@kernel.org>
2022-01-28 17:27:52 -0700

    fish: Initial distrobox functions
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ fish/functions/dbxc.fish
@@ -0,0 +1,60 @@
+#!/usr/bin/env fish
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2022 Nathan Chancellor
+
+function dbxc -d "Shorthand for 'distrobox create'"
+    in_container_msg -h; or return
+
+    set add_args --pids-limit -1
+
+    set i 1
+    while test $i -le (count $argv)
+        set arg $argv[$i]
+        switch $arg
+            case -e --env -v
+                set next (math $i + 1)
+                set -a add_args $arg $argv[$next]
+                set i $next
+
+            case --env='*' --volume='*'
+                set -a add_args $arg
+
+            case --volume
+                set next (math $i + 1)
+                set -a dbx_args $arg $argv[$next]
+                set i $next
+
+            case dev/'*' gcc-'*' llvm-'*'
+                set img $GHCR/$arg
+                set -a dbx_args -i $img -n (string replace "/" "-" $arg)
+
+            case dev-'*'
+                set img $GHCR/(string replace "-" "/" $arg)
+                set -a dbx_args -i $img -n $arg
+        end
+        set i (math $i + 1)
+    end
+
+    # If we are using a development image AND it is the default one for our
+    # architecture (to avoid weird dynamic linking failures), bind mount some
+    # folders into convenient to use locations.
+    switch (uname -m)
+        case aarch64
+            set def_img $GHCR/dev/fedora
+        case x86_64
+            set def_img $GHCR/dev/arch
+    end
+    if test "$img" = "$def_img"
+        if test -d $CBL_TC_BNTL
+            set -a dbx_args --volume (dirname $CBL_TC_BNTL):/binutils
+        end
+        if test -d $CBL_TC_LLVM
+            set -a dbx_args --volume (dirname $CBL_TC_LLVM):/llvm
+        end
+        if test -d $CBL_QEMU_BIN
+            set -a dbx_args --volume $CBL_QEMU:/qemu
+        end
+    end
+
+    dbx create -a "$add_args" $dbx_args $dbx_img
+end
---
+++ fish/functions/dbxe.fish
@@ -0,0 +1,29 @@
+#!/usr/bin/env fish
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2022 Nathan Chancellor
+
+function dbxe -d "Shorthand for 'distrobox enter'"
+    in_container_msg -h; or return
+
+    set i 1
+    while test $i -le (count $argv)
+        set arg $argv[$i]
+        switch $arg
+            case -e --env
+                set next (math $i + 1)
+                set -a add_args $arg $argv[$next]
+                set i $next
+            case --env='*'
+                set -a add_args $arg
+            case '*'
+                set dbx_img $arg
+        end
+        set i (math $i + 1)
+    end
+
+    if test (count $add_args) -gt 0
+        set dbx_args -a "$add_args"
+    end
+
+    dbx enter $dbx_args $dbx_img
+end
