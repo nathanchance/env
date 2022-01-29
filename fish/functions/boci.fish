bef6dfd5da900fbaa8260310ab78fb8ebb07eee3
Nathan Chancellor <nathan@kernel.org>
2022-01-28 18:14:28 -0700

    fish: boci: Add 'in_container_msg'
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/boci.fish
+++ fish/functions/boci.fish
@@ -4,6 +4,8 @@
 # Copyright (C) 2021-2022 Nathan Chancellor
 
 function boci -d "Build an OCI container image"
+    in_container_msg -h; or return
+
     if not command -q podman
         print_warning "boci requires podman, skipping..."
         return 0
