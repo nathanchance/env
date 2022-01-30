bc706f4b43c40028ef0e92ec7c61960707e565d9
Nathan Chancellor <nathan@kernel.org>
2022-01-30 15:14:06 -0700

    fish: cbl_check_cfi: Use statically compiled read_all
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_check_cfi.fish
+++ fish/functions/cbl_check_cfi.fish
@@ -3,15 +3,7 @@
 # Copyright (C) 2021-2022 Nathan Chancellor
 
 function cbl_check_cfi -d "Run LTP's read_all testcase and check for CFI failures"
-    cbl_clone_repo boot-utils
-
-    set read_all $CBL_GIT/boot-utils/debian/ltp/testcases/kernel/fs/read_all/read_all
-
-    if not test -x $read_all
-        podcmd $CBL_GIT/boot-utils/debian/ltp.sh; or return
-    end
-
-    sudo true; or return
+    set read_all $ENV_FOLDER/bin/read_all
 
     sudo sh -c "dmesg -C && $read_all -d /proc && $read_all -d /sys && dmesg -l warn"
 end
