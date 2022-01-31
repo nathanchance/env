0a0e93a640efc9d758bdfb6f1d90cd8388b57540
Nathan Chancellor <nathan@kernel.org>
2022-01-31 15:22:58 -0700

    fish: fdshfmt: Exclude bin/
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/fdshfmt.fish
+++ fish/functions/fdshfmt.fish
@@ -5,7 +5,7 @@
 function fdshfmt -d "Find and format all shell scripts ending with .{ba,}sh in a directory"
     switch (basename $PWD)
         case env
-            fd -t x -E fish -E windows -X fish -c 'shfmt -ci -i 4 -w $argv'
+            fd -t x -E{ bin,fish,windows} -X fish -c 'shfmt -ci -i 4 -w $argv'
         case '*'
             fd -e bash -e sh -X fish -c 'shfmt -ci -i 4 -w $argv'
     end
