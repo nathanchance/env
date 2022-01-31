2fbc652e56b9846e5c6df02ed7f4b89570c9e71f
Nathan Chancellor <nathan@kernel.org>
2022-01-31 11:32:41 -0700

    fish: edfunc: Make reloading opt-in
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/edfunc.fish
+++ fish/functions/edfunc.fish
@@ -3,13 +3,25 @@
 # Copyright (C) 2021-2022 Nathan Chancellor
 
 function edfunc -d "Edit function file in $ENV_FOLDER"
-    for func_name in $argv
-        set func_file $ENV_FOLDER/fish/functions/$func_name.fish
+    for arg in $argv
+        switch $arg
+            case -r --reload
+                set reload true
+            case '*'
+                set -a funcs_to_edit $arg
+        end
+    end
+
+    for func_to_edit in $funcs_to_edit
+        set func_file $ENV_FOLDER/fish/functions/$func_to_edit.fish
         if test -f "$func_file"
             vim $func_file
-            rld
         else
             print_error "$func_file does not exist!"
         end
+    end
+
+    if test "$reload" = true
+        rld
     end
 end
