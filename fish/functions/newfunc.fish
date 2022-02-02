bdd1d6c368ca27281a4b663ae207cdb4d03bc537
Nathan Chancellor <nathan@kernel.org>
2022-02-02 13:57:48 -0700

    fish: Add newfunc
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ fish/functions/newfunc.fish
@@ -0,0 +1,22 @@
+#!/usr/bin/env fish
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2022 Nathan Chancellor
+
+function newfunc -d "Generate and edit new function fish"
+    for func_name in $argv
+        set func_file $ENV_FOLDER/fish/functions/$func_name.fish
+
+        if test -f $func_file
+            print_error "$func_name already exists!"
+            return 1
+        end
+
+        echo "#!/usr/bin/env fish
+# SPDX-License-Identifier: MIT
+# Copyright (C) "(date +%Y)" Nathan Chancellor
+
+function $func_name
+end" >$func_file
+        edfunc $func_file
+    end
+end
