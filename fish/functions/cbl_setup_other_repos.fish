20142f44bde20715870c56bdf24614389e26012c
Nathan Chancellor <nathan@kernel.org>
2022-02-02 11:27:42 -0700

    fish: cbl_setup_other_repos: Rewrite using gh
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_setup_other_repos.fish
+++ fish/functions/cbl_setup_other_repos.fish
@@ -3,21 +3,16 @@
 # Copyright (C) 2021-2022 Nathan Chancellor
 
 function cbl_setup_other_repos -d "Download other ClangBuiltLinux repos"
-    for repo in boot-utils containers continuous-integration2:ci tc-build
-        if string match -qr ":" $repo
-            set folder (string split -f2 ":" $repo)
-            set repo (string split -f1 ":" $repo)
-        else
-            set folder $repo
-        end
-
+    for repo in boot-utils containers continuous-integration2 tc-build
         set folder $CBL_GIT/$repo
         if not test -d $folder
             mkdir -p (dirname $folder)
-            git clone git@github.com:ClangBuiltLinux/$repo.git $folder
+            gh repo clone ClangBuiltLinux/$repo $folder
+            pushd $folder; or return
+            gh repo fork --remote --remote-name nathanchance; or return
+            git remote update; or return
+            popd; or return
         end
-        git -C $folder fork
-        git -C $folder remote update
     end
 
     for repo in creduce-files llvm-kernel-testing repro-scripts tc-build
@@ -26,9 +21,9 @@ function cbl_setup_other_repos -d "Download other ClangBuiltLinux repos"
             mkdir -p (dirname $folder)
             switch $repo
                 case tc-build
-                    set clone_args -b personal
+                    set clone_args -- -b personal
             end
-            git clone $clone_args git@github.com:nathanchance/$repo.git $folder
+            gh repo clone $repo $folder $clone_args
         end
     end
 
