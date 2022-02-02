d37e6cecfcad0da44caa97d451a8a4f4fd8a7434
Nathan Chancellor <nathan@kernel.org>
2022-02-02 11:11:48 -0700

    fish: edfunc: Allow full paths
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/edfunc.fish
+++ fish/functions/edfunc.fish
@@ -13,12 +13,16 @@ function edfunc -d "Edit function file in $ENV_FOLDER"
     end
 
     for func_to_edit in $funcs_to_edit
-        set func_file $ENV_FOLDER/fish/functions/$func_to_edit.fish
-        if test -f "$func_file"
-            vim $func_file
+        if test -f $func_to_edit
+            set func_file $func_to_edit
         else
-            print_error "$func_file does not exist!"
+            set func_file $ENV_FOLDER/fish/functions/$func_to_edit.fish
+            if not test -f "$func_file"
+                print_error "$func_file does not exist!"
+                return 1
+            end
         end
+        vim $func_file
     end
 
     if test "$reload" = true
