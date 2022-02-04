588941e0556e1964e0fd509732c878e31bcc765b
Nathan Chancellor <nathan@kernel.org>
2022-02-04 14:48:03 -0700

    fish: Add cloci
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ fish/functions/cloci.fish
@@ -0,0 +1,13 @@
+#!/usr/bin/env fish
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2022 Nathan Chancellor
+
+function cloci -d "Clean untagged OCI images"
+    in_container_msg -h; or return
+
+    set images (podman image list &| rg "<none>" &| awk '{print $3}')
+
+    if test -n "$images"
+        podman image rm $images
+    end
+end
