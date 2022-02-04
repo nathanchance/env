fe9e8b1abca9eebb2749a269801d2a52a0eb391d
Nathan Chancellor <nathan@kernel.org>
2022-02-04 16:46:50 -0700

    fish: cbl_gen_ubuntuconfig: Update to 5.17-rc2
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_gen_ubuntuconfig.fish
+++ fish/functions/cbl_gen_ubuntuconfig.fish
@@ -33,7 +33,7 @@ function cbl_gen_ubuntuconfig -d "Generate a kernel .config from Ubuntu's config
     mkdir -p $out
 
     for file in config.common.ubuntu $ubuntu_arch/config.{common.$ubuntu_arch,flavour.generic}
-        crl "https://git.launchpad.net/~ubuntu-kernel-test/ubuntu/+source/linux/+git/mainline-crack/plain/debian.master/config/$file?h=cod/mainline/v5.16"
+        crl "https://git.launchpad.net/~ubuntu-kernel-test/ubuntu/+source/linux/+git/mainline-crack/plain/debian.master/config/$file?h=cod/mainline/v5.17-rc2"
     end >$cfg
 
     scripts/config \
