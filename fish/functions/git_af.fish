ef6e890492df790d880cb46e20c612319fd7a1f9
Nathan Chancellor <nathan@kernel.org>
2022-02-08 12:37:29 -0700

    fish: git_af: Fixes
    * Support running 'git dh' in preview window to see diff.
    
    * Only add files if there were some selected.
    
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/git_af.fish
+++ fish/functions/git_af.fish
@@ -3,7 +3,9 @@
 # Copyright (C) 2022 Nathan Chancellor
 
 function git_af -d "git add with fzf"
-    set files_to_add (git status --porcelain | awk '{print $2}' | fzf -m)
-    git add $files_to_add
-    echo "Added $files_to_add"
+    set files_to_add (git status --porcelain | awk '{print $2}' | fzf -m --preview="git dh {}")
+    if test -n "$files_to_add"
+        git add $files_to_add
+        echo "Added $files_to_add"
+    end
 end
