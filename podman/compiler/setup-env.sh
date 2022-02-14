109073eb0e6a03db6eb1ccc697b0335d19fe553d
Nathan Chancellor <nathan@kernel.org>
2022-02-14 10:07:00 -0700

    podman: compiler: Upgrade Android LLVM to r445002
    [skip actions]
    
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- podman/compiler/setup-env.sh
+++ podman/compiler/setup-env.sh
@@ -270,7 +270,7 @@ function install_packages_apt() {
     rm -fr /var/lib/apt/lists/*
     if [[ $compiler =~ llvm ]]; then
         if [[ $compiler = "llvm-android" ]]; then
-            local android_clang=r437112b
+            local android_clang=r445002
             wget --output-document=/dev/stdout --progress=dot:giga https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/master/clang-$android_clang.tar.gz |
                 tar -C /usr/local -xzf -
         else
