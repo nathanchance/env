4fcbc47f5834692d51a8fa0de4ef7705b85d40d7
Nathan Chancellor <nathan@kernel.org>
2022-03-03 12:59:18 -0700

    fish: Add b4_patch
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ fish/functions/b4_patch.fish
@@ -0,0 +1,22 @@
+#!/usr/bin/env fish
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2022 Nathan Chancellor
+
+function b4_patch -d "Download a b4 .mbx and rename the extension to .patch"
+    for arg in $argv
+        switch $arg
+            case '*@*'
+                set -a msg_ids $arg
+            case '*'
+                set -a b4_args $arg
+        end
+    end
+
+    for msg_id in $msg_ids
+        b4 am -l -P _ $b4_args $msg_id
+    end
+
+    for patch in *.mbx
+        mv -v $patch (string replace .mbx .patch $patch)
+    end
+end
