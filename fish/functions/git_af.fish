78fb0aa4609beb50eeeb1dbc8df7299f1f8a69f0
Nathan Chancellor <nathan@kernel.org>
2022-03-15 15:31:32 -0700

    fish: git_af: Use color for diff
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/git_af.fish
+++ fish/functions/git_af.fish
@@ -3,7 +3,7 @@
 # Copyright (C) 2022 Nathan Chancellor
 
 function git_af -d "git add with fzf"
-    set files_to_add (git status --porcelain | awk '{print $2}' | fzf -m --preview="git dh {}")
+    set files_to_add (git status --porcelain | awk '{print $2}' | fzf -m --preview="git dh --color=always {}")
     if test -n "$files_to_add"
         git add $files_to_add
         echo "Added $files_to_add"
