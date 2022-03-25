da9908b65f11d496a202fd4b180158ff60c18a45
Nathan Chancellor <nathan@kernel.org>
2022-03-25 13:18:45 -0700

    fish: Add function to remove older versions of managed software
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ fish/functions/cbl_cl_software.fish
@@ -0,0 +1,28 @@
+#!/usr/bin/env fish
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2022 Nathan Chancellor
+
+function cbl_cl_software -d "Clean up old versions of managed software"
+    if test (count $argv) -ne 1
+        print_error "This function takes the tool whose versions will be cleaned up as an argument!"
+        return 1
+    end
+
+    switch $argv[1]
+        case binutils
+            set folder $CBL_TC_STOW_BNTL
+            set binary as
+        case llvm
+            set folder $CBL_TC_STOW_LLVM
+            set binary clang
+        case qemu
+            set folder $CBL_QEMU_INSTALL
+            set binary qemu-system-x86_64
+    end
+
+    set folders_to_remove (fd -d 1 -t d . $folder -x basename | sort | fzf -m --preview="$folder/{}/bin/$binary --version")
+    if test -n "$folders_to_remove"
+        set fish_trace 1
+        rm -fr $folder/$folders_to_remove
+    end
+end
