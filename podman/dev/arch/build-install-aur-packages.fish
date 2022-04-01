eed6783ab9cea9af9672d8a9819a7959b807ddbd
Nathan Chancellor <nathan@kernel.org>
2022-04-01 08:13:15 -0700

    env: fish 3.4.1 is available in Arch Linux now
    It is no longer necessary to build it from source and install it
    locally!
    
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- podman/dev/arch/build-install-aur-packages.fish
+++ podman/dev/arch/build-install-aur-packages.fish
@@ -31,16 +31,6 @@ function build_cvise
     makepkg -irs --noconfirm
 end
 
-function build_fish
-    cd $HOME/fish; or exit
-    makepkg -irs --noconfirm
-
-    if test (fish -c 'echo $version' | string replace -a . '') -lt 340
-        error (fish --version)"is too old!"
-    end
-end
-
 check_user
 setup_gpg
 build_cvise
-build_fish
