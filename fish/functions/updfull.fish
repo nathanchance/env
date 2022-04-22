f95900f37e1b067d087ee58ef2c27c84bc7bbe7a
Nathan Chancellor <nathan@kernel.org>
2022-04-22 08:26:58 -0700

    fish: Add function to update host OS, shell env, and main dev container
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ fish/functions/updfull.fish
@@ -0,0 +1,8 @@
+#!/usr/bin/env fish
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2022 Nathan Chancellor
+
+function updfull -d "Update host machine, shell environment, and main distrobox container"
+    upd -y env os
+    and dbxe -- "fish -c 'upd -y'"
+end
