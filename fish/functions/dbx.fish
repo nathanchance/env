e967acb44eeb7607e7af3635795cf50c76a53278
Nathan Chancellor <nathan@kernel.org>
2022-05-02 16:16:11 -0700

    fish: dbx: Handle being installed through system
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/dbx.fish
+++ fish/functions/dbx.fish
@@ -2,10 +2,14 @@
 # SPDX-License-Identifier: MIT
 # Copyright (C) 2022 Nathan Chancellor
 
-function dbx -d "Calls distrobox from a git checkout"
-    set dbx $BIN_SRC_FOLDER/distrobox/distrobox
-    if not test -f $dbx
-        upd distrobox; or return
+function dbx -d "Calls distrobox based on where it is installed"
+    if command -q distrobox
+        command distrobox $argv
+    else
+        set dbx $BIN_SRC_FOLDER/distrobox/distrobox
+        if not test -f $dbx
+            upd distrobox; or return
+        end
+        $dbx $argv
     end
-    $dbx $argv
 end
