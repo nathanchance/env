8a37adb5dd433d5f11eb9db84200351abdfb67e2
Nathan Chancellor <nathan@kernel.org>
2022-05-23 09:32:51 -0700

    fish: tuxmake: Allow use within container
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/tuxmake.fish
+++ fish/functions/tuxmake.fish
@@ -3,7 +3,9 @@
 # Copyright (C) 2021-2022 Nathan Chancellor
 
 function tuxmake -d "Calls tuxmake from a git checkout if it is not available in PATH"
-    in_container_msg -h; or return
+    if string match -qr podman -- $argv
+        in_container_msg -h; or return
+    end
 
     set -lx XDG_CACHE_HOME $XDG_FOLDER/cache
     set -lx XDG_CONFIG_HOME $XDG_FOLDER/config
