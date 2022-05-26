92181f4c2b1ee01df2e09d286307bd8d456c9487
Nathan Chancellor <nathan@kernel.org>
2022-05-26 11:25:05 -0700

    fish: Add function for manually running a GitHub Actions workflow
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ fish/functions/gh_run.fish
@@ -0,0 +1,34 @@
+#!/usr/bin/env fish
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2022 Nathan Chancellor
+
+function gh_run -d "Run GitHub Actions Workflow"
+    if test (count $argv) -lt 3
+        print_error "Expected repo, branch name, and workflow as arguments, in that order!"
+        return 1
+    end
+
+    set repo $argv[1]
+    set branch $argv[2]
+    set workflow $argv[3]
+
+    gh workflow run \
+        --ref $branch \
+        --repo $repo \
+        $workflow
+
+    # Make sure that 'gh run list' call succeeds
+    sleep 1
+
+    set json_key databaseId
+    set workflow_id (gh run list \
+        --json $json_key \
+        --jq .[].$json_key \
+        --limit 1 \
+        --repo $repo \
+        --workflow $workflow)
+
+    gh run view \
+        --repo $repo \
+        $workflow_id
+end
