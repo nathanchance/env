b6eaadbe20ebbcfa6f2cf78b91a42c65e1df4b52
Nathan Chancellor <nathan@kernel.org>
2022-06-04 17:52:03 -0700

    fish: Add vw
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ fish/functions/vw.fish
@@ -0,0 +1,20 @@
+#!/usr/bin/env fish
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2022 Nathan Chancellor
+
+function vw -d "View file with bat"
+    for arg in $argv
+        switch $arg
+            case -c --copy
+                set -a bat_args --style plain
+            case '*'
+                set -a files $arg
+        end
+    end
+
+    bat \
+        --color always \
+        --paging always \
+        $bat_args \
+        $files
+end
