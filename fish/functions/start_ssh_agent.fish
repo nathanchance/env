9f5bad41c41292e713413a2b5a6faaa2ddff4517
Nathan Chancellor <nathan@kernel.org>
2022-07-21 16:29:43 -0700

    fish: start_ssh_agent: Handle 'ssh-add' exiting 1 initially
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/start_ssh_agent.fish
+++ fish/functions/start_ssh_agent.fish
@@ -19,21 +19,25 @@ function start_ssh_agent -d "Launch an ssh agent only if it has not already been
         return
     end
 
-    set ssh_agent_file $HOME/.ssh/.ssh-agent.fish
     ssh-add -l &>/dev/null
-    if test $status -eq 2
-        if test -r $ssh_agent_file
-            cat $ssh_agent_file | source >/dev/null
-        end
-
-        ssh-add -l &>/dev/null
-        if test $status -eq 2
-            begin
-                umask 066
-                ssh-agent -c >$ssh_agent_file
-            end
-            cat $ssh_agent_file | source >/dev/null
+    switch $status
+        case 1
             ssh-add $ssh_key
-        end
+        case 2
+            set ssh_agent_file $HOME/.ssh/.ssh-agent.fish
+
+            if test -r $ssh_agent_file
+                cat $ssh_agent_file | source >/dev/null
+            end
+
+            ssh-add -l &>/dev/null
+            if test $status -eq 2
+                begin
+                    umask 066
+                    ssh-agent -c >$ssh_agent_file
+                end
+                cat $ssh_agent_file | source >/dev/null
+                ssh-add $ssh_key
+            end
     end
 end
