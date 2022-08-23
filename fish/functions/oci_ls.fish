7b257035721216929ccee95e4b36fb1210282b40
Nathan Chancellor <nathan@kernel.org>
2022-08-23 14:25:38 -0700

    fish: Add oci_ls
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ fish/functions/oci_ls.fish
@@ -0,0 +1,13 @@
+#!/usr/bin/env fish
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2022 Nathan Chancellor
+
+function oci_ls -d "Shows OCI containers and images"
+    in_container_msg -h; or return
+
+    header Containers
+    podman container ls --all
+
+    header Images
+    podman image ls --all
+end
