28aa1d20cba0005af50f93422a92b2eba86211cf
Nathan Chancellor <nathan@kernel.org>
2022-08-30 09:01:40 -0700

    env: Update to next-20220830
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_rb_fd.fish
+++ fish/functions/cbl_rb_fd.fish
@@ -15,6 +15,7 @@ function cbl_rb_fd -d "Rebase generic Fedora kernel on latest linux-next"
     # Patching
     set -a patches https://lore.kernel.org/all/20220825180607.2707947-1-nathan@kernel.org/ # [PATCH net-next] net/mlx5e: Do not use err uninitialized in mlx5e_rep_add_meta_tunnel_rule()
     set -a patches https://lore.kernel.org/all/20220829165450.217628-1-nathan@kernel.org/ # [PATCH] drm/msm/dsi: Remove use of device_node in dsi_host_parse_dt()
+    set -a patches https://lore.kernel.org/all/20220830101237.22782-1-gal@nvidia.com/ # [PATCH net-next] net: ieee802154: Fix compilation error when CONFIG_IEEE802154_NL802154_EXPERIMENTAL is disabled
     for patch in $patches
         b4 shazam -l -P _ -s $patch; or return
     end
