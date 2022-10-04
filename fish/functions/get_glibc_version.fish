3e6d35a1d5343ed67dbafecccbdba3fec9530082
Nathan Chancellor <nathan@kernel.org>
2022-10-04 11:39:51 -0700

    fish: get_glibc_version: Use ldd instead of libc file
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/get_glibc_version.fish
+++ fish/functions/get_glibc_version.fish
@@ -3,20 +3,7 @@
 # Copyright (C) 2022 Nathan Chancellor
 
 function get_glibc_version -d "Get glibc version as a five or six digit number"
-    set possible_libc_paths \
-        /lib64/libc.so.6 \
-        /usr/lib/aarch64-linux-gnu/libc.so.6 \
-        /usr/lib/arm-linux-gnueabihf/libc.so.6 \
-        /usr/lib/x86_64-linux-gnu/libc.so.6
-
-    for possible_libc_path in $possible_libc_paths
-        if test -f $possible_libc_path
-            set libc $possible_libc_path
-            break
-        end
-    end
-
-    set ver_array ($libc --version | head -1 | string match -r '[0-9|.]+' | string split .)
+    set ver_array (ldd --version | head -1 | string match -r '[0-9|.]+' | string split .)
     if test -z "$ver_array[3]"
         set ver_array[3] 0
     end
