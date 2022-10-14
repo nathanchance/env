ec192d9a94fa616962ce4f27bf21b9b7de6cb245
Nathan Chancellor <nathan@kernel.org>
2022-10-14 09:59:33 -0700

    env: Update to next-20221014
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_rb_fd.fish
+++ fish/functions/cbl_rb_fd.fish
@@ -14,7 +14,6 @@ function cbl_rb_fd -d "Rebase generic Fedora kernel on latest linux-next"
 
     # Patching
     set -a patches https://lore.kernel.org/all/20221004232359.285685-1-nathan@kernel.org/ # fs/ntfs3: Don't use uni1 uninitialized in ntfs_d_compare()
-    set -a patches https://lore.kernel.org/all/20221006143319.264657-1-zack@kde.org/ # kbuild: Stop including vmlinux.bz2 in the rpm's
     for patch in $patches
         b4 shazam -l -P _ -s $patch; or return
     end
--- fish/functions/cbl_rb_pi.fish
+++ fish/functions/cbl_rb_pi.fish
@@ -24,6 +24,7 @@ function cbl_rb_pi -d "Rebase Raspberry Pi kernel on latest linux-next"
     for patch in $patches
         b4 am -l -o - -P _ -s $patch | git am; or return
     end
+    git am $GITHUB_FOLDER/patches/linux-misc/v3-pass-march=-only-to-compiler.patch; or return
 
     # Regenerate defconfigs
     for arch in arm arm64
@@ -94,100 +95,7 @@ function cbl_rb_pi -d "Rebase Raspberry Pi kernel on latest linux-next"
     end
     git ac -m "ARM: configs: Update defconfigs"
 
-    echo 'From f16e7af3d188d6aa9d45d7502ba3fcebc441f22a Mon Sep 17 00:00:00 2001
-From: Nathan Chancellor <nathan@kernel.org>
-Date: Wed, 28 Jul 2021 12:14:27 -0700
-Subject: [PATCH] ARM: dts: bcm2{711,837}: Disable the display pipeline
-
-Signed-off-by: Nathan Chancellor <nathan@kernel.org>
----
- arch/arm/boot/dts/bcm2711-rpi-4-b.dts      | 16 ++++++++--------
- arch/arm/boot/dts/bcm2837-rpi-3-b-plus.dts |  2 +-
- arch/arm/boot/dts/bcm2837-rpi-3-b.dts      |  2 +-
- 3 files changed, 10 insertions(+), 10 deletions(-)
-
-diff --git a/arch/arm/boot/dts/bcm2711-rpi-4-b.dts b/arch/arm/boot/dts/bcm2711-rpi-4-b.dts
-index f24bdd0870a5..66cfc5d057ce 100644
---- a/arch/arm/boot/dts/bcm2711-rpi-4-b.dts
-+++ b/arch/arm/boot/dts/bcm2711-rpi-4-b.dts
-@@ -57,11 +57,11 @@ sd_vcc_reg: sd_vcc_reg {
- };
- 
- &ddc0 {
--	status = "okay";
-+	status = "disabled";
- };
- 
- &ddc1 {
--	status = "okay";
-+	status = "disabled";
- };
- 
- &expgpio {
-@@ -149,27 +149,27 @@ &gpio {
- };
- 
- &hdmi0 {
--	status = "okay";
-+	status = "disabled";
- };
- 
- &hdmi1 {
--	status = "okay";
-+	status = "disabled";
- };
- 
- &pixelvalve0 {
--	status = "okay";
-+	status = "disabled";
- };
- 
- &pixelvalve1 {
--	status = "okay";
-+	status = "disabled";
- };
- 
- &pixelvalve2 {
--	status = "okay";
-+	status = "disabled";
- };
- 
- &pixelvalve4 {
--	status = "okay";
-+	status = "disabled";
- };
- 
- &pwm1 {
-diff --git a/arch/arm/boot/dts/bcm2837-rpi-3-b-plus.dts b/arch/arm/boot/dts/bcm2837-rpi-3-b-plus.dts
-index 61010266ca9a..e1a6c95f65f9 100644
---- a/arch/arm/boot/dts/bcm2837-rpi-3-b-plus.dts
-+++ b/arch/arm/boot/dts/bcm2837-rpi-3-b-plus.dts
-@@ -128,7 +128,7 @@ &gpio {
- &hdmi {
- 	hpd-gpios = <&gpio 28 GPIO_ACTIVE_LOW>;
- 	power-domains = <&power RPI_POWER_DOMAIN_HDMI>;
--	status = "okay";
-+	status = "disabled";
- };
- 
- &pwm {
-diff --git a/arch/arm/boot/dts/bcm2837-rpi-3-b.dts b/arch/arm/boot/dts/bcm2837-rpi-3-b.dts
-index dd4a48604097..ab2173e3951b 100644
---- a/arch/arm/boot/dts/bcm2837-rpi-3-b.dts
-+++ b/arch/arm/boot/dts/bcm2837-rpi-3-b.dts
-@@ -127,7 +127,7 @@ &pwm {
- &hdmi {
- 	hpd-gpios = <&expgpio 4 GPIO_ACTIVE_LOW>;
- 	power-domains = <&power RPI_POWER_DOMAIN_HDMI>;
--	status = "okay";
-+	status = "disabled";
- };
- 
- /* uart0 communicates with the BT module */
--- 
-2.32.0.264.g75ae10bc75
-
-' | git ams; or return
+    git am $GITHUB_FOLDER/patches/linux-misc/0001-ARM-dts-bcm2-711-837-Disable-the-display-pipeline.patch; or return
 
     for arch in arm arm64
         ../pi-scripts/build.fish $arch; or return
