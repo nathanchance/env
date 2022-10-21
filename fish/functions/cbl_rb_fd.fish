f4abf5f28696fdbfc8ddd17c780c80c86c90ede3
Nathan Chancellor <nathan@kernel.org>
2022-10-20 17:45:56 -0700

    env: Update to next-20221020
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_rb_fd.fish
+++ fish/functions/cbl_rb_fd.fish
@@ -14,6 +14,7 @@ function cbl_rb_fd -d "Rebase generic Fedora kernel on latest linux-next"
 
     # Patching
     set -a patches https://lore.kernel.org/all/20221004232359.285685-1-nathan@kernel.org/ # fs/ntfs3: Don't use uni1 uninitialized in ntfs_d_compare()
+    set -a patches https://lore.kernel.org/all/20221020135709.1549086-1-colin.i.king@gmail.com/ # wifi: rtl8xxxu: Fix reads of uninitialized variables hw_ctrl_s1, sw_ctrl_s1
     for patch in $patches
         b4 shazam -l -P _ -s $patch; or return
     end
