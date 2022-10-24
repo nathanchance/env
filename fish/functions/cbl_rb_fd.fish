4ffc906b59f2a06fbae29ce6b208ee7d025630b2
Nathan Chancellor <nathan@kernel.org>
2022-10-24 10:26:59 -0700

    env: Update to
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_rb_fd.fish
+++ fish/functions/cbl_rb_fd.fish
@@ -14,7 +14,8 @@ function cbl_rb_fd -d "Rebase generic Fedora kernel on latest linux-next"
 
     # Patching
     set -a patches https://lore.kernel.org/all/20221004232359.285685-1-nathan@kernel.org/ # fs/ntfs3: Don't use uni1 uninitialized in ntfs_d_compare()
-    set -a patches https://lore.kernel.org/all/20221020135709.1549086-1-colin.i.king@gmail.com/ # wifi: rtl8xxxu: Fix reads of uninitialized variables hw_ctrl_s1, sw_ctrl_s1
+    set -a patches https://lore.kernel.org/all/20221024151201.2215380-1-nathan@kernel.org/ # coresight: cti: Remove unused variables in cti_{dis,en}able_hw()
+    set -a patches https://lore.kernel.org/all/20221024151953.2238616-1-nathan@kernel.org/ # drm/amdgpu: Fix uninitialized warning in mmhub_v2_0_get_clockgating()
     for patch in $patches
         b4 shazam -l -P _ -s $patch; or return
     end
