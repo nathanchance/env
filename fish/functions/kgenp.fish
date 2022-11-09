e4e9ca457f4d8095b19113dc9f15bbd3c8bafc12
Nathan Chancellor <nathan@kernel.org>
2022-11-09 09:53:10 -0700

    fish: Add function for generate a patch series for git send-email
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ fish/functions/kgenp.fish
@@ -0,0 +1,11 @@
+#!/usr/bin/env fish
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2022 Nathan Chancellor
+
+function kgenp -d "Generate patches from a git tree to send via git send-email"
+    in_kernel_tree; or return
+
+    set mfc (git mfc)
+
+    git fp --base $mfc^ $argv $mfc^..$mfc
+end
