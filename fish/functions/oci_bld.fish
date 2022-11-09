36736684e944a5c0dd0babec6b834dbf31df01e7
Nathan Chancellor <nathan@kernel.org>
2022-11-09 16:39:47 -0700

    fish: oci_bld: Add support for docker
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/oci_bld.fish
+++ fish/functions/oci_bld.fish
@@ -6,9 +6,22 @@
 function oci_bld -d "Build an OCI container image"
     in_container_msg -h; or return
 
-    if not command -q podman
-        print_warning "oci_bld requires podman, skipping..."
-        return 0
+    for mgr in podman docker none
+        if command -q $mgr
+            break
+        end
+    end
+
+    switch $mgr
+        case docker
+            set base_mgr_args \
+                --no-cache
+        case podman
+            set base_mgr_args \
+                --layers false
+        case none
+            print_warning "oci_bld requires podman or docker, skipping..."
+            return 0
     end
 
     for arg in $argv
@@ -25,7 +38,7 @@ function oci_bld -d "Build an OCI container image"
     end
 
     for image in $images
-        set -l podman_args
+        set -l mgr_args
 
         switch $image
             case gcc-5
@@ -48,7 +61,7 @@ function oci_bld -d "Build an OCI container image"
                 set image $folder
 
             case {gcc,llvm}-'*'
-                set podman_args \
+                set mgr_args \
                     --build-arg BASE=docker.io/$base \
                     --build-arg COMPILER=$image
                 set folder compiler
@@ -59,14 +72,14 @@ function oci_bld -d "Build an OCI container image"
 
         pushd $ENV_FOLDER/podman/$folder; or return
 
-        set podman_build_cmd \
-            podman build \
-            $podman_args \
-            --layers=false \
+        set mgr_build_cmd \
+            $mgr build \
+            $base_mgr_args \
+            $mgr_args \
             --pull \
             --tag $GHCR/$image .
-        print_cmd $podman_build_cmd
-        $podman_build_cmd; or return
+        print_cmd $mgr_build_cmd
+        $mgr_build_cmd; or return
 
         popd; or return
     end
