22c72f3c5f2a0d844e0a228ee8a58a520570f244
Nathan Chancellor <nathan@kernel.org>
2022-11-18 13:52:45 -0700

    python/print_cmd.py: Use an array slice instead of explicit indices
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- python/print_cmd.py
+++ python/print_cmd.py
@@ -4,4 +4,4 @@ import shlex
 import sys
 
 if __name__ == '__main__':
-    print(' '.join([shlex.quote(argv[num]) for num in range(1, len(sys.argv))]))
+    print(' '.join([shlex.quote(arg) for arg in sys.argv[1:]]))
