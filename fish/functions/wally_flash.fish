c95356eaf1024ecb6c80a3584641e99a9c2a3278
Nathan Chancellor <nathan@kernel.org>
2022-11-19 16:35:29 -0700

    fish: Add a function to flash Moonlander firmware files from macOS
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ fish/functions/wally_flash.fish
@@ -0,0 +1,16 @@
+#!/usr/bin/env fish
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2022 Nathan Chancellor
+
+function wally_flash -d "Flash keyboard firmware file"
+    if test (count $argv) -gt 0
+        set file $argv[1]
+    else
+        set file (fd -e bin . $HOME/Downloads | fzf)
+        test -z "$file"; and return 0
+    end
+
+    set file (realpath $file)
+    wally-cli $file; or return
+    rm -v $file
+end
