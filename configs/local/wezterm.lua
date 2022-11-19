e20bdf9c515ddd4ce6ed431c52141f647b0a6919
Nathan Chancellor <nathan@kernel.org>
2022-11-19 15:48:09 -0700

    configs: wezterm.lua: Support dynamic font size
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- configs/local/wezterm.lua
+++ configs/local/wezterm.lua
@@ -11,12 +11,27 @@ wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_wid
     {Text=" " .. pane_title .. " "},
   }
 end)
+local function get_hostname()
+    local f = io.popen("/bin/hostname")
+    local hostname = f:read("*a") or ""
+    f:close()
+    hostname =string.gsub(hostname, "\n$", "")
+    return hostname
+end
+local function get_font_size()
+    local _hostname = get_hostname()
+    if _hostname == "Nathans-Mac-Studio.local" then
+        return 14.0
+    else
+        return 13.0
+    end
+end
 return {
   color_scheme = "Snazzy",
   debug_key_events = true,
   default_prog = {"/opt/homebrew/bin/fish", "-l"},
   font = wezterm.font_with_fallback({"Iosevka Term SS08", "SF Mono Regular"}),
-  font_size = 13.0,
+  font_size = get_font_size(),
   initial_cols = 238,
   initial_rows = 51,
   keys = {
