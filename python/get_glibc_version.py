693e46ebc72a74e549205e42532df45b53ef7657
Nathan Chancellor <nathan@kernel.org>
2022-11-23 15:37:31 -0700

    env: Handle opendoas fiasco on Debian and Ubuntu better
    To rebuild the binary packages if necessary:
    
    1. Create container
    
      # native
      $ dbxeph dev/debian
    
      # armhf on arm64
      $ podman pull --arch arm docker.io/debian
      $ dbx ephemeral -i docker.io/debian -n debian-armhf
    
    2. Install packages
    
      $ sudo sh -c "apt update &&
    apt upgrade -y &&
    apt install -y --no-install-recommends \
    build-essential \
    devscripts \
    fakeroot &&
    apt build-dep -y opendoas"
    
    3. Download source
    
      $ mkdir -p $SRC_FOLDER/debian/opendoas
      $ cd $SRC_FOLDER/debian/opendoas
      $ apt source opendoas
      $ cd opendoas-*
    
    4. Build package
    
      $ debuild -b -uc -us
    
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ python/get_glibc_version.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+if __name__ == '__main__':
+    ldd_version_out = subprocess.run(['ldd', '--version'],
+                                     capture_output=True,
+                                     check=True,
+                                     text=True).stdout
+    ldd_version = ldd_version_out.split('\n')[0].split(' ')[-1].split('.')
+    if len(ldd_version) < 3:
+        ldd_version += [0]
+    ldd_version = [int(x) for x in ldd_version]
+    print(f'{ldd_version[0]:d}{ldd_version[1]:02d}{ldd_version[2]:03d}')
