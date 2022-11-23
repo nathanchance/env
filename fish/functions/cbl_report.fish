1eb5d57e563e2081278be18d526342c25857b5cd
Nathan Chancellor <nathan@kernel.org>
2022-11-23 16:19:05 -0700

    fish: Add cbl_report.py wrapper function
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ fish/functions/cbl_report.fish
@@ -0,0 +1,7 @@
+#!/usr/bin/env fish
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2022 Nathan Chancellor
+
+function cbl_report -d "Shell wrapper for cbl_report.py"
+    $PYTHON_FOLDER/cbl_report.py $argv
+end
