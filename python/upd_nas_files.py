dee571ca264b2d75293e3f1fc87e4744d361552c
Nathan Chancellor <nathan@kernel.org>
2022-11-27 06:52:12 -0700

    python/upd_nas_files.py: Add support for calculating checksums dynamically
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- python/upd_nas_files.py
+++ python/upd_nas_files.py
@@ -2,10 +2,24 @@
 
 import argparse
 import datetime
+import hashlib
 import json
 import os
 import pathlib
+import re
 import requests
+
+
+def print_color(color, string):
+    print(f"{color}{string}\033[0m", flush=True)
+
+
+def print_green(msg):
+    print_color('\033[01;32m', msg)
+
+
+def print_yellow(msg):
+    print_color('\033[01;33m', msg)
 
 
 def parse_parameters():
@@ -27,11 +41,46 @@ def parse_parameters():
     return parser.parse_args()
 
 
+def calculate_sha256(file_path):
+    file_hash = hashlib.sha256()
+    with open(file_path, 'rb') as file:
+        while True:
+            chunk = file.read(1048576)  # 1MB at a time
+            if not chunk:
+                break
+            file_hash.update(chunk)
+    return file_hash.hexdigest()
+
+
+def get_sha256_from_url(url, basename):
+    response = requests.get(url, timeout=3600)
+    response.raise_for_status()
+    for line in response.content.decode('utf-8').split('\n'):
+        if re.search(basename, line):
+            sha256_match = re.search('[A-Fa-f0-9]{64}', line)
+            if sha256_match:
+                return sha256_match.group(0)
+    return None
+
+
+def validate_sha256(file, url):
+    computed_sha256 = calculate_sha256(file)
+    expected_sha256 = get_sha256_from_url(url, file.stem)
+
+    if computed_sha256 == expected_sha256:
+        print_green(f"SUCCESS: {file.stem} sha256 passed!")
+    else:
+        raise Exception(
+            f"{file.stem} computed checksum ('{computed_sha256}') did not match expected checksum ('{expected_sha256}')!"
+        )
+
+
 def get_latest_ipsw_url(identifier, version):
     # Query the endpoint for list of builds
     response = requests.get(f"https://api.ipsw.me/v4/device/{identifier}?type=ipsw",
                             params={'Accept': 'application/json'},
                             timeout=3600)
+    response.raise_for_status()
     firmwares = json.loads(response.content)['firmwares']
     # Eliminate builds that are not for the requested versions
     version_firmwares = [item for item in firmwares if item['version'].split('.')[0] == version]
@@ -39,75 +88,97 @@ def get_latest_ipsw_url(identifier, version):
     return version_firmwares[0]['url']
 
 
-def download_if_necessary(parent, url):
-    base_file = url.split('/')[-1]
-    target = parent.joinpath(base_file)
+def download_if_necessary(item):
+    base_file = item['file_url'].split('/')[-1]
+    target = item['containing_folder'].joinpath(base_file)
+    target.parent.mkdir(exist_ok=True)
     if target.exists():
-        print(f"{base_file} already downloaded, skipping...")
+        print_yellow(f"SKIP: {base_file} already downloaded!")
     else:
-        print(f"{base_file} downloading...")
-        response = requests.get(url, timeout=3600)
+        print_green(f"INFO: {base_file} downloading...")
+        response = requests.get(item['file_url'], timeout=3600)
         response.raise_for_status()
         with open(target, 'xb') as file:
             file.write(response.content)
 
+    sha_url = item['sha_url']
+    if sha_url:
+        validate_sha256(target, sha_url)
+
 
 def download_items(targets, containing_folder):
+    items = []
     for target in targets:
-        items = []
         if target == 'arch':
-            subfolder = 'Arch Linux'
+            arch_day = '.01'
+            arch_date = datetime.datetime.now(datetime.timezone.utc).strftime("%Y.%m") + arch_day
 
-            arch_date = datetime.datetime.now(datetime.timezone.utc).strftime("%Y.%m") + ".01"
-            items += [
-                f"https://mirrors.edge.kernel.org/archlinux/iso/{arch_date}/archlinux-{arch_date}-x86_64.iso"
-            ]
+            base_arch_url = f"https://mirrors.edge.kernel.org/archlinux/iso/{arch_date}"
+            items += [{
+                'containing_folder': containing_folder.joinpath('Arch Linux'),
+                'file_url': f"{base_arch_url}/archlinux-{arch_date}-x86_64.iso",
+                'sha_url': f"{base_arch_url}/sha256sums.txt"
+            }]
 
         if target == 'fedora':
             fedora_arches = ['aarch64', 'x86_64']
-            subfolder = target.capitalize()
+            subfolder = containing_folder.joinpath(target.capitalize())
 
             # Constants to update
             fedora_ver = 37
             server_iso_ver = 1.7
             workstation_iso_ver = 1.7
-            base_fedora_url = f"https://download.fedoraproject.org/pub/fedora/linux/releases/{fedora_ver}"
+
+            # Base URLs
+            base_fedora_file_url = f"https://download.fedoraproject.org/pub/fedora/linux/releases/{fedora_ver}"
+            base_fedora_checksum_url = f"https://getfedora.org/static/checksums/{fedora_ver}/iso/"
 
             # Server
-            items += [
-                f"{base_fedora_url}/Server/{arch}/iso/Fedora-Server-netinst-{arch}-{fedora_ver}-{server_iso_ver}.iso"
-                for arch in fedora_arches
-            ]
+            for arch in fedora_arches:
+                items += [{
+                    'containing_folder':
+                    subfolder,
+                    'file_url':
+                    f"{base_fedora_file_url}/Server/{arch}/iso/Fedora-Server-netinst-{arch}-{fedora_ver}-{server_iso_ver}.iso",
+                    'sha_url':
+                    f"{base_fedora_checksum_url}/Fedora-Server-{fedora_ver}-{server_iso_ver}-{arch}-CHECKSUM"
+                }]
             # Workstation
-            items += [
-                f"{base_fedora_url}/Workstation/{arch}/iso/Fedora-Workstation-Live-{arch}-{fedora_ver}-{workstation_iso_ver}.iso"
-                for arch in fedora_arches
-            ]
+            for arch in fedora_arches:
+                items += [{
+                    'containing_folder':
+                    subfolder,
+                    'file_url':
+                    f"{base_fedora_file_url}/Workstation/{arch}/iso/Fedora-Workstation-Live-{arch}-{fedora_ver}-{workstation_iso_ver}.iso",
+                    'sha_url':
+                    f"{base_fedora_checksum_url}/Fedora-Workstation-{fedora_ver}-{server_iso_ver}-{arch}-CHECKSUM"
+                }]
 
         elif target == 'ipsw':
             mac_versions = ['13', '12']
-            subfolder = 'macOS/VM'
 
-            items += [
-                get_latest_ipsw_url('VirtualMac2,1', mac_version) for mac_version in mac_versions
-            ]
+            for mac_version in mac_versions:
+                items += [{
+                    'containing_folder': base_folder.joinpath('macOS', 'VM'),
+                    'file_url': get_latest_ipsw_url('VirtualMac2,1', mac_version),
+                    'sha_url': None
+                }]
 
-        elif targets == 'rpios':
-            subfolder = 'Raspberry Pi OS'
+        elif target == 'rpios':
             rpi_arches = ['armhf', 'arm64']
             rpi_date = '2022-09-26/2022-09-22'
             deb_ver = 'bullseye'
 
-            items += [
-                f"https://downloads.raspberrypi.org/raspios_lite_{rpi_arch}/images/raspios_lite_{rpi_arch}-{rpi_date}-raspios-{deb_ver}-{rpi_arch}-lite.img.xz"
-                for rpi_arch in rpi_arches
-            ]
+            for rpi_arch in rpi_arches:
+                base_rpi_url = f"https://downloads.raspberrypi.org/raspios_lite_{rpi_arch}/images/raspios_lite_{rpi_arch}-{rpi_date}-raspios-{deb_ver}-{rpi_arch}-lite.img.xz"
+                items += [{
+                    'containing_folder': containing_folder.joinpath('Raspberry Pi OS'),
+                    'file_url': base_rpi_url,
+                    'sha_url': base_rpi_url + '.sha256'
+                }]
 
-        target_folder = containing_folder.joinpath(subfolder)
-        target_folder.mkdir(exist_ok=True)
-
-        for item in items:
-            download_if_necessary(target_folder, item)
+    for item in items:
+        download_if_necessary(item)
 
 
 if __name__ == '__main__':
