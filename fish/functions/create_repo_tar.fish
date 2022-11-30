efe2085f78c8c0f766720e2771a5ccfc9ce3ee2f
Nathan Chancellor <nathan@kernel.org>
2022-11-29 21:43:08 -0700

    fish: Add function to create a tarball from a repository
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ fish/functions/create_repo_tar.fish
@@ -0,0 +1,27 @@
+#!/usr/bin/env fish
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2022 Nathan Chancellor
+
+function create_repo_tar -d "Create a tarball of a git repo from just the tracked files"
+    if test (count $argv) -gt 0
+        set repo $argv[1]
+        if not test -d $repo
+            print_error "$repo does not exist?"
+            return 1
+        end
+    else
+        set repo (realpath $PWD)
+    end
+
+    set output $repo/dist/(basename $repo)-(date +%Y-%m-%d-%H-%M).tar.zst
+
+    mkdir -p (dirname $output)
+    tar \
+        --create \
+        --directory $repo \
+        --file $output \
+        --zstd \
+        (git -C $repo ls-files)
+
+    echo "File is now available at: $output"
+end
