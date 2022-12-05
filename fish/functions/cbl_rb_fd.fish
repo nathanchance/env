67fd801207d4e2b5c169c54f6a5ec9ebbcc0f060
Nathan Chancellor <nathan@kernel.org>
2022-12-05 12:04:20 -0700

    env: Update to next-20221205 and 6.1-rc8
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_rb_fd.fish
+++ fish/functions/cbl_rb_fd.fish
@@ -20,13 +20,6 @@ function cbl_rb_fd -d "Rebase generic Fedora kernel on latest linux-next"
     for hash in $ln_commits
         git -C $CBL_BLD_P/linux-next fp -1 --stdout $hash | git am; or return
     end
-    # https://github.com/ClangBuiltLinux/linux/issues/1767
-    git diff 47f68266d6ad94860c6cd9d2145cb91350b47e43^..327b555ed078dde9e119fee497d7ae60b5b1dd62 | git ap -R; or return
-    crl https://git.kernel.org/efi/efi/p/71ed3fb090f8b3fb433d946fb8c68053f4a42bd8 | git ap; or return
-    crl https://git.kernel.org/efi/efi/p/6736ebb6e18898978f8e49d6ee9662e34993e176 | git ap; or return
-    git ac -m "Apply updated version of 'arm64: efi: Move runtime services asm wrapper out of .text'
-
-Link: https://github.com/ClangBuiltLinux/linux/issues/1767"
 
     # Build kernel
     cbl_bld_krnl_rpm --cfi --lto arm64; or return
