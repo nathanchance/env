02b73c0500ada67181beee8f7446231500867843
Nathan Chancellor <nathan@kernel.org>
2022-12-07 11:17:58 -0700

    fish: get_dev_img: Add support for armv7
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/get_dev_img.fish
+++ fish/functions/get_dev_img.fish
@@ -4,6 +4,8 @@
 
 function get_dev_img -d "Print the default development container image for the host architecture"
     switch (uname -m)
+        case 'armv7*'
+            echo dev/suse
         case aarch64
             echo dev/fedora
         case x86_64
