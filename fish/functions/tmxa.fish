fd0f5affdcd1d30c9ee03b025fe400bbbf8887b5
Nathan Chancellor <nathan@kernel.org>
2022-12-08 22:40:13 -0700

    env: Add support for using tmuxp on Raspberry Pi 3
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/tmxa.fish
+++ fish/functions/tmxa.fish
@@ -7,12 +7,7 @@ function tmxa -d "Attach to a tmux session if it exists, start a new one if not"
         switch $LOCATION
             case generic workstation
                 set tmuxp_cfg $LOCATION
-            case pi
-                # Only Raspberry Pi 4
-                if test (uname -m) = aarch64
-                    set tmuxp_cfg test
-                end
-            case honeycomb test-{desktop-intel,desktop-amd,laptop-intel}
+            case honeycomb pi test-{desktop-intel,desktop-amd,laptop-intel}
                 set tmuxp_cfg test
         end
         if set -q tmuxp_cfg
