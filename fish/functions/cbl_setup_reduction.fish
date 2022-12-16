0033cc6c5d4b1060f221fad0330446e654b6ba94
Nathan Chancellor <nathan@kernel.org>
2022-12-16 12:11:12 -0700

    fish: Add function to prepare for cvise reduction
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ fish/functions/cbl_setup_reduction.fish
@@ -0,0 +1,57 @@
+#!/usr/bin/env fish
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2022 Nathan Chancellor
+
+function cbl_setup_reduction -d "Build good and bad versions of LLVM for cvise reductions"
+    if not test -e llvm/CMakeLists.txt
+        print_error "Not in an LLVM tree?"
+        return 1
+    end
+
+    set bad_sha $argv[1]
+    if test -z "$bad_sha"
+        print_error "No bad sha provided?"
+        return 1
+    end
+    set bad_sha (git sha $bad_sha)
+    set good_sha (git sha $bad_sha^)
+
+    set -g tmp_dir (mktemp -d -p $TMP_FOLDER)
+    for sha in $good_sha $bad_sha
+        git checkout $sha; or return
+
+        if test $sha = $bad_sha
+            set folder bad
+        else
+            set folder good
+        end
+
+        $CBL_GIT/tc-build/build-llvm.py \
+            --assertions \
+            --build-folder $tmp_dir/build/llvm-$folder \
+            --build-stage1-only \
+            --install-folder $tmp_dir/install/llvm-$folder \
+            --install-stage1-only \
+            --llvm-folder $PWD \
+            --projects 'clang;lld'; or return
+    end
+
+    set cvise $dest/cvise
+    mkdir -p $cvise
+
+    set cvise_test $cvise/test.fish
+    echo '#!/usr/bin/env fish
+
+set install_folder (dirname (realpath (status dirname)))/install
+set bad_clang $install_folder/llvm-bad/bin/clang
+set good_clang $install_folder/llvm-good/bin/clang
+
+' >$cvise_test
+    chmod +x $cvise_test
+
+    git -C $cvise init; or return
+    git -C $cvise add test.fish; or return
+    git -C $cvise commit -m "Initial commit"; or return
+
+    echo "cvise reduction has been prepared at: $tmp_dir"
+end
