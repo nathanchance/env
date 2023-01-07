7a96e3c29823d9d7af828f43dd71ee61217a0555
Nathan Chancellor <nathan@kernel.org>
2023-01-06 17:49:34 -0700

    python: cbl_report: Add new system
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- python/scripts/cbl_report.py
+++ python/scripts/cbl_report.py
@@ -179,6 +179,7 @@ def create_report_file(report_file, report_date):
         'a Raspberry Pi 4',
         'a Raspberry Pi 3',
         'a SolidRun Honeycomb LX2',
+        'an Ampere Altra Developer Platform',
         'an Intel-based desktop',
         'an AMD-based desktop',
         'an Intel-based laptop'
