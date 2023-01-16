7a7d003d7907d4c440fa32cac77b950a4f51c7de
Nathan Chancellor <nathan@kernel.org>
2023-01-16 13:01:24 -0700

    env: Update to next-20220116
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_rb_fd.fish
+++ fish/functions/cbl_rb_fd.fish
@@ -14,18 +14,12 @@ function cbl_rb_fd -d "Rebase generic Fedora kernel on latest linux-next"
 
     # Patching
     set -a patches https://lore.kernel.org/all/20221130070511.46558-1-vdasa@vmware.com/ # VMCI: Use threaded irqs instead of tasklets
-    set -a patches https://lore.kernel.org/all/20230108224057.354438-2-beanhuo@iokpp.de/ # scsi: ufs: core: bsg: Fix sometimes-uninitialized warnings
     for patch in $patches
         b4 shazam -l -P _ -s $patch; or return
     end
     for hash in $ln_commits
         git -C $CBL_BLD_P/linux-next fp -1 --stdout $hash | git am; or return
     end
-    # https://lore.kernel.org/Y73PAtm6FPuT+1cM@dev-arch.thelio-3990X/
-    git fp -2 --stdout a7334dc70496bb0ce | git ap -R; or return
-    git f https://git.kernel.org/pub/scm/linux/kernel/git/efi/efi.git/ urgent; or return
-    git fp -2 --stdout b9c66533400fad0f31aed561bae92638986b6b28 | git ap; or return
-    git ac -m "Apply updated version of 'efi: Follow-up fixes for EFI runtime stack'"
 
     # Build kernel
     cbl_bld_krnl_rpm --cfi --lto arm64; or return
