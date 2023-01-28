64aec6f7d2a5f28974b12931805e367084428785
Nathan Chancellor <nathan@kernel.org>
2023-01-27 22:34:45 -0700

    env: Update to next-20220127
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_rb_fd.fish
+++ fish/functions/cbl_rb_fd.fish
@@ -13,13 +13,20 @@ function cbl_rb_fd -d "Rebase generic Fedora kernel on latest linux-next"
     git rh origin/master
 
     # Patching
-    set -a patches https://lore.kernel.org/all/20230123164700.1074064-1-suzuki.poulose@arm.com/ # coresight: Fix uninitialised variable use in coresight_disable
+    set -a patches https://lore.kernel.org/all/20230127162906.872395-1-trix@redhat.com/ # udf: remove reporting loc in debug output
+    set -a patches https://lore.kernel.org/all/20230127221418.2522612-1-arnd@kernel.org/ # gpu: host1x: fix uninitialized variable use
     for patch in $patches
         b4 shazam -l -P _ -s $patch; or return
     end
+    # amdgpu: fix build on non-DCN platforms.
+    crl 'https://cgit.freedesktop.org/drm/drm/patch/?id=f439a959dcfb6b39d6fd4b85ca1110a1d1de1587' | git am; or return
     for hash in $ln_commits
         git -C $CBL_BLD_P/linux-next fp -1 --stdout $hash | git am; or return
     end
+    git diff 9d0bccbd7160aa79825b11bf8f4c19cdb9b02b65^..f3db8be4bf6788fa84c997f63143717138ab6b9f | git ap -R; or return
+    git ac -m 'kbuild: Revert recent setlocalversion series
+
+Link: https://lore.kernel.org/Y9QmChqp0WEZSk+H@dev-arch.thelio-3990X/'; or return
 
     # Build kernel
     cbl_bld_krnl_rpm --cfi --lto arm64; or return
