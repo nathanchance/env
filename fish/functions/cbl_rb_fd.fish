8e5883ac199890cc56350ed15d0590dcea8dabe1
Nathan Chancellor <nathan@kernel.org>
2023-02-01 11:22:07 -0700

    env: Update to next-20230201
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_rb_fd.fish
+++ fish/functions/cbl_rb_fd.fish
@@ -15,6 +15,7 @@ function cbl_rb_fd -d "Rebase generic Fedora kernel on latest linux-next"
     # Patching
     set -a patches https://lore.kernel.org/all/20230127162906.872395-1-trix@redhat.com/ # udf: remove reporting loc in debug output
     set -a patches https://lore.kernel.org/all/20230127221418.2522612-1-arnd@kernel.org/ # gpu: host1x: fix uninitialized variable use
+    set -a patches https://lore.kernel.org/all/20230201-f2fs-fix-single-length-bitfields-v1-1-e386f7916b94@kernel.org/ # f2fs: Fix type of single bit bitfield in f2fs_io_info
     for patch in $patches
         b4 shazam -l -P _ -s $patch; or return
     end
