a19dc3c2252286580eb646411f4bc9cfd023c834
Nathan Chancellor <nathan@kernel.org>
2023-02-02 13:14:20 -0700

    fish: kpch: Automatically figure out commit range on b4 managed branches
    There is no need to run get_maintainer.pl, the To and Cc are a part of
    the commits themselves in b4 managed branches.
    
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/kpch.fish
+++ fish/functions/kpch.fish
@@ -4,19 +4,28 @@
 
 function kpch -d "Run checkpatch.pl and get_maintainer.pl on a patch"
     in_kernel_tree; or return
+    if string match -qr b4/ (git bn)
+        set b4_branch true
+    end
 
     if test (count $argv) -eq 0
-        set rev HEAD~1..HEAD
+        if set -q b4_branch
+            kchp -g (b4 prep --show-info | string match -gr '(?:start|end)-commit:\s+([a-z0-9]{40})' | string join ..)
+        else
+            set rev HEAD~1..HEAD
+        end
     else
         set rev $argv[1]
     end
 
-    for sha in (git log --format=%H --no-merges --reverse $rev)
-        set title Commit (git kf $sha)
-        set header (for i in (seq 1 (string length "$title")); printf "-"; end)
-        printf "\n%s\n%s\n%s\n\n" $header "$title" $header
+    if not set -q b4_branch
+        for sha in (git log --format=%H --no-merges --reverse $rev)
+            set title Commit (git kf $sha)
+            set header (for i in (seq 1 (string length "$title")); printf "-"; end)
+            printf "\n%s\n%s\n%s\n\n" $header "$title" $header
 
-        kchp -g $sha
-        git fp -1 --stdout $sha | kgtm
+            kchp -g $sha
+            git fp -1 --stdout $sha | kgtm
+        end
     end
 end
