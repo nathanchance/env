684d2d9bfbdfcc23afd99cdbcd881b47a1f8a1c2
Nathan Chancellor <nathan@kernel.org>
2023-02-13 17:49:01 -0700

    fish: cbl_bld_qemu: Slim down dynamic dependencies and use pending series over revert
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_bld_qemu.fish
+++ fish/functions/cbl_bld_qemu.fish
@@ -34,8 +34,6 @@ function cbl_bld_qemu -d "Build QEMU for use with ClangBuiltLinux"
             git -C $qemu_src submodule update --recursive
 
             # Reverts
-            # https://lore.kernel.org/Y+Pf0q6LmQKN+FHo@dev-arch.thelio-3990X/
-            set -a reverts eac7a7791bb6d719233deed750034042318ffd56 # x86: don't let decompressed kernel image clobber setup_data
             for revert in $reverts
                 git -C $qemu_src revert --no-edit $revert; or return
             end
@@ -43,10 +41,12 @@ function cbl_bld_qemu -d "Build QEMU for use with ClangBuiltLinux"
             # Patches from mailing lists
             # https://lore.kernel.org/Y88BmxzRqtnpAsWG@dev-arch.thelio-3990X/
             set -a b4_patches https://lore.kernel.org/all/20230118095751.49728-2-philmd@linaro.org/ # hw/pci-host/gt64120: Fix PCI I/O config register endianness
+            # https://lore.kernel.org/Y+Pf0q6LmQKN+FHo@dev-arch.thelio-3990X/
+            set -a b4_patches https://lore.kernel.org/all/20230208211212.41951-(seq 2 8)-mst@redhat.com # revert RNG seed mess
             if set -q b4_patches
                 pushd $qemu_src; or return
                 for patch in $b4_patches
-                    b4 shazam -l -P _ $patch; or return
+                    b4 am -l -o - -P _ $patch | git ap; or return
                 end
                 popd
             end
@@ -65,7 +65,48 @@ function cbl_bld_qemu -d "Build QEMU for use with ClangBuiltLinux"
         mkdir -p $qemu_bld
         pushd $qemu_bld; or return
 
-        $qemu_src/configure --disable-curl --prefix=$PREFIX; or return
+        $qemu_src/configure \
+            --disable-alsa \
+            --disable-bochs \
+            --disable-bpf \
+            --disable-bzip2 \
+            --disable-capstone \
+            --disable-cloop \
+            --disable-coreaudio \
+            --disable-curl \
+            --disable-dmg \
+            --disable-docs \
+            --disable-gcrypt \
+            --disable-glusterfs \
+            --disable-gnutls \
+            --disable-gtk \
+            --disable-keyring \
+            --disable-l2tpv3 \
+            --disable-libdaxctl \
+            --disable-libiscsi \
+            --disable-libnfs \
+            --disable-libssh \
+            --disable-libusb \
+            --disable-linux-aio \
+            --disable-lzo \
+            --disable-nettle \
+            --disable-opengl \
+            --disable-oss \
+            --disable-parallels \
+            --disable-png \
+            --disable-qed \
+            --disable-qom-cast-debug \
+            --disable-sdl \
+            --disable-snappy \
+            --disable-tpm \
+            --disable-user \
+            --disable-vde \
+            --disable-vdi \
+            --disable-vnc \
+            --disable-vvfat \
+            --disable-zstd \
+            --disable-xen \
+            --prefix=$PREFIX; or return
         make -skj(nproc) install; or return
         popd
     end
