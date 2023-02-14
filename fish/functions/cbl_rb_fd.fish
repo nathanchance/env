34159a7f20778c7625b0418d1b08ad9c3d1e01a4
Nathan Chancellor <nathan@kernel.org>
2023-02-13 17:52:39 -0700

    env: Update to next-20230213
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_rb_fd.fish
+++ fish/functions/cbl_rb_fd.fish
@@ -13,11 +13,18 @@ function cbl_rb_fd -d "Rebase generic Fedora kernel on latest linux-next"
     git rh origin/master
 
     # Patching
+    set -a patches https://lore.kernel.org/all/20230213101220.3821689-1-arnd@kernel.org/ # cxl: avoid returning uninitialized error code
     for patch in $patches
         b4 shazam -l -P _ -s $patch; or return
     end
     for hash in $ln_commits
         git -C $CBL_BLD_P/linux-next fp -1 --stdout $hash | git am; or return
+    end
+    # https://lore.kernel.org/Y+rSXg14z1Myd8Px@dev-arch.thelio-3990X/
+    set -a reverts 31b4b6730fd4f5d503c9f23619c920ce7b794754 # driver core: add error handling for devtmpfs_create_node()
+    set -a reverts 90a9d5ff225267b3376f73c19f21174e3b6d7746 # devtmpfs: add debug info to handle()
+    for revert in $reverts
+        git revert --no-edit $revert; or return
     end
 
     # Build kernel
