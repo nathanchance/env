e26efdebaabf8c61c04769d6820c2cadd3b1fa82
Nathan Chancellor <nathan@kernel.org>
2023-02-21 09:09:22 -0700

    fish: has_container_manager: Handle docker on macOS
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/has_container_manager.fish
+++ fish/functions/has_container_manager.fish
@@ -3,5 +3,6 @@
 # Copyright (C) 2022-2023 Nathan Chancellor
 
 function has_container_manager -d "Returns true if either docker or podman is installed"
+    test "$LOCATION" = mac; and return 1 # OrbStack has a docker wrapper, ignore it
     command -q docker; or command -q podman
 end
