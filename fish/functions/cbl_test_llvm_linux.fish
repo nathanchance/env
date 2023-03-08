04e44d125a9e14858eea50bcb657374d020cd3cb
Nathan Chancellor <nathan@kernel.org>
2023-03-08 13:36:10 -0700

    fish: cbl_test_llvm_linux: Stop if mainline builds fail
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_test_llvm_linux.fish
+++ fish/functions/cbl_test_llvm_linux.fish
@@ -3,6 +3,6 @@
 # Copyright (C) 2021-2023 Nathan Chancellor
 
 function cbl_test_llvm_linux -d "Test stable and mainline Linux with all supported versions of LLVM"
-    cbl_test_llvm_mainline_linux
+    cbl_test_llvm_mainline_linux; or return
     cbl_test_llvm_stable_linux
 end
