0b2108ec57ceb9c04b10a8a3a93b61c52b68a1cb
Nathan Chancellor <nathan@kernel.org>
2023-03-11 16:24:07 -0700

    fish: cbl_test_llvm_linux: Update and simplify
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_test_llvm_linux.fish
+++ fish/functions/cbl_test_llvm_linux.fish
@@ -3,6 +3,42 @@
 # Copyright (C) 2021-2023 Nathan Chancellor
 
 function cbl_test_llvm_linux -d "Test stable and mainline Linux with all supported versions of LLVM"
-    cbl_test_llvm_mainline_linux; or return
-    cbl_test_llvm_stable_linux
+    in_container_msg -c; or return
+
+    set targets $argv
+    if test -z "$targets"
+        set targets mainline stable
+    end
+
+    for target in $targets
+        switch $target
+            case mainline
+                set linux_folders $CBL_BLD_C/linux
+                if not test -d $linux_folders
+                    mkdir -p (dirname $linux_folders)
+                    git clone https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/ $linux_folders
+                end
+
+            case stable
+                set base $CBL_BLD_C/linux-stable
+                cbl_upd_stbl_wrktrs $base
+                set linux_folders $base-$CBL_STABLE_VERSIONS
+        end
+
+        for linux_folder in $linux_folders
+            git -C $linux_folder pull --rebase
+
+            for ver in (get_latest_stable_llvm_version $LLVM_VERSIONS_KERNEL)
+                if not test -x $CBL_TC_LLVM_STORE/$ver/bin/clang-(string split -f 1 -m 1 . $ver)
+                    print_error "LLVM $ver not available in $CBL_TC_LLVM_STORE!"
+                    return 1
+                end
+
+                cbl_lkt \
+                    --build-folder $TMP_BUILD_FOLDER/cbl_test_llvm_linux \
+                    --linux-folder $linux_folder \
+                    --llvm-prefix $CBL_TC_LLVM_STORE/$ver; or return
+            end
+        end
+    end
 end
---
+++ fish/functions/get_latest_stable_llvm_version.fish
@@ -0,0 +1,20 @@
+#!/usr/bin/env fish
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2023 Nathan Chancellor
+
+function get_latest_stable_llvm_version -d 'Get the latest stable version for an LLVM major version'
+    for arg in $argv
+        switch $arg
+            case 11
+                echo $arg.1.0
+            case 12 13
+                echo $arg.0.1
+            case 14
+                echo $arg.0.6
+            case 15
+                echo $arg.0.7
+            case 16
+                echo $arg.0.0-rc4
+        end
+    end
+end
