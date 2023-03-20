b01be2fdecc2209e19015a76ef947f1b963ff07f
Nathan Chancellor <nathan@kernel.org>
2023-03-20 16:02:36 -0700

    fish: glr: Handle authenticated requests
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/glr.fish
+++ fish/functions/glr.fish
@@ -3,5 +3,10 @@
 # Copyright (C) 2021-2023 Nathan Chancellor
 
 function glr -d "Get latest release of software from GitHub"
-    crl "https://api.github.com/repos/$argv[1]/releases/latest" | jq -r .tag_name
+    if set -q GITHUB_TOKEN
+        set -a crl_args \
+            -H "Accept: application/vnd.github+json" \
+            -H "Authorization: Bearer $GITHUB_TOKEN"
+    end
+    crl $crl_args "https://api.github.com/repos/$argv[1]/releases/latest" | jq -r .tag_name
 end
