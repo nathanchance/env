3d8fca55bdf88bce0ed1d300b4e91b2feb7803c7
Nathan Chancellor <nathan@kernel.org>
2023-03-20 15:57:18 -0700

    fish: updfull: Require running in host environment
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/updfull.fish
+++ fish/functions/updfull.fish
@@ -3,6 +3,8 @@
 # Copyright (C) 2022-2023 Nathan Chancellor
 
 function updfull -d "Update host machine, shell environment, and main distrobox container"
+    in_container_msg -h; or return
+
     upd -y \
         env \
         fisher \
