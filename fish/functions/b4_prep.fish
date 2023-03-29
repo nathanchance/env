199414ed8e470cb7a9cf1f935eaa4c1831f81e37
Nathan Chancellor <nathan@kernel.org>
2023-03-29 11:03:34 -0700

    fish: Add wrapper around 'b4 prep'
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ fish/functions/b4_prep.fish
@@ -0,0 +1,19 @@
+#!/usr/bin/env fish
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2023 Nathan Chancellor
+
+function b4_prep -d "Wrapper around 'b4 prep'"
+    if test (count $argv) -ne 3
+        print_error "b4_prep <repo> <branch> <b4_branch_name>"
+        return 1
+    end
+
+    set repo $argv[1]
+    set remote_branch $argv[2]
+    set local_branch $argv[3]
+
+    in_kernel_tree; or return
+
+    git f $repo $remote_branch
+    and b4 prep -f FETCH_HEAD -n $local_branch
+end
