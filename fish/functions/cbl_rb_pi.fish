0414f4103c7060090f7e6639541b2109ed8943ea
Nathan Chancellor <nathan@kernel.org>
2023-04-28 10:37:25 -0700

    env: Update to next-20230428
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_rb_pi.fish
+++ fish/functions/cbl_rb_pi.fish
@@ -22,10 +22,11 @@ function cbl_rb_pi -d "Rebase Raspberry Pi kernel on latest linux-next"
 
     # Patching
     set -a b4_patches https://lore.kernel.org/all/20230413-fixes-for-mt8195-hdmi-phy-v2-1-bbad62e64321@baylibre.com/ # phy: mediatek: hdmi: mt8195: fix uninitialized variable usage in pll_calc
-    set -a b4_patches https://lore.kernel.org/all/20230420-ext4-unused-variables-super-c-v1-1-138b6db6c21c@kernel.org/ # ext4: Fix unused iterator variable warnings
     for patch in $b4_patches
         b4 shazam -l -P _ -s $patch; or return
     end
+    crl https://lore.kernel.org/all/20230428162718.GA1099174@dev-arch.thelio-3990X/raw | git ap; or return
+    git ac -m "tty: Shuffle location of hung_up_tty_compat_ioctl()"; or return
     for patch in $crl_patches
         crl $patch | git am -3; or return
     end
