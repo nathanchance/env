a7f91c85390f2e2c8772000717216862d721e951
Nathan Chancellor <nathan@kernel.org>
2023-06-26 12:31:20 -0700

    env: Update to next-20230626
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_rb_fd.fish
+++ fish/functions/cbl_rb_fd.fish
@@ -17,13 +17,14 @@ function cbl_rb_fd -d "Rebase generic Fedora kernel on latest linux-next"
     # c3b60ab7a4dff6e6e608e685b70ddc3d6b2aca81 is the real problem but it is easier to just revert
     # the merge that brought in the whole series.
     set -a reverts 712557f210723101717570844c95ac0913af74d7 # Merge branch 'ptp-adjphase-cleanups'
+    # https://lore.kernel.org/8c7f9abd-4f84-7296-2788-1e130d6304a0@kernel.org/
+    set -a reverts 3f5f118bb657f94641ea383c7c1b8c09a5d46ea2 # af_unix: Call scm_recv() only after scm_set_cred().
     for revert in $reverts
         git revert --mainline 1 --no-edit $revert; or return
     end
     for patch in $b4_patches
         b4 shazam -l -P _ -s $patch; or return
     end
-    set -a crl_patches https://git.kernel.org/kvmarm/kvmarm/p/1caa71a7a600f7781ce05ef1e84701c459653663 # KVM: arm64: Restore GICv2-on-GICv3 functionality
     for patch in $crl_patches
         crl $patch | git am -3; or return
     end
--- fish/functions/cbl_rb_pi.fish
+++ fish/functions/cbl_rb_pi.fish
@@ -25,6 +25,8 @@ function cbl_rb_pi -d "Rebase Raspberry Pi kernel on latest linux-next"
     # c3b60ab7a4dff6e6e608e685b70ddc3d6b2aca81 is the real problem but it is easier to just revert
     # the merge that brought in the whole series.
     set -a reverts 712557f210723101717570844c95ac0913af74d7 # Merge branch 'ptp-adjphase-cleanups'
+    # https://lore.kernel.org/8c7f9abd-4f84-7296-2788-1e130d6304a0@kernel.org/
+    set -a reverts 3f5f118bb657f94641ea383c7c1b8c09a5d46ea2 # af_unix: Call scm_recv() only after scm_set_cred().
     for revert in $reverts
         git revert --mainline 1 --no-edit $revert; or return
     end
