bb96e62a8f812650ba93a8764d3c1d458a61d9c2
Nathan Chancellor <nathan@kernel.org>
2023-06-28 09:08:31 -0700

    fish: kbf: Make it more flexible
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/kbf.fish
+++ fish/functions/kbf.fish
@@ -3,5 +3,17 @@
 # Copyright (C) 2023 Nathan Chancellor
 
 function kbf -d "Prints a build folder specific to the current directory for use with O="
-    echo $TMP_BUILD_FOLDER/(basename $PWD)
+    if test (count $argv) -eq 0
+        set src $PWD
+    else
+        set src $argv
+    end
+
+    if string match -qr ^$CBL_WRKTR $src
+        set base (string join - (string split -f 2,3 -m 2 -r / $src))
+    else
+        set base (basename $src)
+    end
+
+    echo $TMP_BUILD_FOLDER/$base
 end
