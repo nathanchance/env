4a8fbcc33a35efc9d7e2e31601aa687657d55d9f
Nathan Chancellor <nathan@kernel.org>
2023-06-30 13:57:45 -0700

    fish: cbl_cl_software: Improvements
    - Allow multiple tool arguments
    - Do not run fzf in tmux
    - Use sudo, as this should only be run in a container
    
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_cl_software.fish
+++ fish/functions/cbl_cl_software.fish
@@ -5,28 +5,25 @@
 function cbl_cl_software -d "Clean up old versions of managed software"
     in_container_msg -c; or return
 
-    if test (count $argv) -ne 1
-        print_error "This function takes the tool whose versions will be cleaned up as an argument!"
-        return 1
-    end
+    for arg in $argv
+        switch $arg
+            case binutils
+                set folder $CBL_TC_BNTL_STORE
+                set binary as
+            case llvm
+                set folder $CBL_TC_LLVM_STORE
+                set binary clang
+            case qemu
+                set folder $CBL_QEMU_INSTALL
+                set binary qemu-system-x86_64
+        end
 
-    switch $argv[1]
-        case binutils
-            set folder $CBL_TC_BNTL_STORE
-            set binary as
-        case llvm
-            set folder $CBL_TC_LLVM_STORE
-            set binary clang
-        case qemu
-            set folder $CBL_QEMU_INSTALL
-            set binary qemu-system-x86_64
-    end
-
-    set folders_to_remove (fd -d 1 -t d . $folder -x basename | sort | fzf -m --preview="$folder/{}/bin/$binary --version")
-    if test -n "$folders_to_remove"
-        set rm_cmd \
-            rm -fr $folder/$folders_to_remove
-        print_cmd $rm_cmd
-        $rm_cmd
+        set folders_to_remove (fd -d 1 -t d . $folder -x basename | sort | run_cmd fzf -m --preview="$folder/{}/bin/$binary --version")
+        if test -n "$folders_to_remove"
+            set rm_cmd \
+                sudo rm -fr $folder/$folders_to_remove
+            print_cmd $rm_cmd
+            $rm_cmd
+        end
     end
 end
