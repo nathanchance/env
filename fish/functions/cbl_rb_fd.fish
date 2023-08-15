bb72fac9d48056e920c832c155b9b4009cbdc67a
Nathan Chancellor <nathan@kernel.org>
2023-08-15 13:31:12 -0700

    env: Update to next-20230815
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_rb_fd.fish
+++ fish/functions/cbl_rb_fd.fish
@@ -16,9 +16,6 @@ function cbl_rb_fd -d "Rebase generic Fedora kernel on latest linux-next"
     for revert in $reverts
         git revert --mainline 1 --no-edit $revert; or return
     end
-    set -a b4_patches https://lore.kernel.org/all/20230724121934.1406807-1-arnd@kernel.org/ # btrfs: remove unused pages_processed variable
-    set -a b4_patches https://lore.kernel.org/all/20230731123625.3766-1-christian.koenig@amd.com/ # drm/exec: use unique instead of local label
-    set -a b4_patches https://lore.kernel.org/all/(seq 1 4)-v2-d2762acaf50a+16d-iommu_group_locking2_jgg@nvidia.com/ # Fix device_lock deadlock on two probe() paths
     set -a b4_patches https://lore.kernel.org/all/20230809114216.4078-1-aweber.kernel@gmail.com/ # backlight: lp855x: Drop ret variable in brightness change function
     for patch in $b4_patches
         b4 shazam -l -P _ -s $patch; or return
