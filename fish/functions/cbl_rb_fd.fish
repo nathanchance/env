4f93621b41b8ba843098567472741225cdc3dccf
Nathan Chancellor <nathan@kernel.org>
2023-08-21 17:07:10 -0700

    env: Update to next-20230821
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_rb_fd.fish
+++ fish/functions/cbl_rb_fd.fish
@@ -16,7 +16,6 @@ function cbl_rb_fd -d "Rebase generic Fedora kernel on latest linux-next"
     for revert in $reverts
         git revert --mainline 1 --no-edit $revert; or return
     end
-    set -a b4_patches https://lore.kernel.org/all/20230809114216.4078-1-aweber.kernel@gmail.com/ # backlight: lp855x: Drop ret variable in brightness change function
     for patch in $b4_patches
         b4 shazam -l -P _ -s $patch; or return
     end
