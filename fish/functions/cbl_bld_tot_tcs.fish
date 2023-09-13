863dc2cbfd83a1200497233bda643c418a17f940
Nathan Chancellor <nathan@kernel.org>
2023-09-13 12:36:58 -0700

    fish: cbl_bld_tot_tcs: Add GitHub pull request handling for LLVM
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_bld_tot_tcs.fish
+++ fish/functions/cbl_bld_tot_tcs.fish
@@ -139,10 +139,18 @@ function cbl_bld_tot_tcs -d "Build LLVM and binutils from source for kernel deve
     end
 
     # Add in-review patches here
-    for revision in $revisions
+    for gh_pr in $gh_prs
+        if not gh -R llvm/llvm-project pr diff (basename $gh_pr) | git -C $llvm_project ap
+            set message "Failed to apply $gh_pr"
+            print_error "$message"
+            tg_msg "$message"
+            return 1
+        end
+    end
+    for phab_revision in $phab_revisions
         set -l git_ap_args
-        set -l base_rev (basename $revision)
-        if not crl "$revision?download=true" | git -C $llvm_project ap $git_ap_args
+        set -l base_rev (basename $phab_revision)
+        if not crl "$phab_revision?download=true" | git -C $llvm_project ap $git_ap_args
             set message "Failed to apply $base_rev"
             print_error "$message"
             tg_msg "$message"
