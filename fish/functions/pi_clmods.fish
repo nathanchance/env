e7ffc6cfc98fa7e213752743285225de925170aa
Nathan Chancellor <nathan@kernel.org>
2023-10-18 07:46:46 -0700

    fish: pi_clmods: Handle no folders to remove
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/pi_clmods.fish
+++ fish/functions/pi_clmods.fish
@@ -8,8 +8,10 @@ function pi_clmods -d "Clean up old modules on Raspberry Pi"
     end
     set folders (fd -d 1 $fd_args -E (uname -r) . /lib/modules)
 
-    set rm_cmd \
-        sudo rm -frv $folders
-    print_cmd $rm_cmd
-    $rm_cmd
+    if test -n "$folders"
+        set rm_cmd \
+            sudo rm -frv $folders
+        print_cmd $rm_cmd
+        $rm_cmd
+    end
 end
