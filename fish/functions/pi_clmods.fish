d70e353df7c409c6abba3e44583f8743aaec79d4
Nathan Chancellor <nathan@kernel.org>
2023-10-18 08:46:58 -0700

    fish: pi_clmods: Update for new Bookworm modules location
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/pi_clmods.fish
+++ fish/functions/pi_clmods.fish
@@ -3,7 +3,7 @@
 # Copyright (C) 2021-2023 Nathan Chancellor
 
 function pi_clmods -d "Clean up old modules on Raspberry Pi"
-    for dir in /lib/modules/*+
+    for dir in /lib/modules/*+ /lib/modules/*rpi*
         set -a fd_args -E (basename $dir)
     end
     set folders (fd -d 1 $fd_args -E (uname -r) . /lib/modules)
