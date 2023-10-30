351bb45957957b1078a4a2d9b97fd35baf02a41d
Nathan Chancellor <nathan@kernel.org>
2023-10-30 10:51:43 -0700

    fish: variables: 6.6
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/conf.d/variables.fish
+++ fish/conf.d/variables.fish
@@ -163,6 +163,7 @@ set -gx ICLOUD_DOCS_FOLDER /Users/$USER/Library/'Mobile Documents/com~apple~Clou
 
 # Versions of stable that I build locally
 set -gx CBL_STABLE_VERSIONS \
+    6.6 \
     6.5 \
     6.1 \
     5.15 \
