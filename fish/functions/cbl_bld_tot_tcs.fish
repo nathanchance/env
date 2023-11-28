a9f2aef490c29ef789246ecdaf2c006f81cfb29d
Nathan Chancellor <nathan@kernel.org>
2023-11-28 10:52:56 -0700

    fish: cbl_bld_tot_tcs: Replace revert of bc09ec696209b with forward fix
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_bld_tot_tcs.fish
+++ fish/functions/cbl_bld_tot_tcs.fish
@@ -128,8 +128,6 @@ function cbl_bld_tot_tcs -d "Build LLVM and binutils from source for kernel deve
     end
 
     # Add patches to revert here
-    # https://github.com/llvm/llvm-project/pull/70606#issuecomment-1809250417
-    set -a reverts https://github.com/llvm/llvm-project/commit/bc09ec696209b3aea74d49767b15c2f34e363933 # [CodeGen] Revamp counted_by calculations (#70606)
     for revert in $reverts
         set -l revert (basename $revert)
         if not git -C $llvm_project rv -n $revert
@@ -141,6 +139,10 @@ function cbl_bld_tot_tcs -d "Build LLVM and binutils from source for kernel deve
     end
 
     # Add in-review patches here
+    # Revert changes to clang/test/CodeGen/attr-counted-by.c to avoid conflicts with next application
+    git -C $llvm_project diff -R (string join '' b31cd07de5b7dfb435b062c80d85083e14f28321{^..,}) clang/test/CodeGen/attr-counted-by.c | git -C $llvm_project ap
+    # https://github.com/llvm/llvm-project/issues/73168
+    set -a gh_prs https://github.com/llvm/llvm-project/pull/73465 # [Clang] Use correct base expression for counted_by field (#73168)
     for gh_pr in $gh_prs
         if not gh -R llvm/llvm-project pr diff (basename $gh_pr) | git -C $llvm_project ap
             set message "Failed to apply $gh_pr"
