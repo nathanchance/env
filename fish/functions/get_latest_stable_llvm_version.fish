6913b7b41ef728e9603245c06494e2fd87aeb70f
Nathan Chancellor <nathan@kernel.org>
2023-11-28 10:22:38 -0700

    env: LLVM 17.0.6
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/get_latest_stable_llvm_version.fish
+++ fish/functions/get_latest_stable_llvm_version.fish
@@ -5,13 +5,11 @@
 function get_latest_stable_llvm_version -d 'Get the latest stable version for an LLVM major version'
     for arg in $argv
         switch $arg
-            case 17
-                echo $arg.0.5
+            case 17 16 14
+                echo $arg.0.6
             case 15
                 echo $arg.0.7
-            case 14 16
-                echo $arg.0.6
-            case 12 13
+            case 13 12
                 echo $arg.0.1
             case 11
                 echo $arg.1.0
--- python/pgo-llvm-builder/build.py
+++ python/pgo-llvm-builder/build.py
@@ -18,13 +18,11 @@ MACHINE = platform.machine()
 
 LLVM_REFS = {
     '18.0.0': 'origin/main',
-    '17.0.6': 'origin/release/17.x',
 }
 
 LLVM_VERSIONS = [
     '18.0.0',  # git
-    '17.0.6',  # latest branch
-    '17.0.5',  # latest tag
+    '17.0.6',
     '16.0.6',
     '15.0.7',
     '14.0.6',
