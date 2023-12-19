ee9d8fcf0078aed5a112e96a21570fb6f9a6e2ae
Nathan Chancellor <nathan@kernel.org>
2023-12-19 10:20:45 -0700

    fish: Add prep_config
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ fish/functions/prep_config.fish
@@ -0,0 +1,30 @@
+#!/usr/bin/env fish
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2023 Nathan Chancellor
+
+function prep_config -d "Prepare kernel configuration in build folder"
+    set src_cfg $argv[1]
+
+    set bld $argv[2]
+    if test -z "$bld"
+        set bld (kbf)
+    end
+    set dst_cfg $bld/.config
+
+    if string match -qr 'http(s?)://' $src_cfg
+        set url true
+    else if test -e $src_cfg
+        set local true
+    else
+        print_error "Could not handle $src_cfg?"
+        return 1
+    end
+
+    remkdir $bld
+    if set -q local
+        cp -v $src_cfg $dst_cfg
+    end
+    if set -q url
+        crl -o $dst_cfg $src_cfg
+    end
+end
