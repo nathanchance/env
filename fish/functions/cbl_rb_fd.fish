d3dfe244d5245d7f8db5be4827d4ec7a0aadb07b
Nathan Chancellor <nathan@kernel.org>
2023-12-21 14:57:13 -0700

    env: next-20231221
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_rb_fd.fish
+++ fish/functions/cbl_rb_fd.fish
@@ -16,19 +16,13 @@ function cbl_rb_fd -d "Rebase generic Fedora kernel on latest linux-next"
     for revert in $reverts
         git revert --mainline 1 --no-edit $revert; or return
     end
-    git show 0df8e97085946dd79c06720678a845778b6d6bf8 scripts/package/kernel.spec | git ap -R
-    git ac -m 'Partially revert "scripts: clean up IA-64 code"
-
-To make the following patch apply cleanly'
-    or return
-    set -a b4_patches https://lore.kernel.org/all/20231219155659.1591792-1-jtornosm@redhat.com/ # rpm-pkg: simplify installkernel %post
+    set -a b4_patches https://lore.kernel.org/all/20231219201719.1967948-1-jtornosm@redhat.com/ # rpm-pkg: simplify installkernel %post
     for patch in $b4_patches
         b4 shazam -l -P _ -s $patch; or return
     end
     for patch in $crl_patches
         crl $patch | git am -3; or return
     end
-    set -a ln_commits cce20159e877039e6cc1e8e12d1b05952c251a22 # NOTCBL: WIP: Add exports for generic out of line NUMA logging functions
     for hash in $ln_commits
         git -C $CBL_BLD_P/linux-next fp -1 --stdout $hash | git am; or return
     end
