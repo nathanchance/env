cc1af308c8df55cdfef990ba20f27463be4302b9
Nathan Chancellor <nathan@kernel.org>
2023-12-22 12:20:24 -0700

    env: next-20231222
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_rb_fd.fish
+++ fish/functions/cbl_rb_fd.fish
@@ -17,9 +17,14 @@ function cbl_rb_fd -d "Rebase generic Fedora kernel on latest linux-next"
         git revert --mainline 1 --no-edit $revert; or return
     end
     set -a b4_patches https://lore.kernel.org/all/20231219201719.1967948-1-jtornosm@redhat.com/ # rpm-pkg: simplify installkernel %post
+    set -a b4_patches https://lore.kernel.org/all/20231222-dma-xilinx-xdma-clang-fixes-v1-1-84a18ff184d2@kernel.org/ # dmaengine: xilinx: xdma: Fix operator precedence in xdma_prep_interleaved_dma()
+    set -a b4_patches https://lore.kernel.org/all/20231222-dma-xilinx-xdma-clang-fixes-v1-2-84a18ff184d2@kernel.org/ # dmaengine: xilinx: xdma: Fix initialization location of desc in xdma_channel_isr()
     for patch in $b4_patches
         b4 shazam -l -P _ -s $patch; or return
     end
+    # https://lore.kernel.org/20231221230153.GA1607352@dev-arch.thelio-3990X/
+    set -a crl_patches https://lore.kernel.org/all/2229136.1703246451@warthog.procyon.org.uk/raw # Fix oops in NFS
+    set -a crl_patches https://git.kernel.org/vkoul/dmaengine/p/3d0b2176e04261ab4ac095ff2a17db077fc1e46d # dmaengine: xilinx: xdma: statify xdma_prep_interleaved_dma
     for patch in $crl_patches
         crl $patch | git am -3; or return
     end
