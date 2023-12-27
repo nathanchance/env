26d366c3a6a19adb71604924e3469b4c24d4e2dd
Nathan Chancellor <nathan@kernel.org>
2023-12-27 10:04:40 -0700

    fish: cbl_bld_tot_tcs: Revert LLVM commit 0e46b49de43349f8cbb2a7d4c6badef6d16e31ae
    Along with a revert of a test added after that change that fails without
    it.
    
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_bld_tot_tcs.fish
+++ fish/functions/cbl_bld_tot_tcs.fish
@@ -130,6 +130,10 @@ function cbl_bld_tot_tcs -d "Build LLVM and binutils from source for kernel deve
     # Add patches to revert here
     # https://github.com/llvm/llvm-project/pull/72960#issuecomment-1866844679
     set -a reverts https://github.com/llvm/llvm-project/commit/a8081ed8ff0fd11fb8d5f4c83df49da909e49612 # [LoongArch] Allow delayed decision for ADD/SUB relocations (#72960)
+    # Implicitly depends on the next change in the reverts list so revert it for clean test results
+    set -a reverts https://github.com/llvm/llvm-project/commit/5cfc7b3342ce4de0bbe182b38baa8a71fc83f8f8 # [PowerPC] Add test after #75271 on PPC. NFC. (#75616)
+    # https://github.com/llvm/llvm-project/issues/76416
+    set -a reverts https://github.com/llvm/llvm-project/commit/0e46b49de43349f8cbb2a7d4c6badef6d16e31ae # Reapply "RegisterCoalescer: Add implicit-def of super register when coalescing SUBREG_TO_REG"
     for revert in $reverts
         set -l revert (basename $revert)
         if not git -C $llvm_project rv -n $revert
