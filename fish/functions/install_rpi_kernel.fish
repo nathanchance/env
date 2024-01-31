c47b4ac15a71dbc28762a809826fa8bbf7c5e32a
Nathan Chancellor <nathan@kernel.org>
2024-01-31 10:00:53 -0700

    fish: install_rpi_kernel: Use known prefix for cmdline.txt
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/install_rpi_kernel.fish
+++ fish/functions/install_rpi_kernel.fish
@@ -76,7 +76,7 @@ function install_rpi_kernel -d "Install Raspberry Pi kernel from a tarball"
 
     # Copy cmdline.txt because we are modifying os_prefix
     set cmdline $prefix/cmdline.txt
-    sudo cp -v (realpath /boot/cmdline.txt) $cmdline; or return
+    sudo cp -v (dirname $prefix)/cmdline.txt $cmdline; or return
 
     # Ensure that there is always a serial console option
     if grep -q console= $cmdline
