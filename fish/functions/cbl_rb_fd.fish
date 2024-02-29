4e2e5f14f13cce767c818b6c9abe658867e38fd0
Nathan Chancellor <nathan@kernel.org>
2024-02-29 14:33:49 -0700

    env: next-20240229
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_rb_fd.fish
+++ fish/functions/cbl_rb_fd.fish
@@ -16,9 +16,6 @@ function cbl_rb_fd -d "Rebase generic Fedora kernel on latest linux-next"
     git rh origin/master
 
     # Patching
-    # https://lore.kernel.org/20240228043534.GG11972@google.com/
-    set -a reverts 0c2f314a62342898813d5434945920f2090f44c8 # mm/zsmalloc: remove the deferred free mechanism
-    set -a reverts 2258bdebb55e3ad3d30fd3849ddb955ff36825de # mm/zsmalloc: don't hold locks of all pages when free_zspage()
     for revert in $reverts
         git revert --mainline 1 --no-edit $revert
         or return
@@ -31,9 +28,6 @@ function cbl_rb_fd -d "Rebase generic Fedora kernel on latest linux-next"
             return $ret
         end
     end
-    # https://lore.kernel.org/20240222190334.GA412503@dev-arch.thelio-3990X/
-    # https://lore.kernel.org/20240227192648.GA2621994@dev-arch.thelio-3990X/
-    set -a crl_patches https://git.kernel.org/vfs/vfs/p/57a220844820980f8e3de1c1cd9d112e6e73da83 # pidfs: default to n for now
     for patch in $crl_patches
         crl $patch | git am -3
         or begin
