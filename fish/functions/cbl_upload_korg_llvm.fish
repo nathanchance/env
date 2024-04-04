5b170ae6efab7b14fd80f36b41033228a9901a76
Nathan Chancellor <nathan@kernel.org>
2024-04-04 14:25:15 -0700

    fish: cbl_upload_korg_llvm: Automate announcements
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_upload_korg_llvm.fish
+++ fish/functions/cbl_upload_korg_llvm.fish
@@ -38,10 +38,66 @@ function cbl_upload_korg_llvm -d "Upload kernel.org toolchain releases with kup"
     for tar in $release_tars
         $kup put $tar $tar.asc /pub/tools/llvm/files/$tar.gz
         or return
+
+        set llvm_ver (string match -gr 'llvm-([0-9|.]+)' $tar)
+        if test (string split . $llvm_ver | count) != 3
+            print_error "Malformed LLVM version found ('$llvm_ver')?"
+            return 1
+        end
+        if not contains $llvm_ver $llvm_vers
+            set -a llvm_vers $llvm_ver
+        end
     end
     if test -n "$release_tars"
         $kup put index.html{,.asc} /pub/tools/llvm/index.html
         or return
+
+        if test (count $llvm_vers) = 1
+            set llvm_maj (string split -f 1 . $llvm_ver)
+            echo "Hi all,
+
+I have built and uploaded LLVM $llvm_ver to
+https://mirrors.edge.kernel.org/pub/tools/llvm/.
+
+If there are any issues found, please let us know via email or
+https://github.com/ClangBuiltLinux/linux/issues/new, so that we have an
+opportunity to get them fixed in main and backported before the $llvm_maj.x
+series is no longer supported.
+
+Cheers,
+Nathan" >msg
+
+            for korg_user in conor ojeda
+                set -a mutt_args -c $korg_user@kernel.org
+            end
+            echo "#!/usr/bin/env fish
+
+echo 'Contents of msg for mailing list announcement:'
+echo
+cat msg
+echo
+
+read -P 'Would you like to edit the mailing list announcement before sending? [Y/N] ' req
+if test (string lower \$req) = y
+    \$EDITOR msg
+end
+
+read -P 'Send mailing list announcement? [Y/N] ' req
+if test (string lower \$req) = y
+    mutt $mutt_args -s 'Prebuilt LLVM $llvm_ver uploaded' -- llvm@lists.linux.dev linux-kernel@vger.kernel.org <msg
+end
+
+read -P 'Would you like to link announcement on GitHub issue? [Y/N] ' req
+if test (string lower \$req) = y
+    read -P 'GitHub issue: ' gh_issue
+    read -P 'lore.kernel.org Message-ID: ' msg_id
+
+    if test -n \"\$gh_issue\"; and test -n \"\$msg_id\"
+        gh -R ClangBuiltLinux/linux issue comment \$gh_issue -b '$llvm_ver uploaded to kernel.org: https://lore.kernel.org/\$msg_id/'
+    end
+end" >announce
+            chmod +x announce
+        end
     end
 
     for tar in $prerelease_tars
