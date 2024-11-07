44d412c40947eb8d636f4987f9e06f3249859c97
Nathan Chancellor <nathan@kernel.org>
2024-11-07 12:16:33 -0700

    fish: cbl_bld_tot_tcs: Replace revert of 4d3c427f33956 with forward fix PR
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_bld_tot_tcs.fish
+++ fish/functions/cbl_bld_tot_tcs.fish
@@ -138,8 +138,6 @@ function cbl_bld_tot_tcs -d "Build LLVM and binutils from source for kernel deve
     end
 
     # Add patches to revert here
-    # https://github.com/llvm/llvm-project/pull/114195#issuecomment-2455173554
-    set -a reverts https://github.com/llvm/llvm-project/commit/4d3c427f339562b73938296d77103baf9ab9dc4b # [CodeGen] Use first EHLabel as a stop gate for live range shrinking (#114195)
     for revert in $reverts
         if string match -qr 'https?://' $revert
             set -l revert (basename $revert)
@@ -160,6 +158,8 @@ function cbl_bld_tot_tcs -d "Build LLVM and binutils from source for kernel deve
     end
 
     # Add in-review patches here
+    # https://github.com/llvm/llvm-project/pull/114195#issuecomment-2455173554
+    set -a gh_prs https://github.com/llvm/llvm-project/pull/115276 # [CodeGen][X86] LiveRangeShrink: fix increment after end
     for gh_pr in $gh_prs
         if gh_llvm pr view --json state (basename $gh_pr) | python3 -c "import json, sys; sys.exit(0 if json.load(sys.stdin)['state'] == 'MERGED' else 1)"
             print_warning "$gh_pr has already been merged, skipping..."
