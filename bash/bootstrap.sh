7ca9ed8ff352686d7fb89640a90124a26a8eaa7a
Nathan Chancellor <nathan@kernel.org>
2024-11-13 07:51:17 -0700

    bash/bootstrap.sh: Only download env tarball after Python is installed
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- bash/bootstrap.sh
+++ bash/bootstrap.sh
@@ -8,23 +8,6 @@ is_available() {
     command -v "$1" >/dev/null 2>&1
 }
 
-if is_available curl; then
-    dwnld_cmd="curl -LSs"
-elif is_available wget; then
-    dwnld_cmd="wget -q -O-"
-else
-    echo "[-] No suitable download command could be found!" >&2
-    exit 1
-fi
-
-$dwnld_cmd https://github.com/nathanchance/env/tarball/main | tar -C "$tmp_dir" -xvzf - --strip-components=1
-
-python_setup=$tmp_dir/python/setup
-if [ ! -d "$python_setup" ]; then
-    echo "[-] $python_setup could not be found, did download command fail?" >&2
-    exit 1
-fi
-
 for file in /etc/os-release /usr/lib/os-release; do
     # We don't care if this fails, we will fail on the next command
     if . $file >/dev/null 2>&1; then
@@ -40,6 +23,23 @@ if ! is_available python3; then
     else
         echo "[-] No suitable command could be found to install python3" >&2
     fi
+fi
+
+if is_available curl; then
+    dwnld_cmd="curl -LSs"
+elif is_available wget; then
+    dwnld_cmd="wget -q -O-"
+else
+    echo "[-] No suitable download command could be found!" >&2
+    exit 1
+fi
+
+$dwnld_cmd https://github.com/nathanchance/env/tarball/main | tar -C "$tmp_dir" -xvzf - --strip-components=1
+
+python_setup=$tmp_dir/python/setup
+if [ ! -d "$python_setup" ]; then
+    echo "[-] $python_setup could not be found, did download command fail?" >&2
+    exit 1
 fi
 
 "$python_setup"/"${ID_LIKE-$ID}".py "$@"
