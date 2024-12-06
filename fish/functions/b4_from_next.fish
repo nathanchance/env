53ba4165da26f887ede24e8efdaa0029ef839f9c
Nathan Chancellor <nathan@kernel.org>
2024-12-06 14:00:55 -0700

    fish: b4_from_next: Reduce indentation by using negation
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/b4_from_next.fish
+++ fish/functions/b4_from_next.fish
@@ -3,22 +3,21 @@
 # Copyright (C) 2024 Nathan Chancellor
 
 function b4_from_next -d "Run b4_prep after interactively selecting a base from Next/Trees"
-    if test -e Next/Trees
-        set parts (fzf <Next/Trees | awk '{print $3}' | string split '#')
-
-        if test (count $parts) -eq 2
-            echo "git repo: $parts[1]"
-            echo "git branch: $parts[2]"
-
-            read -l -P 'b4 branch arg: ' branch
-            b4_prep $parts $branch
-        else
-            print_error "Malformed parts?"
-            echo "parts: $parts"
-            return 1
-        end
-    else
+    if not test -e Next/Trees
         print_error "Not in a -next tree?"
         return 1
     end
+
+    set parts (fzf <Next/Trees | awk '{print $3}' | string split '#')
+    if test (count $parts) -ne 2
+        print_error "Malformed parts?"
+        echo "parts: $parts"
+        return 1
+    end
+
+    echo "git repo: $parts[1]"
+    echo "git branch: $parts[2]"
+    read -P 'b4 branch arg: ' branch
+
+    b4_prep $parts $branch
 end
