fcdfa794affd970b1a76ad67f78c508791b98789
Nathan Chancellor <nathan@kernel.org>
2024-12-18 21:06:20 -0700

    fish: cbl_setup_other_repos: Add fork to llvm-project
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_setup_other_repos.fish
+++ fish/functions/cbl_setup_other_repos.fish
@@ -3,7 +3,16 @@
 # Copyright (C) 2021-2023 Nathan Chancellor
 
 function cbl_setup_other_repos -d "Download other ClangBuiltLinux repos"
-    cbl_clone_repo llvm-project
+    begin
+        cbl_clone_repo llvm-project
+        and set folder $CBL_SRC_D/llvm-project
+        and if not string match -qr ^nathanchance (git -C $folder remote)
+            pushd $folder
+            and gh repo fork --remote --remote-name nathanchance
+            and git remote update nathanchance
+            and popd
+        end
+    end
     or return
 
     set repos_cbl_github \
