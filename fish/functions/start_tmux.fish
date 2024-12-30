e5064a615a1e2aeed030ed98a387730e4fb4f681
Nathan Chancellor <nathan@kernel.org>
2024-12-29 22:11:42 -0700

    fish: start_tmux: Do not start in systemd-nspawn environment
    This will never be desirable.
    
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/start_tmux.fish
+++ fish/functions/start_tmux.fish
@@ -13,6 +13,8 @@ function start_tmux -d "Start tmux under certain conditions (for use in config.f
     and not set -q TMUX
     # and we are not in a chroot (which may imply a tmux environment)...
     and not in_deb_chroot
+    # and we are not in a systemd-nspawn environment (which will only interact with the host's environment)
+    and not in_nspawn
     # and we have it installed,
     and command -q tmux
     # attempt to attach to a session named "main" while detaching everyone
