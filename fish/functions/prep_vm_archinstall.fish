c7917a048ecccb071f29026c4c6b62d2061fad20
Nathan Chancellor <nathan@kernel.org>
2024-12-31 16:48:48 -0700

    fish: Copy archinstall JSON files to new virtual machine for quick setup
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ fish/functions/prep_vm_archinstall.fish
@@ -0,0 +1,24 @@
+#!/usr/bin/env fish
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2024 Nathan Chancellor
+
+function prep_vm_archinstall -d "Prepare archinstall files for virtual machine installation"
+    read -P 'Password for virtual machine user: ' -s user_pass
+
+    set creds /tmp/creds.json
+    echo '{
+    "!root-password": null,
+    "!users": [
+        {
+            "!password": "'$user_pass'",
+            "sudo": true,
+            "username": "nathan"
+        }
+    ]
+}' >$creds
+
+    ssh_vm root transfer $ENV_FOLDER/configs/archinstall/vm_config.json :/root/config.json
+    ssh_vm root transfer $creds :/root/(basename $creds)
+
+    rm -f $creds
+end
--- fish/functions/ssh_vm.fish
+++ fish/functions/ssh_vm.fish
@@ -10,21 +10,67 @@ function ssh_vm -d "ssh into a VM running via cbl_vmm.py"
             switch $arg
                 case nathan root
                     set user $arg
+                case scp transfer
+                    set ssh_cmd scp
                 case '*'
-                    set -a cmd $arg
+                    if test "$ssh_cmd" = scp
+                        if set -q src
+                            set dst $arg
+                        else
+                            set src $arg
+                        end
+                    else
+                        set -a cmd $arg
+                    end
             end
         end
     end
-    set -q user; or set user nathan
+    set -q ssh_cmd; or set ssh_cmd ssh
     set -q port; or set port 8022
+    set -q user; or set user nathan
+
+    set user_host $user@localhost
 
     if not grep -q "^\[localhost\]:$port" $HOME/.ssh/known_hosts
-        set ssh_args \
+        set -a cmd_args \
             -o "StrictHostKeyChecking no"
     end
 
-    set ssh_cmd \
-        ssh $ssh_args -p $port $user@localhost $cmd
-    print_cmd $ssh_cmd
-    $ssh_cmd
+    switch $ssh_cmd
+        case scp
+            if not set -q src
+                print_error "No source and destination provided?"
+                return 1
+            end
+            if not set -q dst
+                print_error "No destination provided?"
+                return 1
+            end
+
+            if string match -qr ^: $src
+                set src $user_host$src
+            else if string match -qr ^: $dst
+                set dst $user_host$dst
+            else
+                print_error "Neither source nor destination have a colon to mark remote file?"
+                return 1
+            end
+
+            set -a cmd_args \
+                -P $port \
+                -r \
+                $src \
+                $dst
+
+        case ssh
+            set -a cmd_args \
+                -p $port \
+                $user_host \
+                $cmd
+    end
+
+    set full_cmd \
+        $ssh_cmd $cmd_args
+    print_cmd $full_cmd
+    $full_cmd
 end
