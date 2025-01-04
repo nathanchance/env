4a8f0cded0662e4952c4f60bc9a43142ae3c6445
Nathan Chancellor <nathan@kernel.org>
2025-01-03 23:11:45 -0700

    env: Add test tmux configuration for systemd-nspawn
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/tmxa.fish
+++ fish/functions/tmxa.fish
@@ -6,7 +6,11 @@ function tmxa -d "Attach to a tmux session if it exists, start a new one if not"
     if test -z "$TMUX"
         switch $LOCATION
             case aadp honeycomb pi test-{desktop-intel-{11700,n100},desktop-amd,laptop-intel}
-                set tmuxp_cfg test
+                if using_nspawn
+                    set tmuxp_cfg test-nspawn
+                else
+                    set tmuxp_cfg test-dbx
+                end
             case generic
                 if in_orb
                     set tmuxp_cfg primary
