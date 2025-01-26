28b07528c000cae570339b2e971d90cf8aa21346
Nathan Chancellor <nathan@kernel.org>
2025-01-25 17:31:56 -0700

    fish: rmbx: Ignore exit 130 from mutt
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/rmbx.fish
+++ fish/functions/rmbx.fish
@@ -22,4 +22,9 @@ function rmbx -d "Read an mbox directory created by lei"
     end
 
     mutt -f $mbx
+    set ret $status
+    if test $ret -eq 130
+        return 0
+    end
+    return $ret
 end
