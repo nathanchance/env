d61fdb2385f347f8f62811768ffc8e345f0818a2
Nathan Chancellor <nathan@kernel.org>
2025-02-26 14:25:20 -0700

    fish: cbl_bld_llvm_korg: Add '-b' and '-r'
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_bld_llvm_korg.fish
+++ fish/functions/cbl_bld_llvm_korg.fish
@@ -11,6 +11,10 @@ function cbl_bld_llvm_korg -d "Build (and optionally test) LLVM for kernel.org"
 
     for arg in $argv
         switch $arg
+            case -b --build-env
+                set build_env y
+            case -r --reset
+                set reset y
             case -t --test-linux
                 set test_linux y
             case '*'
@@ -28,6 +32,12 @@ function cbl_bld_llvm_korg -d "Build (and optionally test) LLVM for kernel.org"
         set -a repos_to_update m s
     end
 
+    if test "$reset" = y
+        for old_dir in $TMP_FOLDER/pgo-llvm-builder.*
+            rm -fr $old_dir
+        end
+    end
+
     # Permission could be requested in build.py but downloading
     # or updating the source may take a bit and sudo may time out
     # waiting for authorization
@@ -35,10 +45,23 @@ function cbl_bld_llvm_korg -d "Build (and optionally test) LLVM for kernel.org"
     sudo true
     or return
 
+    if test "$build_env" = y
+        set mach_dir /var/lib/machines/pgo-llvm-builder
+        if sudo test -e $mach_dir
+            sudo rm -r $mach_dir
+            or return
+        end
+
+        mkosi_bld $PYTHON_FOLDER/(path basename $mach_dir)
+        or return
+    end
+
     begin
         header "Updating sources"
 
         cbl_upd_src c $repos_to_update
+        # the above will take the longest, refresh the sudo timer
+        and sudo true
         and if not test -d $CBL_TC_BLD
             git clone -b personal https://github.com/nathanchance/tc-build $CBL_TC_BLD
         end
@@ -57,7 +80,7 @@ function cbl_bld_llvm_korg -d "Build (and optionally test) LLVM for kernel.org"
             --llvm-folder $CBL_SRC_C/llvm-project \
             --tc-build-folder $CBL_TC_BLD \
             --versions $llvm_vers
-        tg_msg "pgo-llvm-builder failed on $(uname -n) @ $(date) with return code '$ret'!"
+        tg_msg "pgo-llvm-builder failed on $(uname -n) @ $(date)!"
         return 1
     end
     bell
