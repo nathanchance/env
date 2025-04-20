a7076eccec765715450b92b773f1e2b27cb578ed
Nathan Chancellor <nathan@kernel.org>
2025-04-20 01:36:47 -0400

    fish: completions: Add initial completions for kmake
    This is inspired by scripts/bash-completion/make in the Linux kernel.
    
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ fish/completions/kmake.fish
@@ -0,0 +1,126 @@
+function __kmake_get_srctree
+    set tokens (commandline -xpc)
+    if set index (contains -i -- -C $tokens)
+        echo $tokens[(math $index + 1)]
+    else
+        echo $PWD
+    end
+end
+
+function __kmake_get_srcarch
+    for token in (commandline -xpc)
+        if string match -qr '^ARCH=' $token
+            set srcarch (string split -f 2 = $token)
+        end
+    end
+    if not set -q srcarch
+        switch (uname -m)
+            case arm64 aarch64'*'
+                set srcarch arm64
+            case x86_64
+                set srcarch x86
+        end
+    end
+    echo $srcarch
+end
+
+function __kmake_handle_make_var
+    string match -rq -- "(?<name>.+)=(?<value>.*)" (commandline -ct)
+    or return
+
+    switch $name
+        case ARCH
+            # sub-directories under arch/
+            set srctree (__kmake_get_srctree)
+            for arch in $srctree/arch/*
+                if test -d $arch
+                    set -a vals (path basename $arch)
+                end
+            end
+            # architectures hard-coded in the top Makefile
+            set -a vals i386 x86_64 sparc32 sparc64 parisc64
+
+        case LLVM
+            set -a vals 1
+            for val in $PATH/clang
+                if test -e $val
+                    set val (path dirname $val | path resolve)/
+                    if not contains $val $vals
+                        set -a vals $val
+                    end
+                end
+            end
+    end
+
+    set vals (string match -er -- "^$value" $vals)
+    string join \n -- $name=$vals
+end
+
+function __kmake_pos_args
+    # Variables (not exhaustive)
+    set vars \
+        ARCH \
+        CROSS_COMPILE \
+        KCONFIG_ALLCONFIG \
+        LLVM \
+        O \
+        V \
+        W \
+        INSTALL{,_MOD,_HDR,_DTBS}_PATH
+
+    # Targets
+    set targets \
+        all help \
+        clean mrproper distclean \
+        clang-{tidy,analyzer} compile_commands.json \
+        coccicheck \
+        dtbs{,_check,_install} dt_binding_{check,schemas} \
+        headers{,_install} \
+        vmlinux install \
+        modules{,_prepare,_install,_sign} \
+        vdso_install \
+        tags TAGS cscope gtags \
+        rust{available,fmt,fmtcheck} \
+        kernel{version,release} image_name \
+        kselftest{,-all,-install,-clean,-merge}
+
+    set configs \
+        {,old,olddef,sync,def,savedef,rand,listnew,helpnew,test,tiny}config \
+        {,build_}{menu,n,g,x}config \
+        local{mod,yes}config \
+        all{no,yes,mod,def}config \
+        {yes2mod,mod2yes,mod2no}config
+    set srctree (__kmake_get_srctree)
+    set srcarch (__kmake_get_srcarch)
+    for cfg in $srctree/{arch/$srcarch,kernel}/configs/*
+        set val (path basename $cfg)
+        if not contains $val $configs
+            set -a configs $val
+        end
+    end
+
+    set docs \
+        {html,textinfo,info,latex,pdf,epub,xml,linkcheck,refcheck,clean}docs
+
+    set packages \
+        {,bin,src}{rpm,deb}-pkg \
+        {pacman,dir,tar}-pkg \
+        tar{,gz,bz2,xz,zst}-pkg \
+        perf-tar{,gz,bz2,xz,zst}-src-pkg
+
+    string join \n -- $vars= $targets $configs $docs $packages
+end
+
+# Options
+complete -f -c kmake -s h -l help -d "Show help message and exit"
+complete -x -c kmake -s C -l directory -d "Mirrors the equivalent make argument" -a '(__fish_complete_directories (commandline -ct))'
+complete -f -c kmake -l no-ccache -d "Disable the use of ccache"
+complete -f -c kmake -l omit-o-arg -d "Avoid default use of O="
+complete -x -c kmake -s p -l prepend-to-path -d "Prepend specified directory to PATH" -a '(__fish_complete_directories (commandline -ct))'
+complete -x -c kmake -s j -l jobs -d "Number of jobs"
+complete -f -c kmake -l use-time -d "Call 'time -v' for time tracking"
+complete -f -c kmake -s v -l verbose -d "Do a more verbose build"
+
+# Positional arguments
+complete -f -c kmake -n 'not string match -eq = -- (commandline -ct)' -a '(__kmake_pos_args)' -f
+complete -f -c kmake -n 'string match -eq = -- (commandline -ct)' -a '(__kmake_handle_make_var)' -f
