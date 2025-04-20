0817cc3c2b53e544bbe9f58cfcff6f1c9de507c4
Nathan Chancellor <nathan@kernel.org>
2025-04-20 17:43:15 -0400

    fish: completions: tbf: Use 'path filter' instead of 'find'
    path does not warn when globs expand to empty.
    
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/completions/tbf.fish
+++ fish/completions/tbf.fish
@@ -1,1 +1,1 @@
-complete -c tbf -x -a "(find $TMP_BUILD_FOLDER -maxdepth 1 -mindepth 1 -type d 2>/dev/null | path basename)"
+complete -c tbf -x -a "(path filter -t dir $TMP_BUILD_FOLDER/* | path basename)"
