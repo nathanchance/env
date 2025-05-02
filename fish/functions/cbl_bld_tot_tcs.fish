7cb83701f85a7a5e04a0305063b37948ed66dc0f
Nathan Chancellor <nathan@kernel.org>
2025-05-01 20:17:49 -0700

    fish: cbl_bld_tot_tcs: Remove all patches
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/cbl_bld_tot_tcs.fish
+++ fish/functions/cbl_bld_tot_tcs.fish
@@ -156,16 +156,8 @@ function cbl_bld_tot_tcs -d "Build LLVM and binutils from source for kernel deve
             end
         end
     end
-    # https://github.com/llvm/llvm-project/pull/137166#issuecomment-2831550023
-    if not crl https://github.com/nathanchance/llvm-project/commit/855b06ca137298042f0ef944f3fc5d61b575e324.patch | git -C $llvm_project ap
-        set message "Failed to apply patch to turn off -Wdefault-const-init-unsafe by default"
-        print_error "$message"
-        tg_msg "$message"
-        return 1
-    end
 
     # Add in-review patches here
-    set -a gh_prs https://github.com/llvm/llvm-project/pull/137809 # [PowerPC] Fix an LowerADDSUBO_CARRY error when converting carry bit for usubo_carry
     for gh_pr in $gh_prs
         if gh_llvm pr view --json state (path basename $gh_pr) | python3 -c "import json, sys; sys.exit(0 if json.load(sys.stdin)['state'] == 'MERGED' else 1)"
             print_warning "$gh_pr has already been merged, skipping..."
