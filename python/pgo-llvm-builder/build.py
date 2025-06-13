ac47f145c2adda32cea134152abcd030da0827ae
Nathan Chancellor <nathan@kernel.org>
2025-06-13 09:11:01 -0700

    python: pgo-llvm-builder: LLVM 20.1.7
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- python/pgo-llvm-builder/build.py
+++ python/pgo-llvm-builder/build.py
@@ -22,13 +22,13 @@ MACHINE = platform.machine()
 
 LLVM_REFS = {
     '21.0.0': 'origin/main',
-    '20.1.7': 'origin/release/20.x',
+    '20.1.8': 'origin/release/20.x',
 }
 
 LLVM_VERSIONS = [
     '21.0.0',  # git
-    '20.1.7',  # release/20.x
-    '20.1.6',
+    '20.1.8',  # release/20.x
+    '20.1.7',
     '19.1.7',
     '18.1.8',
     '17.0.6',
