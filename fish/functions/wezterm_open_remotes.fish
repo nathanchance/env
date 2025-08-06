47db06ca2f81b9cb936501b6798694d896156c01
Nathan Chancellor <nathan@kernel.org>
2025-08-06 15:31:02 -0700

    fish: wezterm_open_remotes: Rank by relative power
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/wezterm_open_remotes.fish
+++ fish/functions/wezterm_open_remotes.fish
@@ -13,9 +13,9 @@ function wezterm_open_remotes -d "Open a new wezterm tab for each remote machine
         aadp:AADP \
         intel-desktop-11700:"Intel i7-11700" \
         amd-desktop-8745HS:"AMD mini desktop" \
-        intel-desktop-n100:"Intel mini desktop" \
         intel-laptop:"Intel laptop" \
-        honeycomb:Honeycomb
+        honeycomb:Honeycomb \
+        intel-desktop-n100:"Intel mini desktop"
 
     set msh_args $argv
 
