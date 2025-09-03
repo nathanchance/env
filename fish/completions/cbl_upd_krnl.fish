6fd4d04f17fb18cd8133ef9320e6939fa8500ae2
Nathan Chancellor <nathan@kernel.org>
2025-09-03 15:24:31 -0700

    fish: completions: Add cbl_upd_krnl
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ fish/completions/cbl_upd_krnl.fish
@@ -0,0 +1,8 @@
+complete -c cbl_upd_krnl -f
+complete -c cbl_upd_krnl -f -s r -l reboot -d "Reboot system after updating kernel"
+if test (get_distro) = arch
+    complete -c cbl_upd_krnl -f -s k -l kexec -d "Kexec system after updating kernel"
+
+    set -l valid_arch_krnls {linux-,}{debug,{mainline,next}-llvm}
+    complete -c cbl_upd_krnl -x -n "not __fish_seen_argument $valid_arch_krnls" -d Kernel -a "$valid_arch_krnls"
+end
