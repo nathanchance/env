33f92a0e2fa6a78fde88570ec750ebe9af3093da
Nathan Chancellor <nathan@kernel.org>
2025-09-05 15:02:19 -0700

    fish: klog: Wrap dmesg completions and handle other pager options
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/klog.fish
+++ fish/functions/klog.fish
@@ -2,18 +2,23 @@
 # SPDX-License-Identifier: MIT
 # Copyright (C) 2024 Nathan Chancellor
 
-function klog -d "View kernel log with bat"
+function klog -d "View kernel log with bat" -w dmesg
     in_container_msg -h
     or return
 
-    if contains -- --follow-new $argv
-        set provides_pager true
+    for arg in $argv
+        set -a dmesg_args $arg
+        switch $arg
+            case -w --follow -W --follow-new
+                set provides_pager true
+        end
     end
+
     set dmesg_cmd \
         sudo dmesg \
         --human \
         --color=always \
-        $argv
+        $dmesg_args
 
     if set -q provides_pager
         $dmesg_cmd
