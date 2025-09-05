063418b39476f185647cbc8a7f5827f9bd6c0d6e
Nathan Chancellor <nathan@kernel.org>
2025-09-05 16:29:15 -0700

    fish: completions: edfunc: Add flags
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/completions/edfunc.fish
+++ fish/completions/edfunc.fish
@@ -1,1 +1,3 @@
 complete -c edfunc -x -a "(__gen_func_completions)"
+complete -c edfunc -f -s r -l reload -d "Reload fish files after editing function"
+complete -c edfunc -f -s f -l fzf -d "Select functions to edit using fzf"
