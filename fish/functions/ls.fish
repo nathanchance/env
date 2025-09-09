b697102e404b25cb0752c9f13ab2c826a3934ad0
Nathan Chancellor <nathan@kernel.org>
2025-09-09 11:38:59 -0700

    fish: ls: Fix '--color' and simplify
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/ls.fish
+++ fish/functions/ls.fish
@@ -3,13 +3,9 @@
 # Copyright (C) 2021-2023 Nathan Chancellor
 
 function ls -d "Use eza instead of ls if it is available" -w eza
-    if status is-interactive
-        if command -q eza
-            eza $argv
-        else
-            command ls --colors=auto $argv
-        end
+    if status is-interactive; and command -q eza
+        eza $argv
     else
-        command ls $argv
+        command ls --color=auto $argv
     end
 end
