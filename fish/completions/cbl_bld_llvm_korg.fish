ae511618630766b6f1011aa59748cd3b0315b7d1
Nathan Chancellor <nathan@kernel.org>
2025-09-08 22:41:50 -0700

    fish: completions: Add cbl_bld_llvm_korg
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ fish/completions/cbl_bld_llvm_korg.fish
@@ -0,0 +1,10 @@
+function __cbl_bld_llvm_korg_gen_vers
+    PYTHONPATH=$PYTHON_FOLDER/pgo-llvm-builder python3 -c 'from build import LLVM_VERSIONS
+for ver in LLVM_VERSIONS:
+    print(ver)'
+end
+
+complete -c cbl_bld_llvm_korg -f -s b -l build-env -d "Build environment with mkosi"
+complete -c cbl_bld_llvm_korg -f -s r -l reset -d "Delete outputs of previous runs"
+complete -c cbl_bld_llvm_korg -f -s t -l test-linux -d "Build Linux with toolchain"
+complete -c cbl_bld_llvm_korg -x -d "LLVM version" -a '(__cbl_bld_llvm_korg_gen_vers)'
