7fb3bfbd7dd20ec6c64eb702128645a2f105428d
Nathan Chancellor <nathan@kernel.org>
2025-09-08 22:40:27 -0700

    fish: edcomp: Allow creating completions
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/edcomp.fish
+++ fish/functions/edcomp.fish
@@ -19,19 +19,7 @@ function edcomp -d "Edit completions in $ENV_FOLDER"
         set -a comps_to_edit (path filter -f $comp_dir/* | path basename | path change-extension '' | fzf -m --preview "cat $comp_dir/{}.fish")
     end
 
-    for comp_to_edit in $comps_to_edit
-        if test -f $comp_to_edit
-            set comp_file $comp_to_edit
-        else
-            set comp_file $comp_dir/$comp_to_edit.fish
-            if not test -f "$comp_file"
-                __print_error "$comp_file does not exist!"
-                return 1
-            end
-        end
-        set -a files_to_edit $comp_file
-    end
-
+    set files_to_edit $comp_dir/$comps_to_edit.fish
     if test -n "$files_to_edit"
         vim -p $files_to_edit
         if test "$reload" = true
