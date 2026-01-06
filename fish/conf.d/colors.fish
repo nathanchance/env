160aed3e750284d8536efa0a76ae9022c19cb001
Nathan Chancellor <nathan@kernel.org>
2026-01-06 14:32:47 -0700

    fish: colors: Use global scope for colors
    A warning is emitted in fish 4.3 if the color variables are universal,
    so migrate them to global variables.
    
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/conf.d/colors.fish
+++ fish/conf.d/colors.fish
@@ -1,32 +1,32 @@
 # fish colors
-set -U fish_color_normal normal
-set -U fish_color_command blue
-set -U fish_color_quote yellow
-set -U fish_color_redirection green
-set -U fish_color_end green
-set -U fish_color_error red
-set -U fish_color_param cyan
-set -U fish_color_comment brblack
-set -U fish_color_match normal
-set -U fish_color_selection 97979b
-set -U fish_color_search_match yellow
-set -U fish_color_history_current normal
-set -U fish_color_operator magenta
-set -U fish_color_escape magenta
-set -U fish_color_cwd blue
-set -U fish_color_cwd_root blue
-set -U fish_color_valid_path normal
-set -U fish_color_autosuggestion 97979b
-set -U fish_color_user yellow
-set -U fish_color_host green
-set -U fish_color_cancel normal
-set -U fish_pager_color_completion normal
-set -U fish_pager_color_description B3A06D yellow
-set -U fish_pager_color_prefix white --bold --underline
-set -U fish_pager_color_progress brwhite --background=cyan
+set -g fish_color_normal normal
+set -g fish_color_command blue
+set -g fish_color_quote yellow
+set -g fish_color_redirection green
+set -g fish_color_end green
+set -g fish_color_error red
+set -g fish_color_param cyan
+set -g fish_color_comment brblack
+set -g fish_color_match normal
+set -g fish_color_selection 97979b
+set -g fish_color_search_match yellow
+set -g fish_color_history_current normal
+set -g fish_color_operator magenta
+set -g fish_color_escape magenta
+set -g fish_color_cwd blue
+set -g fish_color_cwd_root blue
+set -g fish_color_valid_path normal
+set -g fish_color_autosuggestion 97979b
+set -g fish_color_user yellow
+set -g fish_color_host green
+set -g fish_color_cancel normal
+set -g fish_pager_color_completion normal
+set -g fish_pager_color_description B3A06D yellow
+set -g fish_pager_color_prefix white --bold --underline
+set -g fish_pager_color_progress brwhite --background=cyan
 
 # hydro colors
-set -U hydro_color_pwd blue
-set -U hydro_color_git magenta
-set -U hydro_color_duration yellow
-set -U hydro_color_prompt green
+set -g hydro_color_pwd blue
+set -g hydro_color_git magenta
+set -g hydro_color_duration yellow
+set -g hydro_color_prompt green
