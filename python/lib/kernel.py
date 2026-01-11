a3c11552efb0358fffd0135acad9be3f09074980
Nathan Chancellor <nathan@kernel.org>
2026-01-10 17:01:30 -0700

    python: lib: kernel: drm/amd/display dcn30 patches have been merged
    Link: https://git.kernel.org/linus/f54a91f5337cd918eb86cf600320d25b6cfd8209
    Link: https://git.kernel.org/linus/6ce6fbfddc5b127e4f57c3b5bfdcf40239a4fc2f
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- python/lib/kernel.py
+++ python/lib/kernel.py
@@ -99,10 +99,6 @@ def prepare_source(base_name, base_ref='origin/master'):
 
     # Patching section
     # yapf: disable
-    if base_name == 'linux-mainline-llvm':
-        patches.append('https://gitlab.freedesktop.org/agd5f/linux/-/commit/b20b3fc4210f83089f835cdb91deec4b0778761a.patch')  # drm/amd/display: Reduce number of arguments of dcn30's CalculatePrefetchSchedule()
-        patches.append('https://gitlab.freedesktop.org/agd5f/linux/-/commit/563dfbefdf633c8d958398ddfa3955f9f40e47d9.patch')  # drm/amd/display: Reduce number of arguments of dcn30's CalculateWatermarksAndDRAMSpeedChangeSupport()
-
     if base_name in PACMAN_TREES:
         patches.append('https://lore.kernel.org/all/20251124230045.3188417-1-sakari.ailus@linux.intel.com/')  # media: ccs: Avoid possible division by zero
 
