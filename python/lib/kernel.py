9c8e421d4c9b4b0efb10b81853518efd364c1ad6
Nathan Chancellor <nathan@kernel.org>
2026-01-21 14:15:49 -0700

    env: Update for next-20260121
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- python/lib/kernel.py
+++ python/lib/kernel.py
@@ -105,6 +105,9 @@ def prepare_source(base_name, base_ref='origin/master'):
     if base_name == 'fedora':
         patches.append('https://lore.kernel.org/all/20260114-panel-ilitek-ili9882t-fix-override-init-v1-1-1d69a2b096df@kernel.org/')  # drm/panel: ilitek-ili9882t: Remove duplicate initializers in tianma_il79900a_dsc
 
+        # https://lore.kernel.org/kvmarm/86zf67b5oa.wl-maz@kernel.org/T/#mec328f99ad66bc575bc0200bc7ce5840d181e629
+        patches.append('https://lore.kernel.org/kvmarm/86zf67b5oa.wl-maz@kernel.org/raw')
+
     # Distributed ThinLTO support ahead of acceptance in kbuild tree
     if base_name in NEXT_TREES:
         patches.append('https://lore.kernel.org/all/20251028182822.3210436-2-xur@google.com/')  # kbuild: move vmlinux.a build rule to scripts/Makefile.vmlinux_a
