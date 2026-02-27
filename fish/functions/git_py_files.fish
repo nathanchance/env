1b8e708a85e89332e5bfe1eb29d0b3e6f2b2676a
Nathan Chancellor <nathan@kernel.org>
2026-02-27 00:33:44 -0700

    fish: git_py_files: Filter out symlinks altogether
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/git_py_files.fish
+++ fish/functions/git_py_files.fish
@@ -4,10 +4,8 @@
 
 function git_py_files -d "Get Python files checked into current repository for linting"
     # Get list of files, specifically excluding this one, as it is a false
-    # positive due to the command used :) We pass through realpath and uniq
-    # to handle files that may be symlinked multiple times like korg_tc,
-    # which can make linting annoying otherwise
-    set files (git ls-files | string match -rv (status function).fish | path resolve | uniq | string replace "$PWD/" '')
+    # positive due to the command used :)
+    set files (git ls-files | path filter -t link -v | string match -rv (status function).fish)
 
     # Sort after ripgrep because sorting within ripgrep reduces parallelism
     rg -l '#!/usr/bin/(env )?(python|-S uv)' $files 2>/dev/null | path sort
