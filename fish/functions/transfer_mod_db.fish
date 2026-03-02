567dba2bff856e8f9b02ab363f3ee6c6c0dc3563
Nathan Chancellor <nathan@kernel.org>
2026-03-02 16:11:56 -0700

    fish: transfer_mod_db: Allow changing target IP address via argument
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/transfer_mod_db.fish
+++ fish/functions/transfer_mod_db.fish
@@ -2,6 +2,11 @@
 # SPDX-License-Identifier: MIT
 # Copyright (C) 2024 Nathan Chancellor
 
-function transfer_mod_db -d "Transfer modprobed.db to $MAIN_REMOTE_IP"
-    scp $HOME/.config/modprobed.db nathan@$MAIN_REMOTE_IP:/tmp
+function transfer_mod_db -d "Transfer modprobed.db to specified IP address"
+    if test (count $argv) -eq 0
+        set ip (get_ip main)
+    else
+        set ip $argv[1]
+    end
+    scp $HOME/.config/modprobed.db nathan@$ip:/tmp
 end
