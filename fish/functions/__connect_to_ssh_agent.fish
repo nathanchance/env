da16adf6ffdeac3319a1457e5876771ad44e6832
Nathan Chancellor <nathan@kernel.org>
2026-03-06 23:37:39 -0700

    fish: __connect_to_ssh_agent: Fix for use on macOS
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/__connect_to_ssh_agent.fish
+++ fish/functions/__connect_to_ssh_agent.fish
@@ -5,41 +5,43 @@
 function __connect_to_ssh_agent -d "Connect to an ssh-agent and load my SSH key"
     set ssh_key $HOME/.ssh/id_ed25519
     begin
-        not set -q SSH_AUTH_SOCK
-        and status is-interactive
+        status is-interactive
         or __in_nspawn
         and command -q ssh-add
         and test -f "$ssh_key"
     end
     or return 0
 
-    if set -q XDG_RUNTIME_DIR
-        set xdg_runtime_folder $XDG_RUNTIME_DIR
-    else
-        # This should only happen when using 'sd_nspawn -r' with systemd 257
-        # or older
-        set xdg_runtime_folder /run/user/(id -u)
-    end
-    set possible_ssh_agent_sockets \
-        # orbstack
-        $OPT_ORB_GUEST/run/host-ssh-agent.sock \
-        # Arch / Fedora
-        $xdg_runtime_folder/ssh-agent.socket \
-        # Debian
-        $xdg_runtime_folder/openssh_agent
+    # Attempt to locate an ssh-agent socket if it is not already set
+    if not set -q SSH_AUTH_SOCK
+        if set -q XDG_RUNTIME_DIR
+            set xdg_runtime_folder $XDG_RUNTIME_DIR
+        else
+            # This should only happen when using 'sd_nspawn -r' with systemd 257
+            # or older
+            set xdg_runtime_folder /run/user/(id -u)
+        end
+        set possible_ssh_agent_sockets \
+            # orbstack
+            $OPT_ORB_GUEST/run/host-ssh-agent.sock \
+            # Arch / Fedora
+            $xdg_runtime_folder/ssh-agent.socket \
+            # Debian
+            $xdg_runtime_folder/openssh_agent
 
-    for ssh_agent_socket in $possible_ssh_agent_sockets
-        if test -S $ssh_agent_socket; and test (stat -c %u $ssh_agent_socket) = (id -u)
-            set -gx SSH_AUTH_SOCK $ssh_agent_socket
-            break
+        for ssh_agent_socket in $possible_ssh_agent_sockets
+            if test -S $ssh_agent_socket; and test (stat -c %u $ssh_agent_socket) = (id -u)
+                set -gx SSH_AUTH_SOCK $ssh_agent_socket
+                break
+            end
+        end
+        if not set -q SSH_AUTH_SOCK
+            __print_error "No previously started agent available?"
+            return 1
         end
     end
-    if not set -q SSH_AUTH_SOCK
-        __print_error "No previously started agent available?"
-        return 1
-    end
 
-    if not set -q TMUX; and not __in_nspawn; and sd_nspawn --is-running
+    if test "$LOCATION" != mac; and not set -q TMUX; and not __in_nspawn; and sd_nspawn --is-running
         sd_nspawn -r /bin/true
     end
 
