bc9d8039ea8d19b9ce8feedff2923cd3c5cedf18
Nathan Chancellor <nathan@kernel.org>
2026-03-18 16:08:56 -0700

    fish: __gen_func_completions: Run Python scripts with uv quietly
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/__gen_func_completions.fish
+++ fish/functions/__gen_func_completions.fish
@@ -5,7 +5,7 @@
 function __gen_func_completions -d "Generate completions for self-created functions"
     __get_my_funcs | string match -er (commandline -ct) | while read -l func
         if test -e $PYTHON_BIN_FOLDER/$func
-            set desc ($func -h | string match -er '^[A-Z].*$')
+            set desc (uv --no-progress --quiet run --script $PYTHON_SCRIPTS_FOLDER/$func.py -h | string match -er '^[A-Z].*$')
             if test -z "$desc"
                 set desc "python script"
             end
