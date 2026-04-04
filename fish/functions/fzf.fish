5fca6958a81523b3559a9f11d2dfef2b83750aaa
Nathan Chancellor <nathan@kernel.org>
2026-04-05 01:23:00 +0200

    fish: Remove /var/tmp/fzf handling
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/fzf.fish
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env fish
-# SPDX-License-Identifier: MIT
-# Copyright (C) 2022-2023 Nathan Chancellor
-
-function fzf -d "Call fzf with custom environment"
-    set -fx TMPDIR /var/tmp/fzf
-    command fzf $argv
-end
--- fish/functions/setup_sd_nspawn.fish
+++ fish/functions/setup_sd_nspawn.fish
@@ -3,16 +3,6 @@
 # Copyright (C) 2025 Nathan Chancellor
 
 function setup_sd_nspawn -d "Perform initial systemd-nspawn setup"
-    # These platforms will more than likely use tmux so ensure the tmux
-    # directory exists with the expected permissions so that sd_nspawn
-    # will find it and mount it into the container properly. Even if they
-    # do not use tmux, creating the directory and passing it through to
-    # the container is not the end of the world, as no socket will exist.
-    set tmux_tmp /var/tmp/tmux-(id -u)
-    mkdir -p $tmux_tmp
-    # tmux checks that the permissions are restrictive
-    chmod 700 $tmux_tmp
-
     request_root "Setting up systemd-nspawn"
     or return
 
