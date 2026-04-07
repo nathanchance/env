daeee33e8e1e0b5f37b2caab5672eb80bfe80c36
Nathan Chancellor <nathan@kernel.org>
2026-04-06 18:26:17 -0700

    fish: setup_libvirt: Updates
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/setup_libvirt.fish
+++ fish/functions/setup_libvirt.fish
@@ -5,11 +5,12 @@
 function setup_libvirt -d "Setup libvirt for current user"
     __in_container_msg -h; or return
 
-    request_root "Setting up libvirt"
-    or return
-
     if not command -q virsh
-        upd -y; or return
+        request_root "Setting up libvirt"
+        or return
+
+        upd -y
+        or return
 
         set distro (__get_distro)
         switch $distro
@@ -21,29 +22,25 @@ function setup_libvirt -d "Setup libvirt for current user"
                     dnsmasq \
                     libvirt \
                     qemu-desktop \
-                    virt-install; or return
-
-                run0 pacman \
-                    -S \
-                    --noconfirm \
-                    --ask 4 \
-                    iptables-nft; or return
+                    virt-install
+                or return
 
             case fedora
-                run0 dnf install -y @virtualization; or return
+                run0 dnf install -y @virtualization
+                or return
 
             case '*'
                 __print_error "$distro is not supported by setup_libvirt!"
                 return 1
         end
-    end
 
-    # Ensure that the rest of this function says in sync with setup_libvirt_common() in 'bash/common'
-    set user (id -un)
-    run0 fish -c "usermod -aG libvirt $user
+        run0 fish -c "usermod -aG libvirt $USER
 and systemctl enable --now libvirtd.service
 and virsh net-autostart default
 and if virsh net-info default | string match -qr 'Active:\s+no'
     virsh net-start default
 end"
+    end
+
+    setup_libvirt_pool
 end
