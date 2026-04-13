e673ab11665c41ddc66a213820cf0911c009931c
Nathan Chancellor <nathan@kernel.org>
2026-04-12 23:31:07 -0700

    fish: fish_format: Fix regex for completion files
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/fish_format.fish
+++ fish/functions/fish_format.fish
@@ -3,7 +3,7 @@
 # Copyright (C) 2021-2023 Nathan Chancellor
 
 function fish_format -d "Format all fish files in directory with fish_indent"
-    rg -l '#!/usr/bin/(env )?fish' | while read -l file
+    rg -l -e '#!/usr/bin/(env )?fish' -e '^complete -' | while read -l file
         fish_indent -w $file
     end
 end
