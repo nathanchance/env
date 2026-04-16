848941b087169207cfa10b67ff9e0075791658df
Nathan Chancellor <nathan@kernel.org>
2026-04-15 18:48:55 -0700

    fish: git_py_files: Look at extension as well as shebang
    Ruff's EXE001 flags .py files with a shebang that are not executable. To
    fix those, we may need to remove the shebang, which would break this
    check.
    
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/git_py_files.fish
+++ fish/functions/git_py_files.fish
@@ -8,5 +8,8 @@ function git_py_files -d "Get Python files checked into current repository for l
     set files (git ls-files | path filter -t link -v | string match -rv (status function).fish)
 
     # Sort after ripgrep because sorting within ripgrep reduces parallelism
-    rg -l '#!/usr/bin/(env )?(python|-S uv)' $files 2>/dev/null | path sort
+    begin
+        string match -er '\.py$' $files
+        rg -l '#!/usr/bin/(env )?(python|-S uv)' $files 2>/dev/null
+    end | path sort -u
 end
