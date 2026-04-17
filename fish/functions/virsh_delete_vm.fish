ab5bc6a91726797fc77cc4359bb1ea8c157a100c
Nathan Chancellor <nathan@kernel.org>
2026-04-17 14:24:29 -0700

    fish: virsh_delete_vm: Rewrite
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- fish/functions/virsh_delete_vm.fish
+++ fish/functions/virsh_delete_vm.fish
@@ -3,17 +3,20 @@
 # Copyright (C) 2022-2023 Nathan Chancellor
 
 function virsh_delete_vm -d "Remove virtual machine using virsh"
-    set name $argv[1]
+    set running_domains (virsh list --name --state-running | string match -rv '^$')
 
-    if set virsh_dominfo (virsh dominfo $name 2>/dev/null)
-        if string match -qr "State:\s+running" $virsh_dominfo
-            virsh destroy $name
+    for domain in $argv
+        if contains $domain $running_domains
+            virsh destroy $domain
+            or return
         end
 
-        virsh undefine --nvram $name
-        or virsh undefine $name
-        or return
+        set -l nvram
+        if virsh dumpxml $domain | string match -qr nvram
+            set nvram --nvram
+        end
 
-        virsh vol-delete --pool default $name.qcow2
+        virsh undefine $nvram --remove-all-storage $domain
+        or return
     end
 end
