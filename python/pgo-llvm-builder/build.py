07851352f7470c445505e4495c2a2d77117ae10f
Nathan Chancellor <nathan@kernel.org>
2026-04-21 10:09:52 -0700

    python: pgo-llvm-builder: LLVM 22.1.4
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- python/pgo-llvm-builder/build.py
+++ python/pgo-llvm-builder/build.py
@@ -24,13 +24,13 @@ MACHINE = platform.machine()
 
 LLVM_REFS: dict[str, str] = {
     '23.0.0': 'origin/main',
-    '22.1.4': 'origin/release/22.x',
+    '22.1.5': 'origin/release/22.x',
 }
 
 LLVM_VERSIONS: list[str] = [
     '23.0.0',  # git
-    '22.1.4',  # release/22.x
-    '22.1.3',
+    '22.1.5',  # release/22.x
+    '22.1.4',
     '21.1.8',
     '20.1.8',
     '19.1.7',
