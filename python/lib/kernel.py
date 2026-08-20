2f920dcd3b24f4796fec56e2e694e7813f2765b0
Nathan Chancellor <nathan@kernel.org>
2026-08-20 11:50:13 -0700

    env: Update for next-20260820
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- python/lib/kernel.py
+++ python/lib/kernel.py
@@ -111,7 +111,7 @@ def prepare_source(base_name: str, base_ref: str = 'origin/master') -> None:
         patches += [
             'https://lore.kernel.org/all/0445b20df50894615ada70abb1e6238f33c78a2e.1786768375.git.jpoimboe@kernel.org/',  # efi/libstub: Preserve the GNU property note
             # https://lore.kernel.org/20260819003752.GA3063251@ax162/
-            'https://lore.kernel.org/all/20260819194039.GA3686901@ax162/raw',  # fixup for "ACPI: scan: Avoid registering platform devices with resource overlaps"
+            'https://lore.kernel.org/all/12956979.O9o76ZdvQC@rafael.j.wysocki/',  # ACPI: scan: Do not combine resources that overlap completely
         ]
 
     if base_name == 'linux-next-llvm':
