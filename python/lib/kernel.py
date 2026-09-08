d1d21a54e776004eee126ac9b96a76e8e1e1c05b
Nathan Chancellor <nathan@kernel.org>
2026-09-08 16:42:28 -0700

    env: Update for next-20260908
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- python/lib/kernel.py
+++ python/lib/kernel.py
@@ -105,6 +105,7 @@ def prepare_source(base_name: str, base_ref: str = 'origin/master') -> None:
     if base_name == 'linux-next-llvm':
         patches += [
             'https://lore.kernel.org/all/20260618-amdgpu-fix-wq_name_len-warning-v2-1-ef0e2e6f5be7@kernel.org/',  # drm/amd/display: Shorten hdmi_frl_status_polling_workqueue
+            'https://lore.kernel.org/all/20260908092730.4002628-1-rppt@kernel.org/',  # x86/mm/pat: skip RWX verification until kernel text is set to read only
         ]
 
     if base_name in PACMAN_TREES:
